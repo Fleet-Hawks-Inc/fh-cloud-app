288d2314d3a22bcbee8fe8ecbc5994e2e7bd4266
amritfleethawks <60138174+amritfleethawks@users.noreply.github.com>
2020-04-21 22:09:45 +0530

    api url updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  BaseUrl : 'http://localhost:3000/api/v1/',
+  BaseUrl : 'https://fleetservice.us-east-2.fleethawks.com/v1/',
   temp : 'search',
   LoginUrl : 'users/login',
   ApiKey : ''
