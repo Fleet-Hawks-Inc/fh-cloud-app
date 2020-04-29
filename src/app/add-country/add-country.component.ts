304c4b793f84b1be307bc9e31ffdf7238bf639ef
Gaurav Dhiman <gaurav@fleethawks.com>
2020-04-29 10:24:50 +0530

    countries changes

--- src/app/add-country/add-country.component.ts
+++ src/app/add-country/add-country.component.ts
@@ -1,45 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import {ApiService} from '../api.service';
-import {from} from 'rxjs';
-import {map} from 'rxjs/operators';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { ApiService } from "../api.service";
+import { from } from "rxjs";
+import { map } from "rxjs/operators";
 declare var $: any;
 @Component({
-  selector: 'app-add-country',
-  templateUrl: './add-country.component.html',
-  styleUrls: ['./add-country.component.css']
+  selector: "app-add-country",
+  templateUrl: "./add-country.component.html",
+  styleUrls: ["./add-country.component.css"],
 })
 export class AddCountryComponent implements OnInit {
-
-  title = 'Add Countries';
+  title = "Add Countries";
 
   errors = {};
   form;
 
   /********** Form Fields ***********/
 
-  countryID ='';
-  countryName = '';
-
+  countryCode = "";
+  countryName = "";
 
   /******************/
 
-  response : any = '';
-  hasError =  false;
-  hasSuccess =   false;
-  Error : string = '';
-  Success : string = '';
+  response: any = "";
+  hasError = false;
+  hasSuccess = false;
+  Error: string = "";
+  Success: string = "";
 
-  constructor(private route: ActivatedRoute,
-              private apiService: ApiService) { }
-
+  constructor(private route: ActivatedRoute, private apiService: ApiService) {}
 
   ngOnInit() {
     $(document).ready(() => {
-      this.form = $('#form_').validate();
+      this.form = $("#form_").validate();
     });
   }
-
 
   addCountry() {
     this.errors = {};
@@ -47,40 +42,36 @@ export class AddCountryComponent implements OnInit {
     this.hasSuccess = false;
 
     const data = {
-      'countryID': this.countryID,
-      'countryName': this.countryName
+      countryCode: this.countryCode,
+      countryName: this.countryName,
     };
 
-    this.apiService.postData('countries', data)
-      .subscribe({
-        complete : () => {},
-        error : (err) => {
-          from(err.error)
-            .pipe(
-              map((val: any) => {
-                val.message = val.message.replace(/".*"/, 'This Field');
-                this.errors[val.context.key] = val.message;
-              }),
-            )
-            .subscribe((val) => {
-              this.throwErrors();
-            });
+    this.apiService.postData("countries", data).subscribe({
+      complete: () => {},
+      error: (err) => {
+        from(err.error)
+          .pipe(
+            map((val: any) => {
+              val.message = val.message.replace(/".*"/, "This Field");
+              this.errors[val.context.key] = val.message;
+            })
+          )
+          .subscribe((val) => {
+            this.throwErrors();
+          });
+      },
+      next: (res) => {
+        this.countryCode = "";
+        this.countryName = "";
 
-        },
-        next: (res) => {
-          this.countryID = '';
-          this.countryName = '';
-
-          this.response = res;
-          this.hasSuccess = true;
-          this.Success = 'Country Added successfully';
-
-        }
-      });
+        this.response = res;
+        this.hasSuccess = true;
+        this.Success = "Country Added successfully";
+      },
+    });
   }
 
   throwErrors() {
     this.form.showErrors(this.errors);
   }
-
 }
