32bebf21929ed8f0791127557f2856120d74ad18
Gaurav Dhiman <gaurav@fleethawks.com>
2020-05-09 16:31:29 +0530

    HOS-compliance-summary added

--- src/app/hos/compliance/summary/summary.component.ts
+++ src/app/hos/compliance/summary/summary.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { ApiService } from "../../../api.service";
 import { Router } from "@angular/router";
 import { timer } from "rxjs";
+import * as moment from "moment";
 declare var $: any;
 
 @Component({
@@ -12,11 +13,30 @@ declare var $: any;
 export class SummaryComponent implements OnInit {
   title = "Logs";
   logs = [];
-  constructor() {}
+  drivers = [];
+  userName = "";
+  fromDate = "";
+  toDate = "";
+  constructor(private apiService: ApiService) {}
 
   ngOnInit() {
-    this.fetchLogs();
+    this.fetchDrivers();
   }
 
-  fetchLogs() {}
+  fetchDrivers() {
+    this.apiService.getData("users/userType/driver").subscribe((result: any) => {
+      this.drivers = result.Items;
+    });
+    console.log(this.drivers);
+  }
+
+  getLogs() {
+    let from  = moment(this.fromDate).format('DD-MM-YYYY');
+    let to = moment(this.toDate).format('DD-MM-YYYY');
+
+    this.apiService.getData(`eventLogs/HOSSummary?userName=${this.userName}&fromDate=${from}&toDate=${to}`).subscribe((result: any) => {
+      this.logs = result;
+    });
+    console.log(this.logs);
+  }
 }
--- src/app/hos/hos-routing.module.ts
+++ src/app/hos/hos-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SummaryComponent } from "./compliance/summary/summary.component";
-
+import { DetailComponent } from "./compliance/detail/detail.component";
 
 const routes: Routes = [
   {
     path: "compliance",
     children: [
       { path: "summary", component: SummaryComponent },
+      { path: "detail/:userName/:date", component: DetailComponent }
     ],
   },
 ];
