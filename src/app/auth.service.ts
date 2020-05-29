9aea2514d131cb6b7b57f3b620f058f30280b797
Deshmukh, Kunal <kunal.deshmukh@siemens.com>
2020-05-29 14:24:01 +0530

    Update package.

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,20 +1,14 @@
 import { Injectable } from '@angular/core';
-import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
-import {Observable} from 'rxjs/index';
-import {ApiService} from './api.service';
-import { JwtHelperService } from '@auth0/angular-jwt';
-import {AuthenticateService} from './authenticate.service';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 import { Auth } from 'aws-amplify';
+import { Observable } from 'rxjs/index';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService implements CanActivate {
-  private message: string;
-  private jwtHelper = new JwtHelperService();
-  constructor(private _router: Router,
-              private apiService: ApiService,
-              private authenticate: AuthenticateService) { }
+
+  constructor(private readonly router: Router) { }
 
   /**
    * this is used to clear anything that needs to be removed
@@ -23,74 +17,34 @@ export class AuthService implements CanActivate {
     localStorage.clear();
   }
 
-  /**
-   * check for expiration and if token is still existing or not
-   * @return {boolean}
-   */
-  isAuthenticated(): boolean {
-
-    if (localStorage.getItem('LoggedIn') != null && localStorage.getItem('jwt') != null) {
-      this.apiService.jwt = localStorage.getItem('jwt');
-      this.apiService.jwtDecoded = this.decode();
-      return true;
-    }
-    return false;
-    //return localStorage.getItem('token') != null && !this.isTokenExpired();
-  }
-
-
 
   canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    return this.isAuthenticated();
+
+  }
+
+  public isAuthenticated = () => {
     return Auth.currentAuthenticatedUser().then(() => {
       return true;
     }).catch(() => {
-      this._router.navigate(['/Login']);
+      this.router.navigate(['/Login']);
       return false;
     });
-
-  }
-
-  _canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-
-    // const currentUser = this.authenticate.currentUserValue;
-    // if (currentUser) {
-    //   // check if route is restricted by role
-    //   if (next.data.roles && next.data.roles.indexOf(currentUser.role) === -1) {
-    //     // role not authorised so redirect to home page
-    //     this._router.navigate(['/Login']);
-    //     return false;
-    //   }
-    // }
-
-
-    if (this.isAuthenticated()) {
-      this.apiService.jwtDecoded = this.decode();
-      return true;
-    }
-
-    // navigate to login page
-    this._router.navigate(['/Login']);
-    // you can save redirect url so after authing we can move them back to the page they requested
-    return false;
-
   }
 
   // simulate jwt token is valid
-  // https://github.com/theo4u/angular4-auth/blob/master/src/app/helpers/jwt-helper.ts
   isTokenExpired(): boolean {
     return true;
   }
 
   loginAdmin(): void {
-    // localStorage.setItem('token', `eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE1MzMyNzM5NjksImV4cCI6MTU2NDgxMDAwNSwiYXVkIjoid3d3LmV4YW1wbGUuY29tIiwic3ViIjoiVGVzdCBHdWFyZCIsIkdpdmVuTmFtZSI6IkpvaG5ueSIsIlN1cm5hbWUiOiJSb2NrZXQiLCJFbWFpbCI6Impyb2NrZXRAZXhhbXBsZS5jb20iLCJyb2xlIjoiQWRtaW4ifQ.rEkg53_IeCLzGHlmaHTEO8KF5BNfl6NEJ8w-VEq2PkE`);
 
-    this._router.navigate(['/Dashboard']);
+    this.router.navigate(['/Dashboard']);
   }
 
   login(): void {
-    // localStorage.setItem('token', `eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE1MzMyNzM5NjksImV4cCI6MTU2NDgxMDAwNSwiYXVkIjoid3d3LmV4YW1wbGUuY29tIiwic3ViIjoiVGVzdCBHdWFyZCIsIkdpdmVuTmFtZSI6IkpvaG5ueSIsIlN1cm5hbWUiOiJSb2NrZXQiLCJFbWFpbCI6Impyb2NrZXRAZXhhbXBsZS5jb20ifQ.GA0Y9gYIjmx1jLwuRHuBgZ8m6o-NgkD84nO0ym68CWo`);
 
-    this._router.navigate(['/Dashboard']);
+    this.router.navigate(['/Dashboard']);
   }
 
   /**
@@ -98,13 +52,7 @@ export class AuthService implements CanActivate {
    */
   logout(): void {
     this.clear();
-    this._router.navigate(['/Login']);
+    this.router.navigate(['/Login']);
   }
 
-  decode() {
-    const decodedToken =  this.jwtHelper.decodeToken(localStorage.getItem('jwt'));
-    // console.log(decodedToken);
-    // console.log(decodedToken.carrierID);
-    return decodedToken;
-  }
 }
