93aac01db36864f8cf0467d6d3e709105c83e17a
Gaurav Dhiman <gaurav@fleethawks.com>
2020-06-03 17:57:40 +0530

    accounts, safety and manage module added and menu sub menus updated

---
+++ src/app/safety/safety.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SafetyRoutingModule } from './safety-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SafetyRoutingModule
+  ]
+})
+export class SafetyModule { }
