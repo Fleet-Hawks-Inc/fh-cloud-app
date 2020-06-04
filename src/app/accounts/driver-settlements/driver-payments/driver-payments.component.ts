1808a3a2ecba4265b6e07dba91860dc7fa2b4654
Gaurav Dhiman <gaurav@fleethawks.com>
2020-06-04 11:05:09 +0530

    Accounts modules pages added.

---
+++ src/app/accounts/driver-settlements/driver-payments/driver-payments.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-driver-payments',
+  templateUrl: './driver-payments.component.html',
+  styleUrls: ['./driver-payments.component.css']
+})
+export class DriverPaymentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/accounts/invoices/partially-paid-invoices/partially-paid-invoices.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-partially-paid-invoices',
+  templateUrl: './partially-paid-invoices.component.html',
+  styleUrls: ['./partially-paid-invoices.component.css']
+})
+export class PartiallyPaidInvoicesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
