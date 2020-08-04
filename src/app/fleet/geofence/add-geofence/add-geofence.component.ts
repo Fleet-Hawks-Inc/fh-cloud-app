10c38d5b277a5d7254684cd11fd72bf3d5ab46e4
daljit Singh <daljit@fleethawks.com>
2020-08-04 19:20:26 +0530

    geofencing with search done

--- src/app/fleet/geofence/add-geofence/add-geofence.component.ts
+++ src/app/fleet/geofence/add-geofence/add-geofence.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { ApiService } from "../../../api.service";
+import { environment } from '../../../../environments/environment';
+
 import { from } from "rxjs";
 //import { MapBoxService } from "../../../map-box.service";
-import { LeafletMapService } from './../../../services/leaflet-map.service'
+import { LeafletMapService } from './../../../services/leaflet-map.service';
 import { Subject, throwError } from 'rxjs';
 import { map, debounceTime, distinctUntilChanged, switchMap, catchError } from 'rxjs/operators';
 
+
 declare var $: any;
+declare var L: any;
 @Component({
   selector: "app-add-geofence",
   templateUrl: "./add-geofence.component.html",
@@ -15,9 +19,22 @@ declare var $: any;
 })
 export class AddGeofenceComponent implements OnInit {
   title = "Add Geofence";
-  public getPolygonData :any;
+  geofenceData = {
+    geofence: {
+      type: '',
+      cords: []
+    }
+  };
+  public marker;
+  public map;
+  destinationLocation;
+  polygonData = [];
   showDestination = true;
   private readonly search: any;
+  public searchTerm = new Subject<string>();
+  public searchResults: any;
+  
+
   errors = {};
   form;
 
@@ -53,8 +70,8 @@ export class AddGeofenceComponent implements OnInit {
 
   ngOnInit() {
     // here maps initialization
-    this.leafMap.initGeoFenceMap();
-    //this.searchLocation();
+    this.initGeoFenceMap();
+    this.searchLocation()
 
     $(document).ready(() => {
       this.form = $("#form_").validate();
@@ -65,15 +82,79 @@ export class AddGeofenceComponent implements OnInit {
     });
   }
 
-  // initMap() {
-  //   if ($("#map-div").is(":visible")) {
-  //     $("#map-div").hide("slow");
-  //   } else {
-  //     $("#map-div").show("slow");
-  //   }
+  /**
+   * Initialize Leaflet map for addGeofencing Page
+   */
+  initGeoFenceMap = () => {
 
-  //   this.mapBoxService.initMapbox(-104.618896, 50.44521);
-  // }
+    const here = {
+      apiKey: environment.mapConfig.apiKey
+    }
+    const style = 'normal.night';
+	  
+    const hereTileUrl = `https://2.base.maps.ls.hereapi.com/maptile/2.1/trucktile/newest/${style}/{z}/{x}/{y}/512/png8?apiKey=${here.apiKey}&ppi=320&congestion=true`;
+    
+    const map = L.map('map', {
+      	center: [37.773972, -122.431297],
+      	zoom: 17,
+      	layers: [L.tileLayer(hereTileUrl)]
+	  });
+    //L.marker([37.773972, -122.431297]).addTo(map);
+
+    map.attributionControl.addAttribution('&copy; HERE 2020');
+    map.pm.addControls({
+      position: 'topleft',
+      drawCircle: false,
+      drawCircleMarker: false,
+      drawPolyline: false,
+      drawRectangle: false,
+      drawPolygon: true,
+      editMode: true,
+      dragMode: true,
+      cutPolygon: true,
+      removalMode: true,
+      drawMarker: false
+    });
+	  map.on('pm:create', (e) => {
+		  // alert('pm:create event fired. See console for details');
+		  const layer = e.layer;
+			
+			//console.log("lyr", layer)
+      var polyedit = layer.toGeoJSON();
+      this.geofenceData.geofence.type = polyedit.geometry.type;
+      this.geofenceData.geofence.cords = polyedit.geometry.coordinates;
+			
+			console.log("created",this.geofenceData);
+      
+      layer.on('pm:edit',({layer}) => {
+        
+        var polyedit = layer.toGeoJSON();
+        this.geofenceData.geofence.type = polyedit.geometry.type;
+        this.geofenceData.geofence.cords = polyedit.geometry.coordinates;
+
+        //console.log("edited",this.geofenceData);
+        
+      })
+      
+  });
+	
+	map.on('pm:cut', function (e) {
+		  console.log('cut event on map');
+		  //console.log(e);
+	});
+	map.on('pm:remove', function (e) {
+		  console.log('pm:remove event fired.');
+		  // alert('pm:remove event fired. See console for details');
+		  //console.log(e);
+  });
+  
+  this.map = map;
+  
+  
+	
+}
+
+  
   addCategory() {
     this.errors = {};
     this.hasError = false;
@@ -84,13 +165,11 @@ export class AddGeofenceComponent implements OnInit {
     console.log(data1);
   }
   addGeofence() {
+    console.log("data", this.polygonData);
+    console.log("geofence", this.geofenceData);
     this.errors = {};
     this.hasError = false;
     this.hasSuccess = false;
-
-    // if(!this.mapBoxService.plottedMap){
-    //   alert('Please draw the geofence'); return false;
-    // }
 
     const data = {
       fenceName: this.fenceName,
@@ -101,7 +180,7 @@ export class AddGeofenceComponent implements OnInit {
       //   latitude: this.mapBoxService.latitude,
       //   longitude: this.mapBoxService.longitude,
       // },
-      // geofence: this.mapBoxService.plottedMap || [],
+      //geofence: this.polygonData,
     };
     console.log(data);
     return;
@@ -148,8 +227,45 @@ export class AddGeofenceComponent implements OnInit {
     });
   }
 
+
+  public searchLocation() {
+    let target;
+    this.searchTerm.pipe(
+      map((e: any) => {
+        target = e;
+        return e.target.value;
+      }),
+      debounceTime(400),
+      distinctUntilChanged(),
+      switchMap(term => {
+        return this.leafMap.searchEntries(term);
+      }),
+      catchError((e) => {
+        return throwError(e);
+      }),
+    ).subscribe(res => {
+      console.log("res", res);
+      this.searchResults = res;
+      
+    });
+  }
+
+  searchDestination(loc,lat, lng){
+    this.destinationLocation = loc;
+    this.marker = L.marker([lat, lng]).addTo(this.map);
+    this.map.flyTo([lat, lng], 14, {
+        animate: true,
+        duration: 1.5
+    });
+    //this.map.removeLayer(this.marker)
+    
+  }
+  
+
+
   // Use this function while updating edit page of Geofence
-  loadExistingGeoFence=(geofence:[])=>{
+  loadExistingGeoFence =(geofence:[]) =>{
+    //console.log(this.polygonData)
     // code goes here
     // var polygonPoints = [
     //   [37.786617, -122.404654],
--- src/app/services/leaflet-map.service.ts
+++ src/app/services/leaflet-map.service.ts
@@ -1,94 +1,48 @@
 import { Injectable } from '@angular/core';
 import { environment } from './../../environments/environment';
-declare var L: any;
-
+declare var H: any;
 @Injectable({
   providedIn: 'root'
 })
 export class LeafletMapService {
-  public map;
-  public polygonData: any;
+  private platform: any;
+  private readonly apiKey = environment.mapConfig.apiKey;
   
   constructor() { }
 
   
-  /**
-   * Initialize Leaflet map for addGeofencing Page
-   */
-  initGeoFenceMap = () => {
-
-    const here = {
-      apiKey: environment.mapConfig.apiKey
-    }
-    const style = 'normal.night';
-	  
-    const hereTileUrl = `https://2.base.maps.ls.hereapi.com/maptile/2.1/trucktile/newest/${style}/{z}/{x}/{y}/512/png8?apiKey=${here.apiKey}&ppi=320&congestion=true`;
-    
-    const map = L.map('map', {
-      	center: [37.773972, -122.431297],
-      	zoom: 17,
-      	layers: [L.tileLayer(hereTileUrl)]
-	  });
-    
-    map.attributionControl.addAttribution('&copy; HERE 2020');
-    map.pm.addControls({
-      position: 'topleft',
-      drawCircle: true,
-      drawCircleMarker: false,
-      drawPolyline: false,
-      drawRectangle: false,
-      drawPolygon: true,
-      editMode: true,
-      dragMode: true,
-      cutPolygon: true,
-      removalMode: true,
-      drawMarker: false
-    });
-	  map.on('pm:create', function (e) {
-		  // alert('pm:create event fired. See console for details');
-		  const layer = e.layer;
-			
-			console.log("lyr", layer)
-      var coords = layer.getLatLngs();
-      this.polygonData = coords;
-			//var polyedit = layer.toGeoJSON();
-			console.log(coords);
-      //console.log(polyedit);
-      layer.on('pm:edit',({layer}) => {
-        console.log("lyr", layer)
-        var coords = layer.getLatLngs();
-        var polyedit = layer.toGeoJSON();
-        this.polygonData = coords;
-        console.log("dd",this.polygonData);
-        console.log("ddd",polyedit);
-      })
-      return this.polygonData;
-	});
-	
-	map.on('pm:cut', function (e) {
-		  console.log('cut event on map');
-		  console.log(e);
-	});
-	map.on('pm:remove', function (e) {
-		  console.log('pm:remove event fired.');
-		  // alert('pm:remove event fired. See console for details');
-		  console.log(e);
-  });
   
-  
-  var searchControl = L.esri.Geocoding.geosearch().addTo(map);
-
-  var results = L.layerGroup().addTo(map);
-
-  searchControl.on('results', function (data) {
-    console.log("data",data);
-    results.clearLayers();
-    for (var i = data.results.length - 1; i >= 0; i--) {
-      results.addLayer(L.marker(data.results[i].latlng));
-    }
+  /*
+  AutoSuggest Search Api v7
+*/ 
+searchLocation = async (query) => {
+  this.platform = new H.service.Platform({
+    'apikey': this.apiKey,
   });
-	
+  if(query != ''){
+    const service = this.platform.getSearchService();
+    const result = await service.geocode({ q: query });
+    if (result && result.items.length > 0) {
+      //console.log(result);
+      const response = await service.autosuggest(
+        {
+          at: `${result.items[0].position.lat},${result.items[0].position.lng}`,
+          limit: 5,
+          q: query
+        }
+      );
+      return response.items
+    }
+  }
+  
 }
+
+// returns the response
+public searchEntries(query) {
+  return this.searchLocation(query);
+}
+
+
 
 
 }
