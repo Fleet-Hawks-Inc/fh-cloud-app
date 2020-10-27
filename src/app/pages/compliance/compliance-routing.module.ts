1703d6c7db5516e89b16fd7356a07150208f1cf1
Gaurav Dhiman <gaurav@fleethawks.com>
2020-10-27 17:28:58 +0530

    inpsection added

--- src/app/pages/compliance/compliance-routing.module.ts
+++ src/app/pages/compliance/compliance-routing.module.ts
@@ -4,7 +4,6 @@ import { LogsComponent } from './hos/logs/logs.component';
 import { EditComponent } from './hos/edit/edit.component';
 import { DetailedComponent } from './hos/detailed/detailed.component';
 import { SummaryComponent } from './hos/summary/summary.component';
-import { DailyInspectionComponent } from './hos/daily-inspection/daily-inspection.component';
 import { InspectionDetailComponent } from './hos/inspection-detail/inspection-detail.component';
 import { DriverHosComponent } from './hos/driver-hos/driver-hos.component';
 import { MileageComponent } from './ifta/mileage/mileage.component';
@@ -21,7 +20,6 @@ const routes: Routes = [
       { path: 'detailed/:userName/:eventDate', component: DetailedComponent },
       { path: 'unidentified', component: UnidentifiedComponent },
       { path: 'summary', component: SummaryComponent },
-      { path: 'daily-inspection', component: DailyInspectionComponent },
       { path: 'inspection-detail', component: InspectionDetailComponent },
       { path: 'driver-hos', component: DriverHosComponent },
     ],
--- src/app/pages/compliance/compliance.module.ts
+++ src/app/pages/compliance/compliance.module.ts
@@ -8,7 +8,6 @@ import { EditComponent } from './hos/edit/edit.component';
 import { DetailedComponent } from './hos/detailed/detailed.component';
 import {NgbDateAdapter, NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { SummaryComponent } from './hos/summary/summary.component';
-import { DailyInspectionComponent } from './hos/daily-inspection/daily-inspection.component';
 import { InspectionDetailComponent } from './hos/inspection-detail/inspection-detail.component';
 import { DriverHosComponent } from './hos/driver-hos/driver-hos.component';
 import { MileageComponent } from './ifta/mileage/mileage.component';
@@ -32,7 +31,6 @@ import { NgSelect2Module } from 'ng-select2';
     EditComponent,
     DetailedComponent,
     SummaryComponent,
-    DailyInspectionComponent,
     InspectionDetailComponent,
     DriverHosComponent,
     MileageComponent,
