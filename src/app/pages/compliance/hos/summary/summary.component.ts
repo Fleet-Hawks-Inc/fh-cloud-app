fa4c7599dff2dcc638c77fa5e9092f4a1bc6b2cc
Gaurav Dhiman <gaurav@fleethawks.com>
2020-10-30 19:18:25 +0530

    filters added in current duty

--- src/app/pages/compliance/hos/summary/summary.component.ts
+++ src/app/pages/compliance/hos/summary/summary.component.ts
@@ -10,6 +10,10 @@ export class SummaryComponent implements OnInit {
   summary = [];
   userName = '';
 
+  duty = 'all';
+  SB = [];
+  ON = [];
+  OFF = [];
   constructor(private apiService: ApiService) {}
 
   ngOnInit() {
@@ -21,6 +25,30 @@ export class SummaryComponent implements OnInit {
       .getData(`eventLogs/HOSDriverSummary?userName=${this.userName}`)
       .subscribe((result: any) => {
         this.summary = result;
+        this.SB = this.summary.filter((el) => {
+          return el.HOSEventDescription == 'SB';
+        });
+
+        this.ON = this.summary.filter((el) => {
+          return el.HOSEventDescription == 'ON';
+        });
+
+        this.OFF = this.summary.filter((el) => {
+          return el.HOSEventDescription == 'OFF';
+        });
       });
+  }
+
+  toggleData(duty) {
+    console.log(duty);
+    if (duty == 'all') {
+      this.fetchSummary();
+    } else if (duty == 'SB') {
+      this.summary = this.SB;
+    } else if (duty == 'ON') {
+      this.summary = this.ON;
+    } else if (duty == 'OFF') {
+      this.summary = this.OFF;
+    }
   }
 }
