e5099d6d90304ff6180b7cdb5ce51ac0f202faec
amritpalsinghkhalsa <sandhu.amrit@live.in>
2020-10-31 22:21:10 +0530

    fixing route conflicts for all the branches

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -32,22 +32,22 @@ const routes: Routes = [
   },
   { path: 'healthcheck', component: HealthcheckComponent },
   // { path: 'Register', component: RegisterComponent },
-  {
-    path: 'fleet',
-    // canActivate: [AuthService],
-    // data: { roles: [Role.Admin] },
-    loadChildren: () => import('./pages/fleet/fleet.module').then((m) => m.FleetModule) ,
-    data: { preload: true }
-  },
-  {
-    path: 'compliance',
-    loadChildren: () => import('./pages/compliance/compliance.module').then((m) => m.ComplianceModule),
-   data: { preload: false }
-},
-  {
-    path: 'dispatch',
-    loadChildren: () => import('./pages/dispatch/dispatch.module').then((m) => m.DispatchModule), data: { preload: false }
-  },
+//   {
+//     path: 'fleet',
+//     // canActivate: [AuthService],
+//     // data: { roles: [Role.Admin] },
+//     loadChildren: () => import('./pages/fleet/fleet.module').then((m) => m.FleetModule) ,
+//     data: { preload: true }
+//   },
+//   {
+//     path: 'compliance',
+//     loadChildren: () => import('./pages/compliance/compliance.module').then((m) => m.ComplianceModule),
+//    data: { preload: false }
+// },
+//   {
+//     path: 'dispatch',
+//     loadChildren: () => import('./pages/dispatch/dispatch.module').then((m) => m.DispatchModule), data: { preload: false }
+//   },
   {
     path: 'accounts',
     loadChildren: () => import('./pages/accounts/accounts.module').then((m) => m.AccountsModule), data: { preload: false }
