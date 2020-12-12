08a27943b87d3219ccccda1d872e7f6c1c760917
Sandeep kaur <sandeep.kaur@fleethawks.com>
2020-12-12 23:28:53 +0530

    sandeep-dec-12-changes

--- src/app/pages/dispatch/permanent-routing/add-route/add-route.component.ts
+++ src/app/pages/dispatch/permanent-routing/add-route/add-route.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../../../services';
 import { Router, ActivatedRoute } from '@angular/router';
-import { map } from 'rxjs/operators';
-import { from } from 'rxjs';
+import { from, Subject, throwError } from 'rxjs';
 import { ToastrService } from 'ngx-toastr';
 import { AwsUploadService } from '../../../../services';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { NgbCalendar, NgbDateAdapter } from '@ng-bootstrap/ng-bootstrap';
 import { HereMapService } from '../../../../services';
-import { EventActivitiesService } from '../../../../services/event-activities.service';
+import { map, debounceTime, distinctUntilChanged, switchMap, catchError } from 'rxjs/operators';
+
 
 declare var $: any;
 
@@ -20,80 +20,33 @@ declare var $: any;
 export class AddRouteComponent implements OnInit {
 
   pageTitle: string;
-  errors = {};
+  public searchResults: any;
+  public searchResults1: any;
+  private readonly search: any;
+  public searchTerm = new Subject<string>();
+  public searchTerm1 = new Subject<string>();
+  mapVisible = false;
+  errors: {};
   routeData = {
-    sourceInformation: {
-      recurring: {
-        recurringRoute: '',
-        recurringType: '',
-        recurringDate: '',
-        sunday: false,
-        monday: false,
-        tuesday: false,
-        wednesday: false,
-        thursday: false,
-        friday: false,
-        saturday: false
-      }
+    coDriverUserName: '',
+    sourceInformation: {},
+    recurring: {
+      recurringRoute: false,
+      recurringType: '',
+      recurringDate: '',
+      sunday: false,
+      monday: false,
+      tuesday: false,
+      wednesday: false,
+      thursday: false,
+      friday: false,
+      saturday: false
     },
-    destinationInformation: {
-      destinationLocationID: '',
-      destinationAddress: '',
-      destinationCountryID: '',
-      destinationStateID: '',
-      destinationCityID: '',
-      destinationZipCode: '',
-      stop: {
-        destinationStop: '',
-        stopLocation: '',
-        stopNotes: ''
-      }
-    },
+    destinationInformation: {},
+    stops: []
   };
   form;
-
-  // routeNo = '';
-  // routeName = '';
-  // notes = '';
-  // VehicleID = '';
-  // AssetID = '';
-  // miles = '';
-  // driverUserName = '';
-  // coDriverUserName = '';
-  // sourceInformation = {
-  //   sourceLocationID: '',
-  //   sourceAddress: '',
-  //   sourceCountryID: '',
-  //   sourceStateID: '',
-  //   sourceCityID: '',
-  //   sourceZipCode: '',
-  //   recurring: {
-  //     recurringRoute: '',
-  //     recurringType: '',
-  //     recurringDate: '',
-  //     sunday: '',
-  //     monday: '',
-  //     tuesday: '',
-  //     wednesday: '',
-  //     thursday: '',
-  //     friday: '',
-  //     saturday: ''
-
-  //   }
-  // };
-  // destinationInformation = {
-  //   destinationLocationID: '',
-  //   destinationAddress: '',
-  //   destinationCountryID: '',
-  //   destinationStateID: '',
-  //   destinationCityID: '',
-  //   destinationZipCode: '',
-  //   stop: {
-  //     destinationStop: '',
-  //     stopLocation: '',
-  //     stopNotes: ''
-  //   }
-  // };
+  newCoords = [];
 
   vehicles = [];
   assets = [];
@@ -112,7 +65,7 @@ export class AddRouteComponent implements OnInit {
   Error = '';
   Success = '';
   mapView = false;
-
+  stopsData = [];
   locations = [
     {
       locationID: '1',
@@ -132,32 +85,29 @@ export class AddRouteComponent implements OnInit {
     },
   ];
 
-  activityData = {
-    action: '',
-    userID: '',
-    tableName: '',
-    eventID: '',
-    message: ""
-  };
+  new_length = 0;
 
-  constructor(private apiService: ApiService, private awsUS: AwsUploadService, private route: ActivatedRoute,
+  constructor(private apiService: ApiService,
+    private awsUS: AwsUploadService, private route: ActivatedRoute,
     private router: Router, private toastr: ToastrService, private spinner: NgxSpinnerService, private ngbCalendar: NgbCalendar,
-    private dateAdapter: NgbDateAdapter<string>, private hereMap: HereMapService, private EventActivity: EventActivitiesService) {
+    private dateAdapter: NgbDateAdapter<string>, private hereMap: HereMapService) {
   }
 
   get today() {
     return this.dateAdapter.toModel(this.ngbCalendar.getToday())!;
   }
 
+
+
   ngOnInit() {
 
     this.pageTitle = 'Add Route';
 
-    this.fetchCountries();
+    // this.fetchCountries();
     this.fetchVehicles();
     this.fetchAssets();
     this.fetchDrivers();
-    // this.mapShow();
+    this.searchLocation();
 
     $(document).ready(() => {
       this.form = $('#form_').validate();
@@ -173,33 +123,31 @@ export class AddRouteComponent implements OnInit {
       }
     });
 
-    $('#addStop').on('click', function () {
-      // alert('2');
-    });
 
     var thiss = this;
-    $('.countrySelect').on('change', function () {
-      var curr = $(this);
-      var countryId = curr.val();
-      var countryType = curr.closest('select').attr('type');
-      thiss.getStates(countryId, countryType);
-    })
+    // $('.countrySelect').on('change', function () {
+    //   var curr = $(this);
+    //   var countryId = curr.val();
+    //   var countryType = curr.closest('select').attr('type');
+    //   thiss.getStates(countryId, countryType);
+    // })
 
     $('#routeCheckBtn').on('click', function () {
       if (this.checked == true) {
         $('#routeMapDiv').css('display', 'flex');
         thiss.mapShow();
+        //this.getCoords(this.routeData.stops);
       } else {
         $('#routeMapDiv').css('display', 'none');
       }
     })
 
-    $('.stateSelect').on('change', function () {
-      let curr = $(this);
-      let stateId = curr.val();
-      let stateType = curr.closest('select').attr('type');
-      thiss.getCities(stateId, stateType);
-    })
+    // $('.stateSelect').on('change', function () {
+    //   let curr = $(this);
+    //   let stateId = curr.val();
+    //   let stateType = curr.closest('select').attr('type');
+    //   thiss.getCities(stateId, stateType);
+    // })
 
     $('.reccRoute').on('click', function () {
       // alert('redd');
@@ -208,13 +156,66 @@ export class AddRouteComponent implements OnInit {
     })
   }
 
-  fetchCountries() {
-    this.apiService.getData('countries')
-      .subscribe((result: any) => {
-        this.countries = result.Items;
-        // console.log(this.countries)
-      });
+  eventCheck(event) {
+    const value = event.target.checked;
+    if (value === true) {
+      this.mapVisible = true;
+      this.mapShow();
+      if (this.routeData.stops.length > 1) {
+        this.getCoords(this.routeData.stops);
+      }
+    } else {
+      this.mapVisible = false;
+    }
   }
+
+  public searchLocation() {
+    let target;
+    this.searchTerm.pipe(
+      map((e: any) => {
+        $('.map-search__results').hide();
+        $(e.target).closest('div').addClass('show-search__result');
+        target = e;
+        return e.target.value;
+      }),
+      debounceTime(400),
+      distinctUntilChanged(),
+      switchMap(term => {
+        return this.hereMap.searchEntries(term);
+      }),
+      catchError((e) => {
+        return throwError(e);
+      }),
+    ).subscribe(res => {
+      this.searchResults = res;
+    });
+  }
+
+
+
+  /**
+   * pass trips coords to show on the map
+   * @param data
+   */
+  async getCoords(data) {
+    this.spinner.show();
+    await Promise.all(data.map(async item => {
+      let result = await this.hereMap.geoCode(item.stopName);
+      console.log('result', result);
+      this.newCoords.push(`${result.items[0].position.lat},${result.items[0].position.lng}`)
+    }));
+    this.hereMap.calculateRoute(this.newCoords);
+    this.spinner.hide();
+    this.newCoords = [];
+  }
+
+  // fetchCountries() {
+  //   this.apiService.getData('countries')
+  //     .subscribe((result: any) => {
+  //       this.countries = result.Items;
+  //       // console.log(this.countries)
+  //     });
+  // }
 
   fetchVehicles() {
     this.apiService.getData('vehicles')
@@ -237,37 +238,16 @@ export class AddRouteComponent implements OnInit {
     this.apiService.getData('drivers')
       .subscribe((result: any) => {
         this.drivers = result.Items;
-        console.log('this.drivers');
-        console.log(this.drivers);
+        this.coDrivers = result.Items;
+        // console.log('this.drivers');
+        // console.log(this.drivers);
       })
   }
 
-  getStates(countryID, countryType) {
-    this.spinner.show();
-    this.apiService.getData('states/country/' + countryID)
-      .subscribe((result: any) => {
-        if (countryType === 'source') {
-          this.sourceStates = result.Items;
-        } else if (countryType === 'destination') {
-          this.destinationStates = result.Items;
-        }
-        this.spinner.hide();
-        // console.log('this.states', result.Items)
-      });
-  }
-
-  getCities(stateID, stateType) {
-    this.spinner.show();
-    this.apiService.getData('cities/state/' + stateID)
-      .subscribe((result: any) => {
-        if (stateType === 'source') {
-          this.sourceCities = result.Items;
-        } else if (stateType === 'destination') {
-          this.destinationCities = result.Items;
-        }
-        this.spinner.hide();
-        // console.log('this.states', result.Items)
-      });
+  resetCodrivers(event) {
+    $("#codrvr option").show();
+    this.routeData.coDriverUserName = '';
+    $("#codrvr option[value='"+event.target.value+"']").hide();
   }
 
   mapShow() {
@@ -279,16 +259,25 @@ export class AddRouteComponent implements OnInit {
 
 
   addRoute() {
-    this.hideErrors();
     // console.log(this.routeData);
 
-    if (this.routeData.sourceInformation.recurring.recurringType !== '') {
-      if (this.routeData.sourceInformation.recurring.recurringType === 'weekly') {
+    if (this.routeData.recurring.recurringRoute === true) {
+      if(this.routeData.recurring.recurringType == '') {
+        this.toastr.error('Please select recurring type');
+        return false;
+      }
+
+      if ($('input.daysChecked:checked').length == 0) { 
+        this.toastr.error('Please select day and date of recurring');
+        return false;
+      }
+
+      if (this.routeData.recurring.recurringType === 'weekly') {
         if ($('input.daysChecked:checked').length > 1) {
           this.toastr.error('Please select a single day for weekly recurring route');
           return false;
         }
-      } else if (this.routeData.sourceInformation.recurring.recurringType === 'biweekly') {
+      } else if (this.routeData.recurring.recurringType === 'biweekly') {
         if ($('input.daysChecked:checked').length > 2) {
           this.toastr.error('Please select only two days for biweekly recurring route');
           return false;
@@ -300,7 +289,7 @@ export class AddRouteComponent implements OnInit {
     this.errors = {};
     this.hasError = false;
     this.hasSuccess = false;
-
+    console.log("this.routeData", this.routeData);
     this.apiService.postData('routes', this.routeData).subscribe({
       complete: () => {
       },
@@ -308,8 +297,12 @@ export class AddRouteComponent implements OnInit {
         from(err.error)
           .pipe(
             map((val: any) => {
+              const path = val.path;
+              // We Can Use This Method
+              const key = val.message.match(/"([^']+)"/)[1];
+              console.log(key);
               val.message = val.message.replace(/".*"/, 'This Field');
-              this.errors[val.context.key] = val.message;
+              this.errors[key] = val.message;
             })
           )
           .subscribe({
@@ -326,7 +319,6 @@ export class AddRouteComponent implements OnInit {
       next: (res) => {
         this.spinner.hide();
         this.response = res;
-        this.addActivityLog();
         this.toastr.success('Route added successfully');
         this.router.navigateByUrl('/dispatch/routes/route-list');
       },
@@ -335,36 +327,120 @@ export class AddRouteComponent implements OnInit {
 
   throwErrors() {
     console.log(this.errors);
-    from(Object.keys(this.errors))
-      .subscribe((v) => {
-        $('[name="' + v + '"]')
-          .after('<label id="' + v + '-error" class="error" for="' + v + '">' + this.errors[v] + '</label>')
-          .addClass('error')
-      });
+    this.form.showErrors(this.errors);
   }
 
-  hideErrors() {
-    from(Object.keys(this.errors))
-      .subscribe((v) => {
-        $('[name="' + v + '"]')
-          .removeClass('error')
-          .next()
-          .remove('label')
-      });
-    this.errors = {};
-  }
-
-  addActivityLog() {
-    this.activityData = {
-      action: 'add',
-      userID: '1',
-      tableName: 'serviceroutes',
-      eventID: '',
-      message: "added a route"
+  async assignLocation(elem, label) {
+    console.log("dff", elem, label);
+    const result = await this.hereMap.geoCode(label);
+    const labelResult = result.items[0];
+    console.log("labelResult", labelResult);
+    const item = {
+      stopName: label,
+      stopNotes: ''
     };
-    this.EventActivity.addEventActivity(this.activityData);
+    if (elem === 'source') {
+
+      // this.routeData.sourceInformation['sourceAddress'] =
+      //   `${labelResult.title} ${labelResult.address.houseNumber} ${labelResult.address.street}`;
+      this.routeData.sourceInformation['sourceAddress'] = '';
+      this.routeData.sourceInformation['sourceCountry'] = '';
+      this.routeData.sourceInformation['sourceState'] = '';
+      this.routeData.sourceInformation['sourceCity'] = '';
+      this.routeData.sourceInformation['sourceZipCode'] = '';
+
+      this.routeData.sourceInformation['sourceAddress'] = `${labelResult.title}` ;
+      if(labelResult.address.houseNumber !== undefined) {
+        this.routeData.sourceInformation['sourceAddress'] += `${labelResult.address.houseNumber}`;
+      }
+
+      if(labelResult.address.street !== undefined) {
+        this.routeData.sourceInformation['sourceAddress'] += `${labelResult.address.street}`;
+      }
+      
+      if(labelResult.address.countryName !== undefined) {
+        this.routeData.sourceInformation['sourceCountry'] = `${labelResult.address.countryName}`;
+      }
+
+      if(labelResult.address.state !== undefined) {
+        this.routeData.sourceInformation['sourceState'] = `${labelResult.address.state} (${labelResult.address.stateCode})`;
+      }
+
+      if(labelResult.address.city !== undefined) {
+        this.routeData.sourceInformation['sourceCity'] = `${labelResult.address.city}`;
+      }
+      
+      if(labelResult.address.postalCode !== undefined) {
+        this.routeData.sourceInformation['sourceZipCode'] = `${labelResult.address.postalCode}`;
+      }
+      
+      this.routeData.stops.splice(1, 0, item);
+    } else {
+      this.routeData.destinationInformation['destinationAddress'] = '';
+      this.routeData.destinationInformation['destinationCountry'] = '';
+      this.routeData.destinationInformation['destinationState'] = '';
+      this.routeData.destinationInformation['destinationCity'] = '';
+      this.routeData.destinationInformation['destinationZipCode'] = '';
+
+      // this.routeData.destinationInformation['destinationAddress'] =
+      //   `${labelResult.title} ${labelResult.address.houseNumber} ${labelResult.address.street}`;
+
+      this.routeData.destinationInformation['destinationAddress'] = `${labelResult.title}`;
+
+      if(labelResult.address.houseNumber !== undefined) {
+        this.routeData.destinationInformation['destinationAddress'] += `${labelResult.address.houseNumber}`;
+      }
+
+      if(labelResult.address.street !== undefined) {
+        this.routeData.destinationInformation['destinationAddress'] += `${labelResult.address.street}`;
+      }
+      
+      if(labelResult.address.countryName !== undefined) {
+        this.routeData.destinationInformation['destinationCountry'] = `${labelResult.address.countryName}`;
+      }
+
+      if(labelResult.address.state !== undefined) {
+        this.routeData.destinationInformation['destinationState'] = `${labelResult.address.state} (${labelResult.address.stateCode})`;
+      }
+      
+      if(labelResult.address.city !== undefined) {
+        this.routeData.destinationInformation['destinationCity'] = `${labelResult.address.city}`;
+      }
+      
+      if(labelResult.address.postalCode !== undefined) {
+        this.routeData.destinationInformation['destinationZipCode'] = `${labelResult.address.postalCode}`;
+      }
+      
+      this.routeData.stops.splice(1, 1);
+      this.routeData.stops.splice(1, 0, item);
+    }
+    // console.log('this.stopsData', this.routeData.stops);
+    this.searchResults = false;
+    $('div').removeClass('show-search__result');
+
   }
 
+  addStops() {
+    const item = {
+      stopName: '',
+      stopNotes: ''
+    };
+    const length = this.routeData.stops.length;
+    this.routeData.stops.splice(length - 1, 0, item);
+    if (length > 2) {
+      console.log('length if', length);
+      this.routeData.stops[this.routeData.stops.length - 1].stopName = this.routeData.destinationInformation['destinationAddress'];
+    } else {
+      console.log('length else', length);
+      this.routeData.stops[length].stopName = this.routeData.destinationInformation['destinationAddress'];
+    }
+
+    console.log('this.routeData.stops', this.routeData.stops);
+  }
+
+  removeStops(i) {
+    this.routeData.stops.splice(i, 0);
+  }
 
 }
 
