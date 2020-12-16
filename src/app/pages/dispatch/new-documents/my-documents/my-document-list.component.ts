6f1899dfc20360d85bc3cae01c4e2bfcf4e8129c
Sandeep kaur <sandeep.kaur@fleethawks.com>
2020-12-16 23:44:29 +0530

    feat: pagination added in assets-reminders-company documents

--- src/app/pages/dispatch/new-documents/my-documents/my-document-list.component.ts
+++ src/app/pages/dispatch/new-documents/my-documents/my-document-list.component.ts
@@ -8,13 +8,23 @@ import { v4 as uuidv4 } from 'uuid';
 import { DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
 import { Auth } from 'aws-amplify';
 declare var $: any;
+import { AfterViewInit, OnDestroy, ViewChild } from '@angular/core';
+import { DataTableDirective } from 'angular-datatables';
+import * as moment from "moment";
 
 @Component({
   selector: 'app-my-document-list',
   templateUrl: './my-document-list.component.html',
   styleUrls: ['./my-document-list.component.css']
 })
-export class MyDocumentListComponent implements OnInit {
+export class MyDocumentListComponent implements AfterViewInit, OnDestroy, OnInit {
+
+  @ViewChild(DataTableDirective, { static: false })
+  dtElement: DataTableDirective;
+
+  dtOptions: DataTables.Settings = {};
+  dtTrigger: Subject<any> = new Subject();
+
   public documents = [];
   trips;
   currentUser;
@@ -25,7 +35,6 @@ export class MyDocumentListComponent implements OnInit {
   docs: SafeResourceUrl;
   public documentsDocs = [];
   selectedFiles: FileList;
-  private dtTrigger: Subject<any> = new Subject();
   selectedFileNames: Map<any, any>;
   documentMode: string = 'Manual';
   documentPrefix: string;
@@ -41,6 +50,21 @@ export class MyDocumentListComponent implements OnInit {
   documentData = {
     uploadedDocs: []
   };
+
+  totalRecords = 20;
+  pageLength = 10;
+  serviceUrl = '';
+  filterValues = {
+    searchValue: '',
+    startDate: '',
+    endDate: '',
+    start: <any> '',
+    end: <any> ''
+  };
+  lastEvaluated = {
+    value1: '',
+  };
+
   constructor(
     private apiService: ApiService,
     private router: Router,
@@ -53,6 +77,7 @@ export class MyDocumentListComponent implements OnInit {
   ngOnInit() {
     this.fetchDocuments();
     this.getCurrentuser();
+    this.initDataTable();
     $(document).ready(() => {
       this.form = $('#form_').validate();
     });
@@ -60,13 +85,15 @@ export class MyDocumentListComponent implements OnInit {
 
   fetchDocuments = () => {
     // this.spinner.show(); // loader init
+    this.totalRecords = 0;
     this.apiService.getData('documents').subscribe({
       complete: () => {},
       error: () => {},
       next: (result: any) => {
         for (let i = 0; i < result.Items.length; i++) {
           if (result.Items[i].isDeleted === 0) {
-            this.documents.push(result.Items[i]);
+            // this.documents.push(result.Items[i]);
+            this.totalRecords += 1 ;
           }
         }
         // this.spinner.hide(); // loader hide
@@ -261,5 +288,107 @@ export class MyDocumentListComponent implements OnInit {
         this.fetchDocuments();
       });
     }
+  }
+
+  initDataTable() {
+    let current = this;
+    this.dtOptions = { // All list options
+      pagingType: 'full_numbers',
+      pageLength: current.pageLength,
+      serverSide: true,
+      processing: true,
+      order: [],
+      columnDefs: [ //sortable false
+        {"targets": [0],"orderable": false},
+        {"targets": [1],"orderable": false},
+        {"targets": [2],"orderable": false},
+        {"targets": [3],"orderable": false},
+        {"targets": [4],"orderable": false},
+        {"targets": [5],"orderable": false},
+        {"targets": [6],"orderable": false},
+        {"targets": [7],"orderable": false},
+      ],
+      dom: 'lrtip',
+      ajax: (dataTablesParameters: any, callback) => {
+        current.apiService.getDatatablePostData('documents/fetch-records?value1=' + current.lastEvaluated.value1 +
+          '&searchValue=' + this.filterValues.searchValue + "&from=" + this.filterValues.start + 
+          "&to=" + this.filterValues.end, dataTablesParameters).subscribe(resp => {
+            // current.fetchTrips(resp)
+            current.documents = resp['Items'];
+            // console.log(resp)
+            if (resp['LastEvaluatedKey'] !== undefined) {
+              current.lastEvaluated = {
+                value1: resp['LastEvaluatedKey'].docID,
+              }
+            } else {
+              current.lastEvaluated = {
+                value1: '',
+              }
+            }
+
+            callback({
+              recordsTotal: current.totalRecords,
+              recordsFiltered: current.totalRecords,
+              data: []
+            });
+          });
+      }
+    };
+  }
+
+  ngAfterViewInit(): void {
+    this.dtTrigger.next();
+  }
+
+  ngOnDestroy(): void {
+    // Do not forget to unsubscribe the event
+    this.dtTrigger.unsubscribe();
+  }
+
+  rerender(): void {
+    this.dtElement.dtInstance.then((dtInstance: DataTables.Api) => {
+      // Destroy the table first
+      dtInstance.destroy();
+      // Call the dtTrigger to rerender again
+      this.dtTrigger.next();
+    });
+  }
+
+  searchFilter() {
+    if(this.filterValues.startDate !== '' || this.filterValues.endDate !== '') {
+      if(this.filterValues.startDate !== '') {
+        let start = this.filterValues.startDate.split('-').reverse().join('-');
+        this.filterValues.start = moment(start+' 00:00:01').format("X");
+        this.filterValues.start = this.filterValues.start*1000;
+      }
+      if(this.filterValues.endDate !== '') {
+        let end = this.filterValues.endDate.split('-').reverse().join('-');
+        this.filterValues.end = moment(end+' 23:59:59').format("X");
+        this.filterValues.end = this.filterValues.end*1000;
+      }
+      this.pageLength = this.totalRecords;
+      this.rerender();
+    } else {
+      return false;
+    }
+  }
+
+  resetFilter() {
+    if(this.filterValues.startDate !== '' || this.filterValues.endDate !== '') {
+      // this.spinner.show();
+      this.filterValues = {
+        searchValue: '',
+        startDate: '',
+        endDate: '',
+        start: <any> '',
+        end: <any> ''
+      };
+      this.pageLength = 10;
+      this.rerender();
+      // this.spinner.hide();
+    } else {
+      return false;
+    }
+    
   }
 }
