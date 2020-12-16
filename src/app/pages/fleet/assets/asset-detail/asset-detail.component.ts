7fd88890803ee95cf7f1fccd473a59ed1f1f4bcc
Daljit-fleetHawks <67137586+Daljit-fleetHawks@users.noreply.github.com>
2020-12-16 14:57:30 +0530

    Update asset-detail.component.ts

--- src/app/pages/fleet/assets/asset-detail/asset-detail.component.ts
+++ src/app/pages/fleet/assets/asset-detail/asset-detail.component.ts
@@ -155,7 +155,7 @@ export class AssetDetailComponent implements OnInit {
           this.spinner.hide(); // loader hide
         }
       }, (err) => {
-        console.log('asset detail', err);
+        
       });
   }
 
@@ -167,7 +167,7 @@ export class AssetDetailComponent implements OnInit {
           this.deviceData = result['Items'];
         }
       }, (err) => {
-        console.log('asset detail', err);
+       
       });
   }
 
