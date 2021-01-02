6d20eac27259516326bd417ba8c8de3d7642b6cc
Daljit-fleetHawks <67137586+Daljit-fleetHawks@users.noreply.github.com>
2021-01-02 22:17:44 +0530

    Update vehicles.module.ts

--- src/app/pages/fleet/vehicles/vehicles.module.ts
+++ src/app/pages/fleet/vehicles/vehicles.module.ts
@@ -24,7 +24,7 @@ const routes: Routes = [
   declarations: [
 
     // AddVehicleNewComponent,
-    EditVehicleNewComponent,
+    // EditVehicleNewComponent,
 
     VehicleListComponent,
     VehicleDetailComponent,
