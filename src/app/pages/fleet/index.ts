d0bf61c88c9af890d61805b1d40edecb08311ee2
Daljit-fleetHawks <67137586+Daljit-fleetHawks@users.noreply.github.com>
2021-01-02 22:11:03 +0530

    Update index.ts

--- src/app/pages/fleet/index.ts
+++ src/app/pages/fleet/index.ts
@@ -1,6 +1,6 @@
 
 // export * from './vehicles/add-vehicle-new/add-vehicle-new.component';
-export * from './vehicles/edit-vehicle-new/edit-vehicle-new.component';
+// export * from './vehicles/edit-vehicle-new/edit-vehicle-new.component';
 
 export * from './vehicles/vehicle-list/vehicle-list.component';
 
