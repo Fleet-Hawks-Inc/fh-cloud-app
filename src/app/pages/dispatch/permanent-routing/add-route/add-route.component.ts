bab2df317c921e978fc6b356d420175633778f07
FH-sandeepkaur1 <72725109+FH-sandeepkaur1@users.noreply.github.com>
2021-01-18 15:44:01 +0530

    Update add-route.component.ts

--- src/app/pages/dispatch/permanent-routing/add-route/add-route.component.ts
+++ src/app/pages/dispatch/permanent-routing/add-route/add-route.component.ts
@@ -261,7 +261,7 @@ export class AddRouteComponent implements OnInit {
       next: (res) => {
         this.spinner.hide();
         this.response = res;
-        this.toastr.success('Route added successfully');
+        this.toastr.success('Route added successfully.');
         this.router.navigateByUrl('/dispatch/routes/list');
       },
     });
@@ -472,26 +472,27 @@ export class AddRouteComponent implements OnInit {
 
   updateRoute() {
     this.hasError = false;
-    this.hasSuccess = false;
+    this.has
+      = false;
     if (this.routeData.recurring.recurringRoute === true) {
       if(this.routeData.recurring.recurringType == '') {
-        this.toastr.error('Please select recurring type');
+        this.toastr.error('Please select recurring type.');
         return false;
       }
 
       if ($('input.daysChecked:checked').length == 0) { 
-        this.toastr.error('Please select day and date of recurring');
+        this.toastr.error('Please select day and date of recurring.');
         return false;
       }
 
       if (this.routeData.recurring.recurringType === 'weekly') {
         if ($('input.daysChecked:checked').length > 1) {
-          this.toastr.error('Please select a single day for weekly recurring route');
+          this.toastr.error('Please select a single day for weekly recurring route.');
           return false;
         }
       } else if (this.routeData.recurring.recurringType === 'biweekly') {
         if ($('input.daysChecked:checked').length > 2) {
-          this.toastr.error('Please select only two days for biweekly recurring route');
+          this.toastr.error('Please select only two days for biweekly recurring route.');
           return false;
         }
       }
@@ -530,7 +531,7 @@ export class AddRouteComponent implements OnInit {
         this.hasSuccess = true;
         this.spinner.hide();
         this.router.navigateByUrl('/dispatch/routes/list');
-        this.toastr.success('Route updated successfully');
+        this.toastr.success('Route updated successfully.');
       },
     })
   }
