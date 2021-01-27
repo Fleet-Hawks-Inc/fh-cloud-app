b445e50f6295292e1e0bdb5a5cdffc3340bc2cda
Gaurav <gaurav@fleethawks.com>
2021-01-27 21:54:51 +0530

    feedbacks updated

--- src/app/services/list.service.ts
+++ src/app/services/list.service.ts
@@ -9,6 +9,18 @@ export class ListService {
   vendorDataSource: BehaviorSubject<Array<any>> = new BehaviorSubject([]);
   vendorList = this.vendorDataSource.asObservable();
 
+  manufacturerDataSource: BehaviorSubject<Array<any>> = new BehaviorSubject([]);
+  manufacturerList = this.manufacturerDataSource.asObservable();
+
+  modelDataSource: BehaviorSubject<Array<any>> = new BehaviorSubject([]);
+  modelList = this.modelDataSource.asObservable();
+
+  countryDataSource: BehaviorSubject<Array<any>> = new BehaviorSubject([]);
+  countryList = this.countryDataSource.asObservable();
+
+  stateDataSource: BehaviorSubject<Array<any>> = new BehaviorSubject([]);
+  stateList = this.stateDataSource.asObservable();
+
   constructor(private apiService: ApiService) {}
 
   fetchVendors() {
@@ -16,5 +28,33 @@ export class ListService {
       console.log('in new service');
       this.vendorDataSource.next(result.Items);
     });
+  }
+
+  fetchManufacturers() {
+    this.apiService.getData('manufacturers').subscribe((result: any) => {
+      this.manufacturerDataSource.next(result.Items);
+    });
+  }
+
+  fetchCountries() {
+    this.apiService.getData('countries').subscribe((result: any) => {
+      this.countryDataSource.next(result.Items);
+    });
+  }
+
+  fetchModels() {
+    this.apiService
+      .getData(`vehicleModels`)
+      .subscribe((result: any) => {
+        this.modelDataSource.next(result.Items);
+      });
+  }
+
+  fetchStates() {
+    this.apiService
+      .getData(`states`)
+      .subscribe((result: any) => {
+        this.stateDataSource.next(result.Items);
+      });
   }
 }
