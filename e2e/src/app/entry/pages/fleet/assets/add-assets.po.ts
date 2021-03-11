720cc3678af403c33a70b2bfe157cc8a2d271357
Hardeep <hardeep@fleethawks.com>
2021-03-11 17:12:57 +0530

    updated code

---
+++ e2e/src/app/entry/pages/fleet/assets/add-assets.po.ts
@@ -0,0 +1,13 @@
+// import { browser, by, element, ElementFinder, ExpectedConditions } from 'protractor';
+//  export class AddAssetPage {
+//      navigateToAssetPage(){
+//         let browserModule=browser.get("#/fleet/assets/add");
+//      }
+// //--------------------------Basic details------------------//
+//      basicDetailsAsset(){
+//          let assetNameNumber=
+//         //let vehicleIdentificationNumber=
+//          //let group=
+//          //let sttartda
+//      }
+//  }
