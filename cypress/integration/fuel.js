89821169ea3c7c6fb4044412a67617c3fbbb2119
nameet jambla <nameet@fleethawks.com>
2021-04-21 18:12:16 +0530

    changes in add asset and updated fuel module

---
+++ cypress/integration/fuel.js
@@ -0,0 +1,73 @@
+const { type } = require("jquery");
+
+describe("Fuel Test", function () {
+    it('should allow user to add Fuel Entry', function () {
+        cy.visit('http://localhost:4200/');
+        cy.get(':nth-child(1) > .input-group > .form-control').clear();
+        cy.get(':nth-child(1) > .input-group > .form-control').type('e2etestcarrier');
+        cy.get(':nth-child(2) > .input-group > .form-control').clear();
+        cy.get(':nth-child(2) > .input-group > .form-control').type('FleetHawks@2502');
+        cy.get('#btnsubmit').click();
+        cy.get('.ng-star-inserted > .nav > :nth-child(5) > .nav-link').click();
+        cy.get('#form_ > .row > .text-right > .btn').click();
+        cy.get('div#fuelVehicle > .col-lg-10 > ng-select[role="listbox"] input[role="combobox"]').first().click();
+        cy.get('div:nth-of-type(3) > .ng-option-label.ng-star-inserted').last().click();//Basic Details
+        cy.get('input[name="fuelQty"]').clear();
+        cy.get('input[name="fuelQty"]').type('15');//Fuel Quantity
+        cy.get('input[name="fuelQtyAmt"]').clear();
+        cy.get('input[name="fuelQtyAmt"]').type('50');//Fuel Amount
+        cy.get('input[name="DEFFuelQty"]').clear();
+        cy.get('input[name="DEFFuelQty"]').type('15');//DEF Fuel Quantity(Diesel Exaust Fuel)
+        cy.get('input[name="DEFFuelQtyAmt"]').clear();
+        cy.get('input[name="DEFFuelQtyAmt"]').type('50');
+        cy.get('input[name="fuelData.totalAmount"]').clear();
+        cy.get('input[name="fuelData.totalAmount"]').type('30');//Subtotal
+        cy.get('div:nth-of-type(6) > .col-lg-6 > ng-select[role="listbox"] input[role="combobox"]').first().click();
+        cy.get('div:nth-of-type(3) > .ng-option-label.ng-star-inserted').last().click();//discount type
+        cy.get('input[name="discAmount"]').clear();
+        cy.get('input[name="discAmount"]').type('10');//discount amount
+        cy.get('.ng-star-inserted.row > .col-lg-6 > ng-select[role="listbox"] input[role="combobox"]').first().click();
+        cy.get('div:nth-of-type(3) > .ng-option-label.ng-star-inserted').last().click();//Tax Type
+        cy.get('input[name="tax[0].taxAmount"]').clear();
+        cy.get('input[name="tax[0].taxAmount"]').type('10');//Tax Amount
+        cy.get('input[name="fuelData.pricePerUnit"]').clear();
+        cy.get('input[name="fuelData.pricePerUnit"]').type('5');//Cost Per litre
+        cy.get('input[name="fuelData.amountPaid"]').clear();
+        cy.get('input[name="fuelData.amountPaid"]').type('12');//Amount paid
+        cy.get('input[name="fuelDate"]').first().click();
+        cy.get('div:nth-of-type(3) > div:nth-of-type(4) > .btn-light.ng-star-inserted').last().click();//Fuel Date
+        cy.get('input[name="fuelTime"]').type('10:30');//fuel time
+        cy.get('[name="fuelType"] input').first().click();
+        cy.get('div:nth-of-type(1) > .ng-option-label.ng-star-inserted').last().click();// fuel type
+        cy.get('form#fuelForm > div:nth-of-type(2) > div:nth-of-type(2) > div > div:nth-of-type(1) > ng-select[role="listbox"] input[role="combobox"]').first().click();
+        cy.get('div:nth-of-type(1) > .ng-option-label.ng-star-inserted').last().click();//Paid By(Driver)
+        cy.get('div:nth-of-type(2) > div:nth-of-type(2) > div > div:nth-of-type(2) > ng-select[role="listbox"] input[role="combobox"]').first().click();
+        cy.get('div:nth-of-type(1) > .ng-option-label.ng-star-inserted').last().click();//Payment Mode
+        cy.get('input[name="reference"]').type('FP1234');
+        cy.get('div:nth-of-type(3) > div:nth-of-type(2) > div:nth-of-type(1) > .col-lg-10 > ng-select[role="listbox"] input[role="combobox"]').first().click();
+        cy.get('div:nth-of-type(36) > .ng-option-label.ng-star-inserted').last().click();//Country
+        cy.get('div:nth-of-type(3) > div:nth-of-type(2) > div:nth-of-type(2) > .col-lg-10 > ng-select[role="listbox"] input[role="combobox"]').first().click();
+        cy.get('div:nth-of-type(2) > .ng-option-label.ng-star-inserted').last().click();//province
+        cy.get('div:nth-of-type(3) > div:nth-of-type(1) > .col-lg-10 > ng-select[role="listbox"] input[role="combobox"]').first().click();
+        cy.get('div:nth-of-type(2) [role="option"]:nth-of-type(1)').last().click();//city
+        cy.get('div:nth-of-type(3) > div:nth-of-type(3) > div:nth-of-type(2) > .col-lg-10 > ng-select[role="listbox"] input[role="combobox"]').first().click();
+        cy.get('div[role="option"] > .ng-option-label.ng-star-inserted').last().click();//vendor
+        const photo = 'download.jpg'
+        cy.get('input[name="uploadedPhotos"]').attachFile(photo);
+        cy.get('div:nth-of-type(5) input[name="odometer"]').clear();
+        cy.get('div:nth-of-type(5) input[name="odometer"]').type('4500');//odometer
+        cy.get('.col-10 > .btn-success').click();//save button
+
+    });
+    it.only('should allow user to delete the listed vehicle', function () {
+        cy.visit('http://localhost:4200/');
+        cy.get(':nth-child(1) > .input-group > .form-control').clear();
+        cy.get(':nth-child(1) > .input-group > .form-control').type('e2etestcarrier');
+        cy.get(':nth-child(2) > .input-group > .form-control').clear();
+        cy.get(':nth-child(2) > .input-group > .form-control').type('FleetHawks@2502');
+        cy.get('#btnsubmit').click();
+        cy.get('.ng-star-inserted > .nav > :nth-child(5) > .nav-link').click();
+        cy.get(':nth-child(2) > :nth-child(13) > .btn-group > .mb-1').first().click();
+        cy.get(':nth-child(2) > :nth-child(13) > .btn-group > .dropdown-menu > button.dropdown-item').last().click();
+    })
+});
