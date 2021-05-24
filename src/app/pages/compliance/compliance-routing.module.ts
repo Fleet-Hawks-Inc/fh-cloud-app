019a651dc598edf0792c2ce1cc38a6bf8b81875d
daljit <daljit@fleethawks.com>
2021-05-24 16:29:07 +0530

    issue fixed

--- src/app/pages/compliance/compliance-routing.module.ts
+++ src/app/pages/compliance/compliance-routing.module.ts
@@ -1,33 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LogsComponent } from './hos/logs/logs.component';
-import { EditComponent } from './hos/edit/edit.component';
-import { DetailedComponent } from './hos/detailed/detailed.component';
-import { MileageComponent } from './ifta/mileage/mileage.component';
-import { ReportComponent } from './ifta/report/report.component';
-import { UnidentifiedComponent } from './hos/unidentified/unidentified.component';
-import { UncertifiedComponent } from './hos/uncertified/uncertified.component';
-import { InspectionSummaryComponent } from './dvir/inspection-summary/inspection-summary.component';
-import {InspectionDetailComponent} from './dvir/inspection-detail/inspection-detail.component';
-import { GenerateQuarterlyReportComponent } from './ifta/generate-quarterly-report/generate-quarterly-report.component';
-import { IftaReportHistoryComponent } from './ifta/ifta-report-history/ifta-report-history.component';
-import { IftaReturnDetailsComponent } from './ifta/ifta-return-details/ifta-return-details.component';
-import {AddInspectionComponent} from './dvir/inspection/add-inspection/add-inspection.component';
-import {EditInspectionComponent} from './dvir/inspection/edit-inspection/edit-inspection.component';
-import {ListInspectionComponent} from './dvir/inspection/list-inspection/list-inspection.component'
-import {ViewInspectionComponent} from './dvir/inspection/view-inspection/view-inspection.component'
 
 const routes: Routes = [
   {
     path: 'dvir',
-    children: [
-      {path:'inspections',component: ListInspectionComponent},
-      {path: 'inspection-add',component:AddInspectionComponent},
-      {path: 'inspection-details/:inspectionID',component: ViewInspectionComponent},
-      {path: 'inspection-edit/:inspectionID', component:EditInspectionComponent},
-      { path: 'inspection-summary', component: InspectionSummaryComponent },
-      { path: 'inspection-detail/:inspectionID', component: InspectionDetailComponent },
-    ],
+    loadChildren: () => import('./dvir/inspection.module').then((m) => m.InspectionModule) ,
   },
   {
     path: 'ifta',
--- src/app/pages/compliance/compliance.module.ts
+++ src/app/pages/compliance/compliance.module.ts
@@ -3,26 +3,10 @@ import { CommonModule } from "@angular/common";
 import { SharedModule } from "../../shared/shared.module";
 import { FormsModule } from "@angular/forms";
 import { ComplianceRoutingModule } from "./compliance-routing.module";
-// import { LogsComponent } from "./hos/logs/logs.component";
-// import { EditComponent } from "./hos/edit/edit.component";
-// import { DetailedComponent } from "./hos/detailed/detailed.component";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
-// import { MileageComponent } from "./ifta/mileage/mileage.component";
-// import { ReportComponent } from "./ifta/report/report.component";
-// import { UnidentifiedComponent } from "./hos/unidentified/unidentified.component";
-import { InspectionSummaryComponent } from "./dvir/inspection-summary/inspection-summary.component";
-import { InspectionDetailComponent } from "./dvir/inspection-detail/inspection-detail.component";
 import { NgxSpinnerModule } from "ngx-spinner";
 import { NgMultiSelectDropDownModule } from "ng-multiselect-dropdown";
-// import { GenerateQuarterlyReportComponent } from "./ifta/generate-quarterly-report/generate-quarterly-report.component";
-// import { IftaReportHistoryComponent } from "./ifta/ifta-report-history/ifta-report-history.component";
-// import { IftaReturnDetailsComponent } from "./ifta/ifta-return-details/ifta-return-details.component";
 import { MatExpansionModule } from "@angular/material/expansion";
-import { AddInspectionComponent } from "./dvir/inspection/add-inspection/add-inspection.component";
-import { EditInspectionComponent } from "./dvir/inspection/edit-inspection/edit-inspection.component";
-import { ListInspectionComponent } from "./dvir/inspection/list-inspection/list-inspection.component";
-import { ViewInspectionComponent } from "./dvir/inspection/view-inspection/view-inspection.component";
-// import { UncertifiedComponent } from "./hos/uncertified/uncertified.component";
 import { NgSelectModule } from '@ng-select/ng-select';
 import { ChartsModule } from 'ng2-charts';
 @NgModule({
@@ -38,13 +22,6 @@ import { ChartsModule } from 'ng2-charts';
     NgSelectModule,
     ChartsModule
   ],
-  declarations: [
-    InspectionDetailComponent,
-    InspectionSummaryComponent,
-    AddInspectionComponent,
-    EditInspectionComponent,
-    ListInspectionComponent,
-    ViewInspectionComponent,
-  ],
+  declarations: [],
 })
 export class ComplianceModule {}
