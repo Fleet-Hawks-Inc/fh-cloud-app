1cdf2b752997458b312d86e738873ea12fbd70af
hardeep <hardeep@fleethawks.com>
2021-06-14 18:30:09 +0530

    accounts html moved

--- src/app/pages/accounts/accounts.module.ts
+++ src/app/pages/accounts/accounts.module.ts
@@ -2,44 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../../shared/shared.module';
 import { AccountsRoutingModule } from './accounts-routing.module';
-import { AllInvoicesComponent } from './invoices/all-invoices/all-invoices.component';
-import { ChartAccountsComponent } from './accounts/chart-accounts/chart-accounts.component';
-import { DriverPaymentsComponent } from './driver-settlements/driver-payments/driver-payments.component';
-import { EmailedInvoicesComponent } from './invoices/emailed-invoices/emailed-invoices.component';
-import {ExpenseTransactionsComponent } from './accounts/expenses/expense-transactions/expense-transactions.component';
-import { AgingReportComponent } from './invoices/aging-report/aging-report.component';
-import { InvoicesComponent } from './invoices/invoices/invoices.component';
-import { OpenInvoicesComponent } from './invoices/open-invoices/open-invoices.component';
-import { PaidInvoicesComponent } from './invoices/paid-invoices/paid-invoices.component';
-import { PartiallyPaidInvoicesComponent } from './invoices/partially-paid-invoices/partially-paid-invoices.component';
-import { ReceivePaymentsComponent } from './accounts/receive-payments/receive-payments.component';
-import { RecurringDeductionsComponent } from './driver-settlements/recurring-deductions/recurring-deductions.component';
-import { VoidedInvoicesComponent } from './invoices/voided-invoices/voided-invoices.component';
-import { ChartAccountsDetailsComponent } from './accounts/chart-accounts-details/chart-accounts-details.component';
 import { NgSelectModule } from '@ng-select/ng-select';
-import { IncomeTransactionDetailComponent } from './accounts/income-transactions/income-transaction-detail/income-transaction-detail.component';
-import { AddIncomeComponent } from './accounts/income-transactions/add-income/add-income.component';
-import { AllIncomeTransactionsComponent } from './accounts/income-transactions/all-income-transactions/all-income-transactions.component';
 @NgModule({
-  declarations: [
-    AllInvoicesComponent,
-    ChartAccountsComponent,
-    DriverPaymentsComponent,
-    EmailedInvoicesComponent,
-    ExpenseTransactionsComponent,
-    AgingReportComponent,
-    InvoicesComponent,
-    OpenInvoicesComponent,
-    PaidInvoicesComponent,
-    PartiallyPaidInvoicesComponent,
-    ReceivePaymentsComponent,
-    RecurringDeductionsComponent,
-    VoidedInvoicesComponent,
-    ChartAccountsDetailsComponent,
-    IncomeTransactionDetailComponent,
-    AddIncomeComponent,
-    AllIncomeTransactionsComponent,
-  ],
+  declarations: [],
   imports: [
     CommonModule,
     AccountsRoutingModule,
---
+++ src/app/pages/accounts/income/income.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { IncomeListComponent } from './income-list/income-list.component';
+import { AddIncomeComponent } from './add-income/add-income.component';
+import { IncomeDetailComponent } from './income-detail/income-detail.component';
+
+const routes: Routes = [
+  { path: 'list', component: IncomeListComponent},
+  { path: 'add', component: AddIncomeComponent},
+  { path: 'detail', component: IncomeDetailComponent},
+
+];
+
+@NgModule({
+  declarations: [IncomeListComponent, AddIncomeComponent, IncomeDetailComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ]
+})
+export class IncomeModule { }
