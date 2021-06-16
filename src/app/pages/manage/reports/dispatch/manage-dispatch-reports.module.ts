1aceceded8c488db7b14baf2c764bb352021c268
Pankaj Bassi <pankaj@fleethawks.com>
2021-06-16 19:50:10 +0530

    Made trips section reports

--- src/app/pages/manage/reports/dispatch/manage-dispatch-reports.module.ts
+++ src/app/pages/manage/reports/dispatch/manage-dispatch-reports.module.ts
@@ -4,12 +4,17 @@ import { RouterModule, Routes } from '@angular/router';
 
 
 
+
 const routes: Routes = [
   
   {
     path: 'orders',
     loadChildren: () => import('./order-report/manage-orders.module').then((m) => m.ManageOrdersModule) ,
   },
+  {
+    path: 'trips',
+    loadChildren: () => import('./trips/manage-trips.module').then((m) => m.ManageTripsModule) ,
+  }
   
 ]
 
