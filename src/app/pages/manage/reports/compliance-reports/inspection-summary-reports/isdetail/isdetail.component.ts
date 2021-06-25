24ebd9ec2cec978cfda2c6bd35b7e6dd67a64a00
Pankaj Bassi <pankaj@fleethawks.com>
2021-06-25 18:35:01 +0530

    Completed compliance reports

---
+++ src/app/pages/manage/reports/compliance-reports/inspection-summary-reports/isdetail/isdetail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-isdetail',
+  templateUrl: './isdetail.component.html',
+  styleUrls: ['./isdetail.component.css']
+})
+export class IsdetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
