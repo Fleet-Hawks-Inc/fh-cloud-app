f30d119a0358f27f104e8a04248cdaad82f6f2e6
Sandeep Kaur <sandeep.kaur@fleethawks.com>
2021-07-08 13:38:30 +0530

    feat: expense transaction module

--- src/app/pages/accounts/expense/expense-list/expense-list.component.ts
+++ src/app/pages/accounts/expense/expense-list/expense-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { AccountService } from 'src/app/services/account.service';
+import { ApiService } from 'src/app/services/api.service';
+import  Constants  from '../../../fleet/constants'; 
 
 @Component({
   selector: 'app-expense-list',
@@ -7,9 +11,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExpenseListComponent implements OnInit {
 
-  constructor() { }
+  dataMessage: string = Constants.FETCHING_DATA;
+  expenses = [];
+  vendors = [];
+  categories = [];
+  constructor(private accountService: AccountService, private apiService: ApiService, private toaster: ToastrService) { }
 
   ngOnInit() {
+    this.fetchExpenses();
+    this.fetchVendors();
+    this.fetchExpenseCategories();
   }
 
+  fetchExpenses() {
+    this.accountService.getData('expense').subscribe((res: any) => {
+      if(res.length == 0) {
+        this.dataMessage = Constants.NO_RECORDS_FOUND;
+      }
+      this.expenses = res;
+    });
+  }
+
+  fetchVendors() {
+    this.apiService.getData(`contacts/get/list/vendor`)
+      .subscribe((result: any) => {
+        this.vendors = result;
+      })
+  }
+
+  deleteExpense(expenseID) {
+    this.accountService.getData(`expense/delete/${expenseID}`)
+      .subscribe((result: any) => {
+        this.dataMessage = Constants.FETCHING_DATA;
+        this.fetchExpenses();
+        this.toaster.success('Expense transaction deleted successfully.');
+      })
+  }
+
+  fetchExpenseCategories() {
+    this.accountService.getData(`expense/categories/list`)
+      .subscribe((result: any) => {
+        this.categories = result;
+      })
+  }
 }
--- src/app/pages/accounts/expense/expense.module.ts
+++ src/app/pages/accounts/expense/expense.module.ts
@@ -1,14 +1,86 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NgbDateAdapter, NgbDateParserFormatter, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+import { Injectable } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgSelectModule } from '@ng-select/ng-select';
 import { AddExpenseComponent } from './add-expense/add-expense.component';
 import { ExpenseListComponent } from './expense-list/expense-list.component';
 import { ExpenseDetailComponent } from './expense-detail/expense-detail.component';
-import { RouterModule, Routes } from '@angular/router';
+import { AddAccountModule } from '../add-account/add-account.module';
 
+/**
+ * This Service handles how the date is represented in scripts i.e. ngModel.
+ */
+ @Injectable()
+ export class CustomAdapter extends NgbDateAdapter<string> {
+ 
+   readonly DELIMITER = '-';
+ 
+   fromModel(value: string): NgbDateStruct {
+     if (!value)
+       return null
+     let parts = value.split(this.DELIMITER);
+     return {
+       year: + parseInt(parts[0]),
+       month: + parseInt(parts[1]),
+       day: + parseInt(parts[2])
+     }
+   }
+ 
+   toModel(date: NgbDateStruct): string // from internal model -> your mode
+   {
+     let month: any = '';
+     let day: any = '';
+     if (date) {
+       month = (date.month < 10) ? '0' + date.month : date.month;
+       day = (date.day < 10) ? '0' + date.day : date.day;
+     }
+     return date ? date.year + this.DELIMITER + month + this.DELIMITER + day : null;
+   }
+ }
+ 
+ /**
+  * This Service handles how the date is rendered and parsed from keyboard i.e. in the bound input field.
+  */
+ @Injectable()
+ export class CustomDateParserFormatter extends NgbDateParserFormatter {
+ 
+   readonly DELIMITER = '/';
+ 
+   parse(value: string): NgbDateStruct | null {
+     if (value) {
+       const date = value.split(this.DELIMITER);
+       return {
+         year: parseInt(date[2], 10),
+         month: parseInt(date[1], 10),
+         day: parseInt(date[0], 10),
+ 
+       };
+     }
+     return null;
+   }
+ 
+   format(date: NgbDateStruct | null): string {
+     let month: any = '';
+     let day: any = '';
+     if (date) {
+       month = (date.month < 10) ? '0' + date.month : date.month;
+       day = (date.day < 10) ? '0' + date.day : date.day;
+     }
+ 
+     return date ? date.year + this.DELIMITER + month + this.DELIMITER + day : '';
+   }
+ }
+ 
 const routes: Routes = [
   { path: 'list', component: ExpenseListComponent},
   { path: 'add', component: AddExpenseComponent},
-  { path: 'detail', component: ExpenseDetailComponent},
+  { path: 'detail/:expenseID', component: ExpenseDetailComponent},
+  { path: 'edit/:expenseID', component: AddExpenseComponent},
 
 ];
 
@@ -16,7 +88,16 @@ const routes: Routes = [
   declarations: [AddExpenseComponent, ExpenseListComponent, ExpenseDetailComponent],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
-  ]
+    RouterModule.forChild(routes),
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgbModule,
+    NgSelectModule,
+    AddAccountModule
+  ],
+  providers: [
+    { provide: NgbDateAdapter, useClass: CustomAdapter },
+    { provide: NgbDateParserFormatter, useClass: CustomDateParserFormatter }]
 })
 export class ExpenseModule { }
