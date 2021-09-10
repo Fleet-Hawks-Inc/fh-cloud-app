13969587f9c9dc92d541fc6df4b9d3c07e35ee9e
hardeep <hardeep@fleethawks.com>
2021-09-10 09:24:28 +0530

    removed console

--- src/app/pages/dispatch/trips/add-trip/add-trip.component.ts
+++ src/app/pages/dispatch/trips/add-trip/add-trip.component.ts
@@ -2092,7 +2092,6 @@ export class AddTripComponent implements OnInit {
         }
     }
     scrollError() {
-      console.log('hello');
       let errorList;
       setTimeout(() => {
         errorList = document.getElementsByClassName('error').length;
