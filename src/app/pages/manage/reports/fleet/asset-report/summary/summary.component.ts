4ff6e62f27894a507c65005e32489abb8a8a2825
Jaspreet Kaur <jaspreet@fleethawks.com>
2021-10-13 21:34:02 +0530

    fleet: searching in asset reports

--- src/app/pages/manage/reports/fleet/asset-report/summary/summary.component.ts
+++ src/app/pages/manage/reports/fleet/asset-report/summary/summary.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { timeStamp } from 'console';
+import { ApiService } from 'src/app/services';
+import Constants from 'src/app/pages/fleet/constants';
+import { environment } from '../../../../../../../environments/environment';
 
 @Component({
   selector: 'app-summary',
@@ -6,10 +10,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./summary.component.css']
 })
 export class SummaryComponent implements OnInit {
+  allData = [];
+  assetIdentification = '';
+  dataMessage: string = Constants.FETCHING_DATA;
 
-  constructor() { }
+  constructor(private apiService: ApiService) {
 
-  ngOnInit() {
   }
 
+  ngOnInit() {
+    // this.fetchAssetList();
+    this.fetchAssetSearch();
+  }
+
+  fetchAssetList() {
+    this.apiService.getData('assets').subscribe((result: any) => {
+      this.allData = result.Items;
+      console.log('this.allData', this.allData)
+    });
+  }
+
+  fetchAssetSearch() {
+    this.apiService.getData(`assets/fetch/assetList?name=${this.assetIdentification}`).subscribe((result: any) => {
+      // this.apiService.getData('assets/fetch/assetList').subscribe((result: any) => {
+      console.log('this.data', result)
+      this.allData = result.Items;
+    });
+
+
+  }
+  searchAsset() {
+    if (this.assetIdentification !== '') {
+      this.assetIdentification = this.assetIdentification.toLowerCase();
+      this.allData = [];
+      this.dataMessage = Constants.FETCHING_DATA;
+      this.fetchAssetSearch();
+    }
+    else {
+      return false;
+    }
+
+
+  }
 }
+
