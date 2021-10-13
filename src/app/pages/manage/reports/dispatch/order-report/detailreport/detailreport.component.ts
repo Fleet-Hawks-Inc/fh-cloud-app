8c64d10eaf4eab583430ca3f6f7746a22249eb2c
Amitoj Singh <amitoj.singh@fleethawks.com>
2021-10-13 17:30:57 -0600

    feat: added report

--- src/app/pages/manage/reports/dispatch/order-report/detailreport/detailreport.component.ts
+++ src/app/pages/manage/reports/dispatch/order-report/detailreport/detailreport.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService, ListService } from '../../../../../../services'
+import Constant from 'src/app/pages/fleet/constants'
 
 @Component({
   selector: 'app-detailreport',
@@ -7,9 +9,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailreportComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private apiService: ApiService, private listService: ListService) { }
+  totalOrdersCount = 0
+  dispatchedCount = 0
+  delieverdCount = 0
+  deletedCount = 0
+  canceledCount = 0
+  tonuCount = 0
+  records = []
+  dataMessage = ''
+  customers = {}
   ngOnInit() {
+    this.fetchCustomers();
+    this.fetchDetailReport();
+  }
+  async fetchCustomers() {
+    const customers = await this.apiService.getData(`contacts/fetch/order/customers`).toPromise();
+    customers.forEach(element => {
+      this.customers[element.contactID] = element.companyName
+    });
+  }
+
+  async fetchDetailReport() {
+    const result = await this.apiService.getData('orders/report/detail').toPromise()
+    this.totalOrdersCount = result.Count
+    if (this.totalOrdersCount == 0) this.dataMessage = Constant.NO_RECORDS_FOUND
+    result.Items.forEach(element => {
+      if (element.isDeleted == 1) this.deletedCount++
+      if (element.orderStatus == "delivered") this.delieverdCount++
+      if (element.orderStatus == "dispatched") this.dispatchedCount++
+      if (element.orderStatus == "canceled") this.canceledCount++
+      if (element.orderStatus == "tonu") this.tonuCount++
+
+    });
+    this.records = result.Items
+    console.log(this.records)
+
+
   }
 
 }
