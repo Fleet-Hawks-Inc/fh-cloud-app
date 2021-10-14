dc1c2016296b2e0856071554be9c6867d3ad083e
daljit <daljit@fleethawks.com>
2021-10-14 16:07:22 +0530

    CRUD: vendor credits

--- src/app/pages/accounts/purchases/vendor-credit-notes/add-vendor-credit-note/add-vendor-credit-note.component.ts
+++ src/app/pages/accounts/purchases/vendor-credit-notes/add-vendor-credit-note/add-vendor-credit-note.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
+import { AccountService, ListService } from 'src/app/services';
 
 @Component({
   selector: 'app-add-vendor-credit-note',
@@ -7,9 +9,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddVendorCreditNoteComponent implements OnInit {
 
-  constructor() { }
+  creditData: any = {
+    vCrDate: moment().format('YYYY-MM-DD'),
+    currency: null,
+    crRef: '',
+    purOrder: '',
+    vendorID: null,
+    crDetails: [{
+      commodity: '',
+      qty: 0,
+      qtyUnit: null,
+      rate: 0,
+      rateUnit: null,
+      amount: 0,
+      accountID: null,
+    }],
+    remarks: '',
+  }
+
+  accounts: any = [];
+  vendors: any = [];
+
+  constructor(private listService: ListService,
+    private accountService: AccountService) { }
 
   ngOnInit() {
+    this.fetchAccounts();
+    this.listService.fetchVendors();
+    this.vendors = this.listService.vendorList;
+  }
+
+
+
+
+  fetchAccounts() {
+    this.accountService.getData(`chartAc/fetch/list`).subscribe((res: any) => {
+      this.accounts = res;
+    });
+  }
+
+  addDetails() {
+    this.creditData.crDetails.push({
+      commodity: '',
+      qty: 0,
+      qtyUnit: '',
+      rate: 0,
+      rateUnit: '',
+      amount: 0,
+      accountID: null,
+    });
+  }
+
+  deleteDetail(d: number) {
+    this.creditData.crDetails.splice(d, 1);
   }
 
 }
