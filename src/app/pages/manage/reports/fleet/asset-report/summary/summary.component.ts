d7575d9a14a9de15a2da5af59076567203a2deda
Jaspreet kaur <jaspreet@fleethawks.com>
2021-10-16 11:36:01 +0530

    searching and reset

--- src/app/pages/manage/reports/fleet/asset-report/summary/summary.component.ts
+++ src/app/pages/manage/reports/fleet/asset-report/summary/summary.component.ts
@@ -28,43 +28,29 @@ export class SummaryComponent implements OnInit {
   }
 
   ngOnInit() {
-    // this.fetchAssetList();
+    this.fetchAssetList();
+    // this.listCounting()
     this.fetchAssetsCount();
   }
 
   fetchAssetList() {
     this.apiService.getData('assets').subscribe((result: any) => {
-      // this.allData = result.Items;
-      // console.log('this.allData', this.allData)
-
-      for (let i = 0; i < result.Items; i++) {
-        // console.log('count', this.allData[i])
-        if (result.Items[i].currentStatus === "active") {
+      this.allData = result.Items;
+      for (let i = 0; i < this.allData.length; i++) {
+        if (this.allData[i].currentStatus === "active") {
           this.activeAssets += 1
         }
         else {
           this.inActiveAssets += 1
         }
-
       }
     });
   }
-
   fetchAssetsCount() {
     // this.apiService.getData(`assets/fetch/assetList?name=${this.assetIdentification}` + '&assetType=' + this.assetType).subscribe((result: any) => {
     this.apiService.getData(`assets/fetch/assetList?name=${this.assetIdentification}&assetType=${this.assetType}`).subscribe((result: any) => {
       console.log('this.data', result)
       this.allData = result.Items;
-
-      // for (let i = 0; i < this.allData.length; i++) {
-      //   // console.log('count', this.allData[i])
-      //   if (this.allData[i].currentStatus === "active") {
-      //     this.activeAssets += 1
-      //   }
-      //   else {
-      //     this.inActiveAssets += 1
-      //   }
-      // }
       result[`Items`].map((v: any) => {
         v.assetType = v.assetType.replace("_", " ")
       })
@@ -88,7 +74,6 @@ export class SummaryComponent implements OnInit {
       this.allData = [];
       this.dataMessage = Constants.FETCHING_DATA;
       this.fetchAssetsCount();
-
     } else {
       return false;
     }
