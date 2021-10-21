3ebd777ca4ec0ce202ed863e5b309ae64c70a408
harman <harman@fleethawks.com>
2021-10-21 16:57:05 +0530

    reminder report

--- src/app/pages/reports/fleet/reminders-report/sreminders/sreminders.component.ts
+++ src/app/pages/reports/fleet/reminders-report/sreminders/sreminders.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { Constants } from '@aws-amplify/core';
+// import { Constants } from '@aws-amplify/core';
 import { result } from 'lodash';
 import { ApiService } from 'src/app/services';
+import Constants from 'src/app/pages/fleet/constants';
+import { constants } from 'buffer';
+import * as moment from 'moment'
+import { ToastrService } from "ngx-toastr";
 
 @Component({
   selector: 'app-sreminders',
@@ -11,8 +15,9 @@ import { ApiService } from 'src/app/services';
 
 export class SremindersComponent implements OnInit {
   searchServiceTask = null;
-  status = "";
+  status = null;
   entityID = "";
+  lastItemSK = "";
   type = "null";
   allData = [];
   allDatta = [];
@@ -35,7 +40,15 @@ export class SremindersComponent implements OnInit {
   taskName: string;
   serviceTasks = [];
   tasksList = "";
-  constructor(private apiService: ApiService) { }
+  loaded = false
+  totalCount = 0;
+  deletedCount = 0;
+  filterStatus = null;
+  dataMessage: string = Constants.FETCHING_DATA
+  Vehicles = {}
+  tasks = {}
+
+  constructor(private apiService: ApiService, private toastr: ToastrService) { }
 
   ngOnInit() {
     // this.newfunction();
@@ -45,32 +58,57 @@ export class SremindersComponent implements OnInit {
     this.fetchvehicleSList();
     this.fetchTasksList();
     // this. fetchServiceTaks()
+    this.fetchReminderCount();
 
 
 
   }
-  // initDataTable() {
-  //   this.apiService.getData('vehicles/fetch/records?vehicle='+this.carrierID+'&status='+this.currentStatus + '&lastKey=' + this.lastEvaluatedKey)
-  //     .subscribe((result: any) => {
-  //       if(result.Items.length == 0) {
-
-  //       }
-  //     }
-  //     )}
+  setFilterStatus(val) {
+    this.filterStatus = val;
+  }
+  // &type=service
   fetchVehiclesdata() {
-    this.apiService.getData(`reminders/fetch/report/list?entityID=${this.entityID}&type=service&serviceTask=${this.searchServiceTask}`).subscribe((result: any) => {
-      
-      console.log('this.data', result)
-      this.allData = result.Items;
-    });
+    if (this.lastItemSK !== 'end') {
+      this.apiService.getData(`reminders/fetch/report/list?entityID=${this.entityID}&serviceTask=${this.searchServiceTask}&status=${this.filterStatus}&type=service&lastKey=${this.lastItemSK}`).subscribe((result: any) => {
+
+        console.log('this.data', result)
+        // this.allData = result.Items;
+        this.dataMessage = Constants.FETCHING_DATA
+        if (result.Items.length === 0) {
+
+          this.dataMessage = Constants.NO_RECORDS_FOUND
+        }
+        if (result.Items.length > 0) {
+
+          if (result.LastEvaluatedKey !== undefined) {
+            this.lastItemSK = encodeURIComponent(result.Items[result.Items.length - 1].reminderSK);
+          }
+          else {
+            this.lastItemSK = 'end'
+          }
+          this.allData = this.allData.concat(result.Items)
+
+          this.loaded = true;
+        }
+
+
+      });
+    }
+  }
+
+  onScroll() {
+    if (this.loaded) {
+      this.fetchVehiclesdata();
+    }
+    this.loaded = false;
   }
   srchVeh() {
-    if (this.entityID !== '' || this.searchServiceTask !== '') {
+    if (this.entityID !== null || this.searchServiceTask !== null || this.filterStatus !== null) {
 
       // if (this.entityID !== ''){
-      this.entityID = this.entityID;
+      // this.entityID = this.entityID;
       this.allData = [];
-
+      this.lastItemSK = '';
       this.fetchVehiclesdata();
     }
     else {
@@ -79,88 +117,113 @@ export class SremindersComponent implements OnInit {
   }
 
   resetData() {
-    if (this.entityID !== '' || this.searchServiceTask !== null) {
-      this.entityID = '';
+    if (this.entityID !== '' || this.searchServiceTask !== null || this.filterStatus !== null) {
+      this.entityID = null;
       this.searchServiceTask = null;
+      this.status = null;
       this.allData = [];
-
+      this.lastItemSK = '';
+      this.dataMessage = Constants.FETCHING_DATA
       this.fetchVehiclesdata();
     } else {
       return false;
     }
   }
-  
 
 
 
 
-newfunction() {
-  this.apiService.getData("reminders").subscribe((result: any) => {
-    this.allData = result.Items;
-    console.log("this.allData", this.allData);
-  })
-}
+  // newfunction() {
+  //   this.apiService.getData("reminders").subscribe((result: any) => {
+  //     this.allData = result.Items;
+  //     console.log("this.allData", this.allData);
 
-// linkfunction() {
-//   this.apiService.getData("vehicles/get/list").subscribe((result: any) => {
-//     console.log("this.allData", result)
-//     this.vehicleList = result;
-//     if(this.vehicleList == '') {
-//       this.vehicleList = this.srchVeh;
-//     }
-//     this.vehicleList = [];
-//     this.suggestedVehicles = [];
-//     this.fetchVehiclesdata();
-//     // this.initDataTable();
-//   }
-//   )}
-
-fetchvehicleSList() {
-  this.apiService.getData("vehicles/get/list").subscribe((result: any) => {
-    this.vehiclesList = result;
-    console.log("vehicleList", result)
-  });
-}
-// fetchServiceTaks() {
-//   let test = [];
-//   this.apiService.getData('tasks').subscribe((result: any) => {
-//     test = result.Items;
-
-//   });
-// }
-
-fetchTasksList() {
-  this.apiService.getData('tasks/get/list').subscribe((result: any) => { //this is for service task listing
-    this.tasksData = result;
-    console.log("tasksData", result)
-  });
-}
-
-tasksfunction() {
-  this.apiService.getData("tasks/get/list").subscribe((result: any) => {
-    console.log("this.allData", result)
-    this.taskfunction = result;
-
-    for (let i = 0; i < this.allData.length; i++) {
-      if (this.allData[i].status === 'overdue') {
-        this.OverdueService += 1;
-      }
-      else {
-        this.due += 1;
-      }
-
-    }
-    console.log("OverdueService", this.OverdueService)
-  })
-}
-  // searchfunction() {
-  //   console.log("hello", this.srchVeh)
-  //   if (this.searchValues != '') {
-  //     this.searchValues = this.searchValues.toLowerCase();
-  //     console.log("searchValue", this.searchValues)
-
-  //   }
+  //   })
   // }
+  async fetchReminderCount() {
+    const result = await this.apiService.getData('reminders').toPromise()
+    this.totalCount = result.Count
+    if (this.totalCount == 0) this.dataMessage = Constants.NO_RECORDS_FOUND
+    result.Items.forEach(element => {
+      if (element.isDeleted == 1) this.deletedCount++
+      if (element.status == "overdue") this.OverdueService++
+      if (element.status == "dueSoon") this.due++
+    });
+  }
 
+
+  fetchvehicleSList() {
+    this.apiService.getData("vehicles/get/list").subscribe((result: any) => {
+      this.vehiclesList = result;
+      console.log("vehicleList", result)
+    });
+  }
+
+  fetchTasksList() {
+    this.apiService.getData('tasks/get/list').subscribe((result: any) => { //this is for service task listing
+      this.tasksData = result;
+      console.log("tasksData", result)
+    });
+  }
+
+  tasksfunction() {
+    this.apiService.getData("tasks/get/list").subscribe((result: any) => {
+      console.log("this.allData", result)
+      this.taskfunction = result;
+
+      // for (let i = 0; i < this.allData.length; i++) {
+      //   if (this.allData[i].status === 'overdue') {
+      //     this.OverdueService += 1;
+      //   }
+      //   else {
+      //     this.due += 1;
+      //   }
+
+      // }
+      // console.log("OverdueService", this.OverdueService)
+    })
+  }
+
+  generateCSV() {
+    if (this.allData.length > 0) {
+      let dataObject = []
+      let csvArray = []
+
+      this.allData.forEach(element => {
+        let obj = {}
+        obj["Vehicle"] = this.vehiclesList[element.entityID]
+        obj["Service Task"] = this.tasksData[element.tasks.taskID]
+        obj["Next Due"] = element.createdDate
+        obj["Subscribers"] = element.subscribers
+        obj["Renewal Status"] = element.status
+        dataObject.push(obj)
+      });
+      let headers = Object.keys(dataObject[0]).join(',')
+      headers += ' \n'
+      csvArray.push(headers)
+      dataObject.forEach(element => {
+        let obj = Object.values(element).join(',')
+        obj += ' \n'
+        csvArray.push(obj)
+      });
+      const blob = new Blob(csvArray, { type: 'text/csv;charset=utf-8;' });
+
+      const link = document.createElement('a');
+      if (link.download !== undefined) {
+
+        const url = URL.createObjectURL(blob);
+        link.setAttribute('href', url);
+        link.setAttribute('download', `${moment().format("YYYY-MM-DD:HH:m")}serviceReminder-Report.csv`);
+        link.style.visibility = 'hidden';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+
+      }
+    }
+    else {
+      this.toastr.error("No Records found")
+    }
+  }
 }
 
