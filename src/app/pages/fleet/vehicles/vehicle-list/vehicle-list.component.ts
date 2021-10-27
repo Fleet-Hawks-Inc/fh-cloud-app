ca3c585fd0b582259435117429c3abbea1580e97
Kunal Deshmukh <kunal@fleethawks.com>
2021-10-27 19:55:11 +0530

    feat: device online status

--- src/app/pages/fleet/vehicles/vehicle-list/vehicle-list.component.ts
+++ src/app/pages/fleet/vehicles/vehicle-list/vehicle-list.component.ts
@@ -1,14 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { ApiService } from '../../../../services';
-declare var $: any;
-import { ToastrService } from 'ngx-toastr';
-import { HereMapService } from '../../../../services';
-import { NgxSpinnerService } from 'ngx-spinner';
-import  Constants  from '../../constants';
-import {environment} from '../../../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
-import {OnboardDefaultService} from '../../../../services/onboard-default.service';
+import { Component, OnInit } from '@angular/core';
 import * as _ from 'lodash';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
+import { environment } from '../../../../../environments/environment';
+import { ApiService, HereMapService } from '../../../../services';
+import { OnboardDefaultService } from '../../../../services/onboard-default.service';
+import Constants from '../../constants';
+declare var $: any;
 @Component({
   selector: 'app-vehicle-list',
   templateUrl: './vehicle-list.component.html',
@@ -26,11 +25,11 @@ export class VehicleListComponent implements OnInit {
   vehicleIdentification = '';
   allOptions: any = {};
   groupsList: any = {};
-  vehicleModelList: any = {}; 
+  vehicleModelList: any = {};
   vehicleManufacturersList: any = {};
-  serviceProgramsList:any = {};
-  driversList:any = {};
-  vendorsList:any = {};
+  serviceProgramsList: any = {};
+  driversList: any = {};
+  vendorsList: any = {};
   currentView = 'list';
 
   totalRecords = 20;
@@ -72,7 +71,7 @@ export class VehicleListComponent implements OnInit {
   vehicleEndPoint = this.pageLength;
   vehicleTypeObects: any = {};
 
-  constructor(private apiService: ApiService, private httpClient: HttpClient, private hereMap: HereMapService, private toastr: ToastrService, private spinner: NgxSpinnerService, private onboard:OnboardDefaultService) {}
+  constructor(private apiService: ApiService, private httpClient: HttpClient, private hereMap: HereMapService, private toastr: ToastrService, private spinner: NgxSpinnerService, private onboard: OnboardDefaultService) { }
 
   ngOnInit() {
     this.onboard.checkInspectionForms();
@@ -90,21 +89,24 @@ export class VehicleListComponent implements OnInit {
     });
 
     this.httpClient.get('assets/vehicleType.json').subscribe((data: any) => {
-      this.vehicleTypeObects =  data.reduce( (a: any, b: any) => {
+      this.vehicleTypeObects = data.reduce((a: any, b: any) => {
         return a[b['code']] = b['name'], a;
-    }, {});
+      }, {});
     });
+
   }
+
+
 
   getSuggestions = _.debounce(function (value) {
 
     value = value.toLowerCase();
-    if(value != '') {
+    if (value != '') {
       this.apiService
-      .getData(`vehicles/suggestion/${value}`)
-      .subscribe((result) => {
-        this.suggestedVehicles = result;
-      });
+        .getData(`vehicles/suggestion/${value}`)
+        .subscribe((result) => {
+          this.suggestedVehicles = result;
+        });
     } else {
       this.suggestedVehicles = []
     }
@@ -134,6 +136,7 @@ export class VehicleListComponent implements OnInit {
     });
   }
 
+
   fetchServiceProgramsList() {
     this.apiService.getData('servicePrograms/get/list').subscribe((result: any) => {
       this.serviceProgramsList = result;
@@ -147,13 +150,12 @@ export class VehicleListComponent implements OnInit {
   }
 
   fetchVehiclesCount() {
-    this.apiService.getData('vehicles/get/count?vehicle='+this.vehicleID+'&status='+this.currentStatus).subscribe({
-      complete: () => {},
-      error: () => {},
+    this.apiService.getData('vehicles/get/count?vehicle=' + this.vehicleID + '&status=' + this.currentStatus).subscribe({
+      complete: () => { },
+      error: () => { },
       next: (result: any) => {
         this.totalRecords = result.Count;
-        if(this.vehicleID != '' || this.currentStatus != null) 
-        {
+        if (this.vehicleID != '' || this.currentStatus != null) {
           this.vehicleEndPoint = this.totalRecords;
         }
 
@@ -171,13 +173,13 @@ export class VehicleListComponent implements OnInit {
   /**
    * change the view of summary
    */
-  changeView(){
-    if(this.currentView == 'list'){
+  changeView() {
+    if (this.currentView == 'list') {
       this.currentView = 'map'
       setTimeout(() => {
         this.hereMap.mapInit();
       }, 500);
-    }else {
+    } else {
       this.currentView = 'list';
     }
   }
@@ -192,9 +194,9 @@ export class VehicleListComponent implements OnInit {
 
   initDataTable() {
     this.spinner.show();
-    this.apiService.getData('vehicles/fetch/records?vehicle='+this.vehicleID+'&status='+this.currentStatus + '&lastKey=' + this.lastEvaluatedKey)
-      .subscribe((result: any) => {
-        if(result.Items.length == 0) {
+    this.apiService.getData('vehicles/fetch/records?vehicle=' + this.vehicleID + '&status=' + this.currentStatus + '&lastKey=' + this.lastEvaluatedKey)
+      .subscribe(async (result: any) => {
+        if (result.Items.length == 0) {
           this.dataMessage = Constants.NO_RECORDS_FOUND;
         }
         this.suggestedVehicles = [];
@@ -204,14 +206,14 @@ export class VehicleListComponent implements OnInit {
         })
         this.vehicles = result['Items'];
 
-        if(this.vehicleID != '') {
+        if (this.vehicleID != '') {
           this.vehicleStartPoint = 1;
           this.vehicleEndPoint = this.totalRecords;
         }
 
 
         if (result['LastEvaluatedKey'] !== undefined) {
-          let lastEvalKey = result[`LastEvaluatedKey`].vehicleSK.replace(/#/g,'--');
+          let lastEvalKey = result[`LastEvaluatedKey`].vehicleSK.replace(/#/g, '--');
           this.vehicleNext = false;
           // for prev button
           if (!this.vehiclePrevEvauatedKeys.includes(lastEvalKey)) {
@@ -225,7 +227,7 @@ export class VehicleListComponent implements OnInit {
           this.vehicleEndPoint = this.totalRecords;
         }
 
-        if(this.totalRecords < this.vehicleEndPoint) {
+        if (this.totalRecords < this.vehicleEndPoint) {
           this.vehicleEndPoint = this.totalRecords;
         }
 
@@ -235,14 +237,36 @@ export class VehicleListComponent implements OnInit {
         } else {
           this.vehiclePrev = true;
         }
+
         this.spinner.hide();
+        await this.getDashCamLive(this.vehicles);
       });
+  }
+
+  /**
+   * Get device status from DashCam
+   * @param vehicleList all the vehicles
+   */
+  async getDashCamLive(vehicleList: any) {
+    if (vehicleList && vehicleList.length > 0) {
+      for (const data of vehicleList) {
+        if (data.deviceInfo) {
+          data['isDashCam'] = true;
+          const response: any = await this.apiService.postData('vehicles/dashCam/status', { deviceId: data.deviceInfo[0].deviceSrNo.split('#')[1] }).toPromise();
+          if (response && response.isOnline !== undefined) {
+            console.log('isDashOnline', response.isOnline)
+            data['isDashOnline'] = response.isOnline;
+          }
+        }
+      }
+    }
+
   }
 
   searchFilter() {
     if (this.vehicleIdentification !== '' || this.currentStatus !== null) {
       this.vehicleIdentification = this.vehicleIdentification.toLowerCase();
-      if(this.vehicleID == '') {
+      if (this.vehicleID == '') {
         this.vehicleID = this.vehicleIdentification;
       }
       this.dataMessage = Constants.FETCHING_DATA;
@@ -294,149 +318,149 @@ export class VehicleListComponent implements OnInit {
 
   hideShowColumn() {
     //for headers
-    if(this.hideShow.vin == false) {
-      $('.col1').css('display','none');
+    if (this.hideShow.vin == false) {
+      $('.col1').css('display', 'none');
     } else {
-      $('.col1').css('display','');
+      $('.col1').css('display', '');
     }
 
-    if(this.hideShow.vehicleName == false) {
-      $('.col2').css('display','none');
+    if (this.hideShow.vehicleName == false) {
+      $('.col2').css('display', 'none');
     } else {
-      $('.col2').css('display','');
+      $('.col2').css('display', '');
     }
 
-    if(this.hideShow.vehicleType == false) {
-      $('.col3').css('display','none');
+    if (this.hideShow.vehicleType == false) {
+      $('.col3').css('display', 'none');
     } else {
-      $('.col3').css('display','');
+      $('.col3').css('display', '');
     }
 
-    if(this.hideShow.make == false) {
-      $('.col4').css('display','none');
+    if (this.hideShow.make == false) {
+      $('.col4').css('display', 'none');
     } else {
-      $('.col4').css('display','');
+      $('.col4').css('display', '');
     }
 
-    if(this.hideShow.model == false) {
-      $('.col5').css('display','none');
+    if (this.hideShow.model == false) {
+      $('.col5').css('display', 'none');
     } else {
       $('.col5').removeClass('extra');
-      $('.col5').css('display','');
-      $('.col5').css('min-width','120px');
+      $('.col5').css('display', '');
+      $('.col5').css('min-width', '120px');
     }
 
-    if(this.hideShow.lastLocation == false) {
-      $('.col6').css('display','none');
+    if (this.hideShow.lastLocation == false) {
+      $('.col6').css('display', 'none');
     } else {
-      $('.col6').css('display','');
+      $('.col6').css('display', '');
     }
 
-    if(this.hideShow.trip == false) {
-      $('.col7').css('display','none');
+    if (this.hideShow.trip == false) {
+      $('.col7').css('display', 'none');
     } else {
       $('.col7').removeClass('extra');
-      $('.col7').css('display','');
-      $('.col7').css('min-width','200px');
+      $('.col7').css('display', '');
+      $('.col7').css('min-width', '200px');
     }
 
-    if(this.hideShow.plateNo == false) {
-      $('.col8').css('display','none');
+    if (this.hideShow.plateNo == false) {
+      $('.col8').css('display', 'none');
     } else {
-      $('.col8').css('display','');
+      $('.col8').css('display', '');
     }
 
-    if(this.hideShow.fuelType == false) {
-      $('.col9').css('display','none');
+    if (this.hideShow.fuelType == false) {
+      $('.col9').css('display', 'none');
     } else {
-      $('.col9').css('display','');
+      $('.col9').css('display', '');
     }
 
-    if(this.hideShow.status == false) {
-      $('.col10').css('display','none');
+    if (this.hideShow.status == false) {
+      $('.col10').css('display', 'none');
     } else {
-      $('.col10').css('display','');
+      $('.col10').css('display', '');
     }
 
-    if(this.hideShow.group == false) {
-      $('.col11').css('display','none');
+    if (this.hideShow.group == false) {
+      $('.col11').css('display', 'none');
     } else {
       $('.col11').removeClass('extra');
-      $('.col11').css('display','');
-      $('.col11').css('min-width','200px');
+      $('.col11').css('display', '');
+      $('.col11').css('min-width', '200px');
     }
 
-    if(this.hideShow.ownership == false) {
-      $('.col12').css('display','none');
+    if (this.hideShow.ownership == false) {
+      $('.col12').css('display', 'none');
     } else {
       $('.col12').removeClass('extra');
-      $('.col12').css('display','');
-      $('.col12').css('min-width','200px');
+      $('.col12').css('display', '');
+      $('.col12').css('min-width', '200px');
     }
 
     //extra columns
-    if(this.hideShow.driver == false) {
-      $('.col13').css('display','none');
+    if (this.hideShow.driver == false) {
+      $('.col13').css('display', 'none');
     } else {
       $('.col13').removeClass('extra');
-      $('.col13').css('display','');
-      $('.col13').css('min-width','200px');
+      $('.col13').css('display', '');
+      $('.col13').css('min-width', '200px');
     }
 
-    if(this.hideShow.serviceProgram == false) {
-      $('.col14').css('display','none');
+    if (this.hideShow.serviceProgram == false) {
+      $('.col14').css('display', 'none');
     } else {
       $('.col14').removeClass('extra');
-      $('.col14').css('display','');
-      $('.col14').css('min-width','200px');
+      $('.col14').css('display', '');
+      $('.col14').css('min-width', '200px');
     }
 
-    if(this.hideShow.serviceDate == false) {
-      $('.col15').css('display','none');
+    if (this.hideShow.serviceDate == false) {
+      $('.col15').css('display', 'none');
     } else {
       $('.col15').removeClass('extra');
-      $('.col15').css('display','');
-      $('.col15').css('min-width','200px');
+      $('.col15').css('display', '');
+      $('.col15').css('min-width', '200px');
     }
 
-    if(this.hideShow.insuranceVendor == false) {
-      $('.col16').css('display','none');
+    if (this.hideShow.insuranceVendor == false) {
+      $('.col16').css('display', 'none');
     } else {
       $('.col16').removeClass('extra');
-      $('.col16').css('display','');
-      $('.col16').css('min-width','200px');
+      $('.col16').css('display', '');
+      $('.col16').css('min-width', '200px');
     }
 
-    if(this.hideShow.insuranceAmount == false) {
-      $('.col17').css('display','none');
+    if (this.hideShow.insuranceAmount == false) {
+      $('.col17').css('display', 'none');
     } else {
       $('.col17').removeClass('extra');
-      $('.col17').css('display','');
-      $('.col17').css('min-width','200px');
+      $('.col17').css('display', '');
+      $('.col17').css('min-width', '200px');
     }
 
-    if(this.hideShow.engineSummary == false) {
-      $('.col18').css('display','none');
+    if (this.hideShow.engineSummary == false) {
+      $('.col18').css('display', 'none');
     } else {
       $('.col18').removeClass('extra');
-      $('.col18').css('display','');
-      $('.col18').css('min-width','200px');
+      $('.col18').css('display', '');
+      $('.col18').css('min-width', '200px');
     }
 
-    if(this.hideShow.primaryMeter == false) {
-      $('.col19').css('display','none');
+    if (this.hideShow.primaryMeter == false) {
+      $('.col19').css('display', 'none');
     } else {
       $('.col19').removeClass('extra');
-      $('.col19').css('display','');
-      $('.col19').css('min-width','200px');
+      $('.col19').css('display', '');
+      $('.col19').css('min-width', '200px');
     }
 
-    if(this.hideShow.fuelUnit == false) {
-      $('.col20').css('display','none');
+    if (this.hideShow.fuelUnit == false) {
+      $('.col20').css('display', 'none');
     } else {
       $('.col20').removeClass('extra');
-      $('.col20').css('display','');
-      $('.col20').css('min-width','200px');
+      $('.col20').css('display', '');
+      $('.col20').css('min-width', '200px');
     }
   }
 
@@ -451,7 +475,7 @@ export class VehicleListComponent implements OnInit {
     this.vehiclePrev = true;
     this.vehicleDraw += 1;
     this.initDataTable();
-   
+
   }
 
   // prev button func
@@ -461,7 +485,7 @@ export class VehicleListComponent implements OnInit {
     this.vehicleDraw -= 1;
     this.lastEvaluatedKey = this.vehiclePrevEvauatedKeys[this.vehicleDraw];
     this.initDataTable();
-    
+
   }
 
   resetCountResult() {
