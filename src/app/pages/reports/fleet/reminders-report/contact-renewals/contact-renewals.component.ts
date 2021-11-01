1477575c5d5fa48710e30de8ae3e28afc4024b3f
harman <harman@fleethawks.com>
2021-11-01 17:13:27 +0530

    contact-renewal

--- src/app/pages/reports/fleet/reminders-report/contact-renewals/contact-renewals.component.ts
+++ src/app/pages/reports/fleet/reminders-report/contact-renewals/contact-renewals.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { result } from 'lodash';
+import * as moment from 'moment'
 import { ApiService } from 'src/app/services';
 import Constants from 'src/app/pages/fleet/constants';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-contact-renewals',
@@ -10,47 +12,146 @@ import Constants from 'src/app/pages/fleet/constants';
 })
 export class ContactRenewalsComponent implements OnInit {
   empData = [];
+  tasks = []
+  // tasks: any = {};
   dataMessage: string = Constants.NO_RECORDS_FOUND;
-  contactID = null;
+  entityID = null;
   searchServiceTask = null;
   filterStatus = null;
   lastEvaluatedKey = "";
   lastItemSK = "";
   loaded = false
-  constructor(private apiService: ApiService) { }
+  empName = [];
+  count = {
+    total: '',
+    overdue: '',
+    dueSoon: '',
+  };
+  status = null;
+  constructor(private apiService: ApiService, private toastr: ToastrService) { }
 
   ngOnInit() {
     this.fetchEmpData();
     this.fetchallitems();
+    this.fectchTasks();
+    this.getRemindersCount();
+    this.fetchEmployees();
+    this.fetchReminderCount();
   }
 
-
+  fectchTasks() {
+    this.apiService.getData("tasks/get/list").subscribe((result: any) => {
+      this.tasks = result;
+      console.log("tasks", result)
+    })
+  }
+  fetchReminderCount() {
+    this.apiService.getData(`reminders/fetch/count?type=service`).subscribe((result: any) => {
+      this.count = result;
+    })
+  }
+  fetchEmployees() {
+    this.apiService.getData('contacts/get/emp/list').subscribe((res) => {
+      console.log('result', res);
+      this.empName = res;
+    });
+  }
   fetchEmpData() {
-    this.apiService.getData("contacts/employee/records").subscribe((result: any) => {
+    this.apiService.getData("reminders").subscribe((result: any) => {
       this.empData = result.Items;
       console.log('empData', result)
     });
   }
+  getRemindersCount() {
+    this.apiService.getData(`reminders/get/count?reminderIdentification=${this.entityID}&serviceTask=${this.searchServiceTask}&status=${this.filterStatus}&reminderType=contact`).subscribe({
+      complete: () => { },
+      error: () => { },
+      next: (result: any) => {
+
+        this.fetchallitems();
+      },
+    });
+  }
   fetchallitems() {
-    this.apiService.getData(`reminders/fetch/records?reminderIdentification=${this.contactID}&serviceTask=${this.searchServiceTask}&status=${this.filterStatus}&reminderType=contact&lastKey=${this.lastEvaluatedKey}`)
+    this.apiService.getData(`reminders/fetch/records?reminderIdentification=${this.entityID}&serviceTask=${this.searchServiceTask}&status=${this.filterStatus}&reminderType=contact&lastKey=${this.lastEvaluatedKey}`)
       .subscribe((result: any) => {
-        this.dataMessage = Constants.FETCHING_DATA
         if (result.Items.length === 0) {
-
-          this.dataMessage = Constants.NO_RECORDS_FOUND
+          this.dataMessage = Constants.NO_RECORDS_FOUND;
         }
-        if (result.Items.length > 0) {
-
-          if (result.LastEvaluatedKey !== undefined) {
-            this.lastItemSK = encodeURIComponent(result.Items[result.Items.length - 1].reminderSK);
-          }
-          else {
-            this.lastItemSK = 'end'
-          }
-          this.empData = this.empData.concat(result.Items)
-
-          this.loaded = true;
+        this.empData = result[`Items`];
+        console.log(' this.empData', this.empData);
+        if (this.entityID != null || this.searchServiceTask != null) {
         }
+        if (result[`LastEvaluatedKey`] !== undefined) {
+          let lastEvalKey = result[`LastEvaluatedKey`].reminderSK.replace(/#/g, '--');
+        }
+      },
+      );
+  }
+
+  searchData() {
+    if (this.entityID !== null || this.searchServiceTask !== null || this.filterStatus !== null) {
+      this.empData = [];
+      this.lastItemSK = '';
+      this.dataMessage = Constants.FETCHING_DATA
+      this.fetchallitems();
+    }
+    else {
+      return false;
+    }
+  }
+  resetData() {
+    if (this.entityID !== null || this.searchServiceTask !== null || this.filterStatus !== null) {
+      this.entityID = null;
+      this.searchServiceTask = null;
+      this.status = null;
+      this.empData = [];
+      this.lastItemSK = '';
+      this.filterStatus = null;
+      this.dataMessage = Constants.FETCHING_DATA
+      this.fetchallitems();
+    } else {
+      return false;
+    }
+  }
+  generateCSV() {
+    if (this.empData.length > 0) {
+      let dataObject = []
+      let csvArray = []
+      this.empData.forEach(element => {
+        let obj = {}
+        obj["Contact"] = this.empName[element.entityID]
+        obj["Contact Renewal Type"] = this.tasks[element.tasks.taskID]
+        obj["Send Reminder"] = element.tasks.time + " " + element.tasks.timeUnit
+        obj["Expiration Date"] = element.tasks.dueDate
+        obj["Subscribers"] = element.subscribers
+        dataObject.push(obj)
       });
+      let headers = Object.keys(dataObject[0]).join(',')
+      headers += ' \n'
+      csvArray.push(headers)
+      dataObject.forEach(element => {
+        let obj = Object.values(element).join(',')
+        obj += ' \n'
+        csvArray.push(obj)
+      });
+      const blob = new Blob(csvArray, { type: 'text/csv;charset=utf-8;' });
+
+      const link = document.createElement('a');
+      if (link.download !== undefined) {
+
+        const url = URL.createObjectURL(blob);
+        link.setAttribute('href', url);
+        link.setAttribute('download', `${moment().format("YYYY/MM/DD:HH:m")}vehicle-renewal-Report.csv`);
+        link.style.visibility = 'hidden';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+
+      }
+    }
+    else {
+      this.toastr.error("No Records found")
+    }
   }
 }
