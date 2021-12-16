c94b5c8fa9d6fe500e1b5830e50806f7025daf21
Pratik Ganjare <89508246+PratikGanjareFleetHawks@users.noreply.github.com>
2021-12-16 12:14:23 +0530

    Update manage.module.ts

--- src/app/pages/manage/manage.module.ts
+++ src/app/pages/manage/manage.module.ts
@@ -19,7 +19,6 @@ import { ManagemainComponent } from './managemain/managemain.component';
 
 import { AlertsListComponent } from './alerts/alerts-list/alerts-list.component';
 import { AddAlertsComponent } from './alerts/add-alerts/add-alerts.component';
-import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 
 
 const COMPONENTS = [
