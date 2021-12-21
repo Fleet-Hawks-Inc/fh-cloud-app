6b072025b940c203fa0b3a0f7365fb514a95b419
Jaspreet kaur <jaspreet@fleethawks.com>
2021-12-21 11:53:39 +0530

    fix: all states added in us

--- src/app/pages/reports/fleet/vehicle-report/province-miles/province-miles.component.ts
+++ src/app/pages/reports/fleet/vehicle-report/province-miles/province-miles.component.ts
@@ -207,8 +207,9 @@ export class ProvinceMilesComponent implements OnInit {
             })
           })
         })
-        const usProvArr = ["DE", "FL", "GA", "MD", "NC", "NJ", "NY", "SC", "VA", "US"]
-        const canArr = ["AB", "BC", "MB", "NB", "NL", "NT", "NS", "NU", "ON", "PE", "QC", "SK", "US", "YT"]
+        const usProvArr = ["AL","AK","AZ","AR","CA","CO","CT","DE","DC","FL", "GA","HI","ID","IL","IN", "IA","KS","	KY","	LA","ME", "MD","MA","MI","MN",
+        "MS","MO","MT","NE","NV","NH","	NJ","	NM","	NY", "NC", "ND", "OH","OK","OR","PA","PR","	RI","SC","SD","	TN", "TX", "UT", "VT", "VA", "VI", "WA", "WV", "WI", "WY"]
+        const canArr = ["AB", "BC", "MB", "NB", "NL", "NF","NT", "NS", "NU", "ON", "PE","PQ", "QC", "SK", "Canada", "YT"]
         for (let item of data.provinceData) {
           data.finalData = item
          
