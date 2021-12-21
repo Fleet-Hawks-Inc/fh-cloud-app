75e19fbb05b4ef57f917a86dbd33e387768d3ad4
Jaspreet kaur <jaspreet@fleethawks.com>
2021-12-21 18:37:31 +0530

    fix: issue in report

--- src/app/pages/reports/fleet/vehicle-report/province-miles/province-miles.component.ts
+++ src/app/pages/reports/fleet/vehicle-report/province-miles/province-miles.component.ts
@@ -121,7 +121,7 @@ export class ProvinceMilesComponent implements OnInit {
       const usProvArr = ["AL","AK","AZ","AR","CA","CO","CT","DE","DC","FL", "GA","HI","ID","IL","IN", "IA","KS","	KY","	LA","ME", "MD","MA","MI","MN",
       "MS","MO","MT","NE","NV","NH","	NJ","	NM","	NY", "NC", "ND", "OH","OK","OR","PA","PR","	RI","SC","SD","	TN", "TX", "UT", "VT", "VA", "VI", "WA", "WV", "WI", "WY"]
       const canArr = ["AB", "BC", "MB", "NB", "NL", "NF","NT", "NS", "NU", "ON", "PE","PQ", "QC", "SK", "Canada", "YT"]
-      // console.log('data.provinceData', data.provinceData)
+     
       for (let item of data.provinceData) {
         data.finalData = item
        
