33e8064fda53632cfe3f8ac81e962b64987b8496
saurkarankita <ankita@fleethawks.com>
2021-12-30 11:39:37 +0000

    feat: resolved changes in vehicle miles report

--- src/app/pages/reports/fleet/vehicle-report/activity/activity.component.ts
+++ src/app/pages/reports/fleet/vehicle-report/activity/activity.component.ts
@@ -28,8 +28,6 @@ export class ActivityComponent implements OnInit {
     dataMessage = Constants.FETCHING_DATA;
     date = new Date();
     exportData = [];
-    usStateArr: any;
-    element3: any;
     futureDatesLimit = { year: this.date.getFullYear() + 30, month: 12, day: 31 };
     public vehicleId;
     constructor(private apiService: ApiService, private toastr: ToastrService, private route: ActivatedRoute) { }
@@ -57,7 +55,7 @@ export class ActivityComponent implements OnInit {
         this.loaded = false;
     }
 
-    fetchVehicleListing() {
+  fetchVehicleListing() {
         if (this.lastItemSK !== 'end') {
             this.apiService.getData(`vehicles/fetch/TripData?vehicle=${this.vehicleId}&startDate=${this.start}&endDate=${this.end}&lastKey=${this.lastItemSK}&date=${this.datee}`).subscribe((result: any) => {
                 this.allData = this.allData.concat(result.Items)
@@ -86,8 +84,9 @@ export class ActivityComponent implements OnInit {
             });
         }
     }
-
-    fetchFullExport(type = '') {
+    
+    
+    fetchFullExport() {
         this.apiService.getData(`vehicles/fetch/vehicelActivity/list?vehicle=${this.vehicleId}&startDate=${this.start}&endDate=${this.end}`).subscribe((result: any) => {
             this.exportData = result.Items;
             for (let veh of this.exportData) {
@@ -98,8 +97,7 @@ export class ActivityComponent implements OnInit {
                     veh.miles += Number(element.miles);
                 }
             }
-            this.generateCSV(type);
-
+            this.generateCSV();
         });
     }
 
@@ -127,7 +125,7 @@ export class ActivityComponent implements OnInit {
         }
     }
 
-    generateCSV(type = '') {
+    generateCSV() {
         if (this.exportData.length > 0) {
             let dataObject = []
             let csvArray = []
@@ -139,50 +137,45 @@ export class ActivityComponent implements OnInit {
                 let canMiles = ''
                 let usState = ''
                 let canState = ''
-                let stateArr = [];
                 for (let i = 0; i < element.tripPlanning.length; i++) {
                     const element2 = element.tripPlanning[i];
                     date += `"${element2.type} :-  ${element2.date}\n\"`
                     element2.location = element2.location.replace(/,/g, ' ');
                     location += `"${element2.type} :-  ${element2.location}\n\"`
                 }
-                if (type === 'CAN') {
-                    stateArr = element.canStatesData;
-                } else if (type === 'US') {
-                    stateArr = element.usStatesData;
-                }
-                stateArr.forEach((tripUSState, stateIndex) => {
+               
                     if (element.provinceData && element.provinceData.length > 0) {
                         for (let i = 0; i < element.provinceData.length; i++) {
                             const element2 = element.provinceData[i];
-
+                            for(let j = 0; j< element2.usProvince.length; j++)
+                            {
+                              const element3 = element2.usProvince[j];
+                              usState += `"${element3.StCntry}\n\"`;
+                              usMiles += `"${element3.Total}\n\"`;
+                            }
                             for (let k = 0; k < element2.canProvince.length; k++) {
                                 const element4 = element2.canProvince[k];
-                                canState += `"${element4.StCntry}\n\"`;
-                                canMiles += `"${element4.Total}\n\"`;
-                            }
+                                 canState += `"${element4.StCntry}\n\"`;
+                                 canMiles += `"${element4.Total}\n\"`;
+        
+                                    }
+                                }
+                              }
                             let obj = {}
                             obj["Vehicle"] = element.vehicle.replace(/, /g, ' &');
                             obj["Trip#"] = element.tripNo;
                             obj["Order#"] = element.orderName.replace(/, /g, ' &');
                             obj["location"] = location;
                             obj["Date"] = date;
-                            if (type === 'US') {
-                                obj["Province(US)"] = element2.usProvince[stateIndex].StCntry;
-                                obj["US Province Miles"] = element2.usProvince[stateIndex].Total;
-                                obj["US(Total)"] = element.usMiles;
-                            }
-                            else {
-                                obj["Province(Canada)"] = element2.canProvince[stateIndex].StCntry;
-                                obj["Canada Total Miles"] = element2.canProvince[stateIndex].Total;
-                                obj["Canada(Total)"] = element.canMiles;
-                            }
+                            obj["Province(US)"] = usState;
+                            obj["US Province Miles"] = usMiles;
+                            obj["US(Total)"] = element.usMiles;
+                            obj["Province(Canada)"] = canState; 
+                            obj["Canada Total Miles"] = canMiles;
+                            obj["Canada(Total)"] = element.canMiles;
                             obj["Total Miles"] = element.miles;
                             dataObject.push(obj)
-                        }
-                    }
                 });
-            });
             let headers = Object.keys(dataObject[0]).join(',')
             headers += ' \n'
             csvArray.push(headers)
