a1861d72235a597a7383bc7115a9cd709ccedee5
Amitoj Singh <amitoj.singh@fleethawks.com>
2022-01-24 14:33:51 -0700

    fix: build

--- src/app/pages/reports/accounts/payment-report/payment-report.component.ts
+++ src/app/pages/reports/accounts/payment-report/payment-report.component.ts
@@ -12,6 +12,9 @@ export class PaymentReportComponent implements OnInit {
   constructor(private accountService:AccountService) { }
 
   allPayments=[]
+  dateMinLimit = { year: 1950, month: 1, day: 1 };
+  date = new Date();
+  futureDatesLimit = { year: this.date.getFullYear() + 30, month: 12, day: 31 };
   lastAdvanceSK=''
   lastDriverSK=''
   lastEmployeeSK=''
