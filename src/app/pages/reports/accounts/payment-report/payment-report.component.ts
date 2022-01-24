835c41437b2a1e05b31d3197fb68d3f77f03b881
Amitoj Singh <amitoj.singh@fleethawks.com>
2022-01-24 14:23:30 -0700

    fix: payment report search

--- src/app/pages/reports/accounts/payment-report/payment-report.component.ts
+++ src/app/pages/reports/accounts/payment-report/payment-report.component.ts
@@ -34,6 +34,11 @@ paymentType=[
     {name:"Cheque",type:"cheque"},
     {name:"Credit Card",type:"credit_card"}
   ]
+  driverLoaded=false;
+  employeeLoaded=false;
+  expenseLoaded=false;
+  advanceLoaded=false;
+
 
   ngOnInit(): void {
     this.init();
@@ -48,12 +53,11 @@ async init(){
 }
 
 async fetchAdvancePayments(refresh?: boolean){
-  let searchParam=null;
   if (refresh==true){
     this.lastAdvanceSK="";
   }
   if (this.lastAdvanceSK !== "end") {
-    const result=await this.accountService.getData(`advance/report/paging?searchValue=${searchParam}&startDate=${this.filter.startDate}&endDate=${this.filter.endDate}&lastKey=${this.lastAdvanceSK}`).toPromise();
+    const result=await this.accountService.getData(`advance/report/paging?&startPay=${this.filter.startDate}&endPay=${this.filter.endDate}&paymentMode=${this.filter.mode}&lastKey=${this.lastAdvanceSK}`).toPromise();
     if(result && result.length===0){
       this.dataMessage= Constants.NO_RECORDS_FOUND
     }
@@ -62,22 +66,23 @@ async fetchAdvancePayments(refresh?: boolean){
         this.lastAdvanceSK = encodeURIComponent(
           result[result.length - 1].sk
         );
+        this.advanceLoaded=true
       } else {
         this.lastAdvanceSK = "end";
       }
       this.allPayments=this.allPayments.concat(result)
+      
     }
   }
 }
 async fetchDriverPayments(refresh?: boolean){
-  let searchParam=null;
   if (refresh==true){
     this.lastDriverSK="";
   }
   if (this.lastAdvanceSK !== "end") {
     const result=await this.accountService
     .getData(
-      `driver-payments/report/paging?searchValue=${searchParam}&startDate=${this.filter.startDate}&endDate=${this.filter.endDate}&lastKey=${this.lastDriverSK}`
+      `driver-payments/report/paging?startPay=${this.filter.startDate}&endPay=${this.filter.endDate}&paymentMode=${this.filter.mode}&lastKey=${this.lastDriverSK}`
     ).toPromise();
     if(result && result.length===0){
       this.dataMessage= Constants.NO_RECORDS_FOUND
@@ -87,15 +92,16 @@ async fetchDriverPayments(refresh?: boolean){
         this.lastDriverSK = encodeURIComponent(
           result[result.length - 1].sk
         );
+        this.driverLoaded=true;
       } else {
         this.lastDriverSK = "end";
       }
       this.allPayments=this.allPayments.concat(result)
+      
     }
   }
 }
 async fetchEmployeePayments(refresh?: boolean){
-  let searchParam=null;
   if (refresh==true){
     this.lastEmployeeSK="";
   }
@@ -103,33 +109,35 @@ async fetchEmployeePayments(refresh?: boolean){
     
     const result=await this.accountService
     .getData(
-      `employee-payments/report/paging?paymentNo=${searchParam}&startDate=${this.filter.startDate}&endDate=${this.filter.endDate}&lastKey=${this.lastEmployeeSK}`
+      `employee-payments/report/paging?startPay=${this.filter.startDate}&endPay=${this.filter.endDate}&paymentMode=${this.filter.mode}&lastKey=${this.lastEmployeeSK}`
     ).toPromise();
     if(result && result.length===0){
       this.dataMessage= Constants.NO_RECORDS_FOUND
     }
     if(result && result.length>0){
+      
       if (result[result.length - 1].sk !== undefined) {
         this.lastEmployeeSK = encodeURIComponent(
           result[result.length - 1].sk
         );
+        this.employeeLoaded=true;
       } else {
         this.lastEmployeeSK = "end";
       }
       this.allPayments=this.allPayments.concat(result)
+      
     }
   }
 }
 
 async fetchExpencePayments(refresh?: boolean){
-  let searchParam=null;
   if (refresh==true){
     this.lastExpenceSK="";
   }
   if (this.lastExpenceSK !== "end") {
     const result: any = await this.accountService
         .getData(
-          `expense-payments/report/paging?searchValue=${searchParam}&start=${this.filter.startDate}&end=${this.filter.endDate}&lastKey=${this.lastExpenceSK}`
+          `expense-payments/report/paging?startPay=${this.filter.startDate}&endPay=${this.filter.endDate}&paymentMode=${this.filter.mode}&lastKey=${this.lastExpenceSK}`
         )
         .toPromise();
     if(result && result.length===0){
@@ -140,47 +148,78 @@ async fetchExpencePayments(refresh?: boolean){
         this.lastExpenceSK = encodeURIComponent(
           result[result.length - 1].sk
         );
+        this.expenseLoaded=true;
       } else {
         this.lastExpenceSK = "end";
       }
+      
       this.allPayments=this.allPayments.concat(result)
     }
   }
 }
-
-searchFilter(){
+resetVariables(){
+  this.lastEmployeeSK=''
+  this.lastDriverSK=''
+  this.lastAdvanceSK=''
+  this.lastExpenceSK=''
+  this.employeeLoaded=false;
+  this.expenseLoaded=false;
+  this.advanceLoaded=false;
+  this.driverLoaded=false;
+}
+async searchFilter(){
   this.allPayments=[]
+  this.resetVariables();
   if(this.filter.type){
   switch(this.filter.type){
     case "epp":{
-      this.lastEmployeeSK=''
       this.fetchEmployeePayments();
       break;
     }
     case "exp":{
-      this.lastExpenceSK=''
       this.fetchExpencePayments();
       break;
     }
     case "advp":{
-      this.lastAdvanceSK=''
       this.fetchAdvancePayments()
       break;
     }
     case "drvp":{
-      this.lastDriverSK=''
       this.fetchDriverPayments();
       break;
     }
   }
 }
+else{
+  await this.init();
 }
-resetFilter(){
-  this.init();
+}
+async resetFilter(){
+  this.resetVariables();
+  this.filter={
+    startDate:null,
+    endDate:null,
+    mode:null,
+    type:null
+  }
+  await this.init();
 }
 
 
   onScroll(){
+    // console.log("Expense",this.expenseLoaded)
+    // console.log("Advance",this.advanceLoaded)
+    // console.log("Driver",this.driverLoaded)
+    // console.log("employee",this.employeeLoaded)
+      if(this.expenseLoaded) this.fetchExpencePayments();
+      if(this.advanceLoaded) this.fetchAdvancePayments();
+      if(this.driverLoaded) this.fetchDriverPayments();
+      if(this.employeeLoaded) this.fetchEmployeePayments();
+   
+   this.expenseLoaded=false
+   this.advanceLoaded=false
+   this.driverLoaded=false
+   this.employeeLoaded=false
 
   }
 }
