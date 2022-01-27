a0cf0079a71007a8dc0e56223cdff046bf8f9884
Jaspreet kaur <jaspreet@fleethawks.com>
2022-01-27 11:01:58 +0530

    feat: trip and order data on revenue detail page

--- src/app/pages/reports/fleet/vehicle-report/revenue-detail/revenue-detail.component.ts
+++ src/app/pages/reports/fleet/vehicle-report/revenue-detail/revenue-detail.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from '../../../../../services';
+import { ApiService } from 'src/app/services';
 import { ToastrService } from 'ngx-toastr';
 import { ActivatedRoute } from "@angular/router";
 import * as moment from 'moment';
+import Constants from 'src/app/pages/fleet/constants';
+import * as _ from 'lodash';
 @Component({
   selector: 'app-revenue-detail',
   templateUrl: './revenue-detail.component.html',
@@ -14,6 +16,13 @@ export class RevenueDetailComponent implements OnInit {
   end = null;
   allData = [];
   vehicleData = [];
+  lastItemSK = ''
+  datee = ''
+  data = []
+  dataMessage = Constants.FETCHING_DATA;
+  loaded = false;
+  vehicleIdentification = ''
+  suggestedVehicles = [];
   public vehicleId;
   constructor(private apiService: ApiService, private toastr: ToastrService, private route: ActivatedRoute) { }
 
@@ -22,19 +31,93 @@ export class RevenueDetailComponent implements OnInit {
     this.start = moment().subtract(1, 'months').format('YYYY-MM-DD');
 
     this.vehicleId = this.route.snapshot.params[`vehicleId`];
-    this.fetchTripData()
-    this.fetchVehicleName()
-    console.log(' this.vehicleId', this.vehicleId)
+    this.fetchRevenueData()
+    this.fuelQuery()
+    // this.fetchVehicleName()
+    // console.log(' this.vehicleId', this.vehicleId)
   }
-  fetchVehicleName() {
-    this.apiService.getData(`vehicles/fetch/detail/${this.vehicleId}`).subscribe((result: any) => {
-      this.vehicleData = result.Items;
-    });
-  }
-  fetchTripData() {
-    this.apiService.getData(`vehicles/fetch/TripData?vehicle=${this.vehicleId}&startDate=${this.start}&endDate=${this.end}`).subscribe((result: any) => {
-      this.allData = result.Items
-      console.log(' this.allData', this.allData)
+  // fetchVehicleName() {
+  //   this.apiService.getData(`vehicles/fetch/detail/${this.vehicleId}`).subscribe((result: any) => {
+  //     this.vehicleData = result.Items;
+  //   });
+  // }
+  // getSuggestions = _.debounce(function (value) {
+
+  //   value = value.toLowerCase();
+  //   if (value != '') {
+  //     this.apiService
+  //       .getData(`vehicles/suggestion/${value}`)
+  //       .subscribe((result) => {
+
+  //         this.suggestedVehicles = result;
+  //       });
+  //   } else {
+  //     this.suggestedVehicles = []
+  //   }
+  // }, 800);
+  // setVehicle(vehicleIDs, vehicleIdentification) {
+  //   this.vehicleIdentification = vehicleIdentification;
+  //   this.vehicleId = vehicleIDs;
+  //   this.suggestedVehicles = [];
+  // }
+
+  fetchRevenueData() {
+    this.apiService.getData(`vehicles/fetch/revenue/report?vehicle=${this.vehicleId}&startDate=${this.start}&endDate=${this.end}&lastKey=${this.lastItemSK}&date=${this.datee}`).subscribe((result: any) => {
+      this.allData = this.allData.concat(result.Items)
+      // console.log(' this.allData', this.allData)
+      if (result.Items.length === 0) {
+        this.dataMessage = Constants.NO_RECORDS_FOUND
+      }
+      if (result.LastEvaluatedKey !== undefined) {
+        this.lastItemSK = encodeURIComponent(result.LastEvaluatedKey.tripSK);
+        this.datee = encodeURIComponent(result.LastEvaluatedKey.dateCreated)
+      }
+      else {
+        this.lastItemSK = 'end';
+      }
+      this.loaded = true;
     })
   }
+  onScroll() {
+    if (this.loaded) {
+
+      this.fetchRevenueData();
+      // this.fetchVehicleName();
+    }
+    this.loaded = false;
+  }
+  searchFilter() {
+    if (this.start != null && this.end != null) {
+      if (this.start != null && this.end == null) {
+        this.toastr.error('Please select both start and end dates.');
+        return false;
+      } else if (this.start == null && this.end != null) {
+        this.toastr.error('Please select both start and end dates.');
+        return false;
+      } else if (this.start > this.end) {
+        this.toastr.error('Start Date should be less then end date.');
+        return false;
+      }
+      else {
+        this.lastItemSK = '';
+        this.allData = []
+        this.dataMessage = Constants.FETCHING_DATA;
+        this.fetchRevenueData();
+        // this.fetchVehicleName();
+      }
+    } else {
+      return false;
+    }
+  }
+  fuelQuery() {
+    let veh = encodeURIComponent(JSON.stringify(this.vehicleId));
+    // console.log('veh', veh)
+    this.apiService.getData(`fuelEntries/get/vehicle/enteries?vehicle=${veh}&start=${this.start}&end=${this.end}`).subscribe((result: any) => {
+
+      this.data = result
+      // console.log('data', this.data)
+
+    })
+  }
+
 }
