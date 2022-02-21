c84164b21ee9a9f1905ff0be1a87da317d61b8c6
Pankaj Bassi <pankaj@fleethawks.com>
2022-02-21 22:17:50 +0530

    Set Page Heading

--- src/app/pages/accounts/manual-journal/manual-journal.module.ts
+++ src/app/pages/accounts/manual-journal/manual-journal.module.ts
@@ -1,45 +1,49 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
-import { NgbDateAdapter, NgbDateParserFormatter, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
-import { Injectable } from '@angular/core';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import { NgSelectModule } from '@ng-select/ng-select';
-import { AddJournalComponent } from './add-journal/add-journal.component';
-import { JournalListComponent } from './journal-list/journal-list.component';
-import { JournalDetailComponent } from './journal-detail/journal-detail.component';
-import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
+import { HttpClientModule } from "@angular/common/http";
+import {
+  NgbDateAdapter,
+  NgbDateParserFormatter,
+  NgbDateStruct,
+} from "@ng-bootstrap/ng-bootstrap";
+import { Injectable } from "@angular/core";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { NgSelectModule } from "@ng-select/ng-select";
+import { AddJournalComponent } from "./add-journal/add-journal.component";
+import { JournalListComponent } from "./journal-list/journal-list.component";
+import { JournalDetailComponent } from "./journal-detail/journal-detail.component";
+import { InfiniteScrollModule } from "ngx-infinite-scroll";
 
 /**
  * This Service handles how the date is represented in scripts i.e. ngModel.
  */
 @Injectable()
 export class CustomAdapter extends NgbDateAdapter<string> {
-
-  readonly DELIMITER = '-';
+  readonly DELIMITER = "-";
 
   fromModel(value: string): NgbDateStruct {
-    if (!value)
-      return null
+    if (!value) return null;
     let parts = value.split(this.DELIMITER);
     return {
-      year: + parseInt(parts[0]),
-      month: + parseInt(parts[1]),
-      day: + parseInt(parts[2])
-    }
+      year: +parseInt(parts[0]),
+      month: +parseInt(parts[1]),
+      day: +parseInt(parts[2]),
+    };
   }
 
-  toModel(date: NgbDateStruct): string // from internal model -> your mode
-  {
-    let month: any = '';
-    let day: any = '';
+  toModel(date: NgbDateStruct): string {
+    // from internal model -> your mode
+    let month: any = "";
+    let day: any = "";
     if (date) {
-      month = (date.month < 10) ? '0' + date.month : date.month;
-      day = (date.day < 10) ? '0' + date.day : date.day;
+      month = date.month < 10 ? "0" + date.month : date.month;
+      day = date.day < 10 ? "0" + date.day : date.day;
     }
-    return date ? date.year + this.DELIMITER + month + this.DELIMITER + day : null;
+    return date
+      ? date.year + this.DELIMITER + month + this.DELIMITER + day
+      : null;
   }
 }
 
@@ -48,8 +52,7 @@ export class CustomAdapter extends NgbDateAdapter<string> {
  */
 @Injectable()
 export class CustomDateParserFormatter extends NgbDateParserFormatter {
-
-  readonly DELIMITER = '/';
+  readonly DELIMITER = "/";
 
   parse(value: string): NgbDateStruct | null {
     if (value) {
@@ -58,34 +61,54 @@ export class CustomDateParserFormatter extends NgbDateParserFormatter {
         year: parseInt(date[2], 10),
         month: parseInt(date[1], 10),
         day: parseInt(date[0], 10),
-
       };
     }
     return null;
   }
 
   format(date: NgbDateStruct | null): string {
-    let month: any = '';
-    let day: any = '';
+    let month: any = "";
+    let day: any = "";
     if (date) {
-      month = (date.month < 10) ? '0' + date.month : date.month;
-      day = (date.day < 10) ? '0' + date.day : date.day;
+      month = date.month < 10 ? "0" + date.month : date.month;
+      day = date.day < 10 ? "0" + date.day : date.day;
     }
 
-    return date ? date.year + this.DELIMITER + month + this.DELIMITER + day : '';
+    return date
+      ? date.year + this.DELIMITER + month + this.DELIMITER + day
+      : "";
   }
 }
 
 const routes: Routes = [
-  { path: 'list', component: JournalListComponent },
-  { path: 'add', component: AddJournalComponent },
-  { path: 'edit/:journalID', component: AddJournalComponent },
-  { path: 'detail/:journalID', component: JournalDetailComponent },
+  {
+    path: "list",
+    component: JournalListComponent,
+    data: { title: "Manual Journal List" },
+  },
+  {
+    path: "add",
+    component: AddJournalComponent,
+    data: { title: "Add Manual Journal" },
+  },
+  {
+    path: "edit/:journalID",
+    component: AddJournalComponent,
+    data: { title: "Edit Manual Journal" },
+  },
+  {
+    path: "detail/:journalID",
+    component: JournalDetailComponent,
+    data: { title: "Manual Journal Detail" },
+  },
 ];
 
-
 @NgModule({
-  declarations: [AddJournalComponent, JournalListComponent, JournalDetailComponent],
+  declarations: [
+    AddJournalComponent,
+    JournalListComponent,
+    JournalDetailComponent,
+  ],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
@@ -98,6 +121,7 @@ const routes: Routes = [
   ],
   providers: [
     { provide: NgbDateAdapter, useClass: CustomAdapter },
-    { provide: NgbDateParserFormatter, useClass: CustomDateParserFormatter }]
+    { provide: NgbDateParserFormatter, useClass: CustomDateParserFormatter },
+  ],
 })
-export class ManualJournalModule { }
+export class ManualJournalModule {}
