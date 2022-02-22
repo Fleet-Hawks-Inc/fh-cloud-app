e493dedfe6f4f3cbe0854979b0dabb36a6f7f110
Jaspreet kaur <jaspreet@fleethawks.com>
2022-02-22 09:41:29 +0530

    fix: manage vehicle conflict resolved

--- src/app/pages/reports/fleet/vehicle-report/mange-vehicle.module.ts
+++ src/app/pages/reports/fleet/vehicle-report/mange-vehicle.module.ts
@@ -1,42 +1,87 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
-import { NgbDateAdapter, NgbDateParserFormatter, NgbDateStruct, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
+import { HttpClientModule } from "@angular/common/http";
+import {
+  NgbDateAdapter,
+  NgbDateParserFormatter,
+  NgbDateStruct,
+  NgbModule,
+} from "@ng-bootstrap/ng-bootstrap";
 
-import { NgSelectModule } from '@ng-select/ng-select';
-import { NgxSpinnerModule } from 'ngx-spinner';
-import { ChartsModule } from 'ng2-charts';
-import { unsavedChangesGuard } from 'src/app/guards/unsaved-changes.guard';
-import { Injectable } from '@angular/core';
-import { OverviewComponent } from './overview/overview.component';
-import { SummaryComponent } from './summary/summary.component';
-import { DetailComponent } from './detail/detail.component';
-import { UtilizationComponent } from './utilization/utilization.component';
-import { InspectionComponent } from './inspection/inspection.component';
-import { MaintenanceComponent } from './maintenance/maintenance.component';
-import { InfiniteScrollModule } from 'ngx-infinite-scroll';
-import { ActivityListComponent } from './activity-list/activity-list.component';
-import { ActivityComponent } from './activity/activity.component';
+import { NgSelectModule } from "@ng-select/ng-select";
+import { NgxSpinnerModule } from "ngx-spinner";
+import { ChartsModule } from "ng2-charts";
+import { unsavedChangesGuard } from "src/app/guards/unsaved-changes.guard";
+import { Injectable } from "@angular/core";
+import { OverviewComponent } from "./overview/overview.component";
+import { SummaryComponent } from "./summary/summary.component";
+import { DetailComponent } from "./detail/detail.component";
+import { UtilizationComponent } from "./utilization/utilization.component";
+import { InspectionComponent } from "./inspection/inspection.component";
+import { MaintenanceComponent } from "./maintenance/maintenance.component";
+import { InfiniteScrollModule } from "ngx-infinite-scroll";
+import { ActivityListComponent } from "./activity-list/activity-list.component";
+import { ActivityComponent } from "./activity/activity.component";
+import { ProvinceMilesComponent } from "./province-miles/province-miles.component";
+import { ProvinceSummaryComponent } from "./province-summary/province-summary.component";
 import { ExpenseListComponent } from './expense-list/expense-list.component';
 import { ExpenseComponent } from './expense/expense.component';
-import { ProvinceMilesComponent } from './province-miles/province-miles.component';
-import { ProvinceSummaryComponent } from './province-summary/province-summary.component';
-
 const routes: Routes = [
-  { path: 'overview', component: OverviewComponent },
-  { path: 'summary', component: SummaryComponent },
-  { path: 'detail', component: DetailComponent },
-  { path: 'utilization', component: UtilizationComponent },
-  { path: 'inspection', component: InspectionComponent },
-  { path: 'maintenance', component: MaintenanceComponent },
-  { path: 'activity/:vehicleId', component: ActivityComponent },
-  { path: 'activity-list', component: ActivityListComponent },
+  {
+    path: "overview",
+    component: OverviewComponent,
+    data: { title: "Vehicle Reports" },
+  },
+  {
+    path: "summary",
+    component: SummaryComponent,
+    data: { title: "Vehicle Summary Reports" },
+  },
+  {
+    path: "detail",
+    component: DetailComponent,
+    data: { title: "Vehicle Detail Reports" },
+  },
+  {
+    path: "utilization",
+    component: UtilizationComponent,
+    data: { title: "Utilization Reports" },
+  },
+  {
+    path: "inspection",
+    component: InspectionComponent,
+    data: { title: "Inspection Reports" },
+  },
+  {
+    path: "maintenance",
+    component: MaintenanceComponent,
+    data: { title: "Vehicle Maintenance Reports" },
+  },
+  {
+    path: "activity/:vehicleId",
+    component: ActivityComponent,
+    data: { title: "Vehicle Activity Reports" },
+  },
+
+  {
+    path: "activity-list",
+    component: ActivityListComponent,
+    data: { title: "Vehicle Activity List Reports" },
+  },
+  {
+    path: "province-miles",
+    component: ProvinceMilesComponent,
+    data: { title: "Vehicle Province Miles Reports" },
+  },
+  {
+    path: "province-summary",
+    component: ProvinceSummaryComponent,
+    data: { title: "Vehicle Province Summary Reports" },
+  },
   { path: 'expense-list', component: ExpenseListComponent },
   { path: 'expense/:vehicleId', component: ExpenseComponent },
-  { path: 'province-miles', component: ProvinceMilesComponent },
-  { path: 'province-summary', component: ProvinceSummaryComponent },
 ];
 
 /**
@@ -44,24 +89,27 @@ const routes: Routes = [
  */
 @Injectable()
 export class CustomAdapter extends NgbDateAdapter<string> {
-
-  readonly DELIMITER = '-';
+  readonly DELIMITER = "-";
 
   fromModel(value: string): NgbDateStruct {
-    if (!value)
-      return null
+    if (!value) return null;
     let parts = value.split(this.DELIMITER);
     return {
-      year: + parseInt(parts[0]),
-      month: + parseInt(parts[1]),
-      day: + parseInt(parts[2])
-    }
+      year: +parseInt(parts[0]),
+      month: +parseInt(parts[1]),
+      day: +parseInt(parts[2]),
+    };
   }
 
-  toModel(date: NgbDateStruct): string // from internal model -> your mode
-  {
-    return date ? date.year + this.DELIMITER + ('0' + date.month).slice(-2)
-      + this.DELIMITER + ('0' + date.day).slice(-2) : null
+  toModel(date: NgbDateStruct): string {
+    // from internal model -> your mode
+    return date
+      ? date.year +
+      this.DELIMITER +
+      ("0" + date.month).slice(-2) +
+      this.DELIMITER +
+      ("0" + date.day).slice(-2)
+      : null;
   }
 }
 
@@ -70,8 +118,7 @@ export class CustomAdapter extends NgbDateAdapter<string> {
  */
 @Injectable()
 export class CustomDateParserFormatter extends NgbDateParserFormatter {
-
-  readonly DELIMITER = '/';
+  readonly DELIMITER = "/";
 
   parse(value: string): NgbDateStruct | null {
     if (value) {
@@ -80,14 +127,15 @@ export class CustomDateParserFormatter extends NgbDateParserFormatter {
         year: parseInt(date[2], 10),
         month: parseInt(date[1], 10),
         day: parseInt(date[0], 10),
-
       };
     }
     return null;
   }
 
   format(date: NgbDateStruct | null): string {
-    return date ? date.year + this.DELIMITER + date.month + this.DELIMITER + date.day : '';
+    return date
+      ? date.year + this.DELIMITER + date.month + this.DELIMITER + date.day
+      : "";
   }
 }
 
@@ -101,11 +149,11 @@ export class CustomDateParserFormatter extends NgbDateParserFormatter {
     MaintenanceComponent,
     ActivityListComponent,
     ActivityComponent,
+    ProvinceMilesComponent,
+    ProvinceSummaryComponent,
     ExpenseListComponent,
     ExpenseComponent,
-    ExpenseComponent,
-    ProvinceMilesComponent,
-    ProvinceSummaryComponent],
+  ],
 
   imports: [
     CommonModule,
@@ -117,10 +165,12 @@ export class CustomDateParserFormatter extends NgbDateParserFormatter {
     NgSelectModule,
     NgxSpinnerModule,
     ChartsModule,
-    InfiniteScrollModule
+    InfiniteScrollModule,
   ],
-  providers: [unsavedChangesGuard,
+  providers: [
+    unsavedChangesGuard,
     { provide: NgbDateAdapter, useClass: CustomAdapter },
-    { provide: NgbDateParserFormatter, useClass: CustomDateParserFormatter },]
+    { provide: NgbDateParserFormatter, useClass: CustomDateParserFormatter },
+  ],
 })
 export class ManageVehicleModule { }
