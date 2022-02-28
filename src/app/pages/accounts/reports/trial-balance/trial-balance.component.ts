e8d6a7cb20d0da5e91dbf472ce2cf9ce5df71b43
saurkarankita <ankita@fleethawks.com>
2022-02-28 11:53:19 +0000

    feat: detail page in trial report

--- src/app/pages/accounts/reports/trial-balance/trial-balance.component.ts
+++ src/app/pages/accounts/reports/trial-balance/trial-balance.component.ts
@@ -55,7 +55,7 @@ export class TrialBalanceComponent implements OnInit {
     totalDCAD = 0;
     totalCCAD = 0
     allExportData = [];
-    lastExportSk="";
+    lastExportSk = "";
     data1 = [];
     cadDebitTotal = 0;
     cadCreditTotal = 0;
@@ -72,97 +72,77 @@ export class TrialBalanceComponent implements OnInit {
     lastTimestamp: string;
     lastKey: "";
     dataMsgCad: string;
-     currency = 'CAD';
-     transactionLogCAD =[];
+    currency = 'CAD';
+    transactionLogCAD = [];
     creditTotal = 0;
-    
+    result: any;
+
 
     constructor(private accountService: AccountService, private toaster: ToastrService) { }
 
     ngOnInit() {
         this.filter.endDate = moment().format("YYYY-MM-DD");
         this.filter.startDate = moment().subtract(15, 'day').format('YYYY-MM-DD');
-       this.fetchAccounts();
+        this.fetchAccounts();
         this.fetchAccountClassByIDs();
         this.getAcClasses();
-       // this.fetchCOA();
-        //this.logsCADPaging();
     }
-    
-    
+
+
     async fetchAccounts(refresh?: boolean) {
         if (refresh === true) {
             this.lastItemSK = "";
             this.accounts = [];
         }
         if (this.lastItemSK !== "end") {
-            // let name = null;
-            // let type = null;
-            // if (this.filter.actType !== null || this.filter.actName !== null) {
-            //     if (this.filter.actType !== null && this.filter.actType !== "") {
-            //         type = this.filter.actType;
-            //     }https://www.google.com/search?q=how+to+calculate+total+of+array+&sxsrf=APq-WBvNSQgtLqbFQsw1P_f2q8speYTAPA%3A1645847832901&ei=GKUZYve3NpLr0AT_2KTwAw&ved=0ahUKEwj3o7z7vJz2AhWSNZQKHX8sCT4Q4dUDCA4&uact=5&oq=how+to+calculate+total+of+array+&gs_lcp=Cgdnd3Mtd2l6EAMyBAgjECcyBggAEBYQHjIGCAAQFhAeMgYIABAWEB4yBggAEBYQHjIGCAAQFhAeMgYIABAWEB4yBggAEBYQHjIGCAAQFhAeMgYIABAWEB46BwgjELADECc6BwgAEEcQsANKBAhBGABKBAhGGABQlT9YlT9goUhoAnABeACAAdEBiAHRAZIBAzItMZgBAKABAcgBCsABAQ&sclient=gws-wiz
-            //     if (this.filter.actName !== null && this.filter.actName !== "") {
-            //         name = this.filter.actName.toLowerCase();
-            //     }
-            //     this.dataMessage = Constants.FETCHING_DATA;
-            // }
             this.accountService.getData(`chartAc/get/coa/${this.currency}/?lastKey=${this.lastItemSK}&start=${this.filter.startDate}&end=${this.filter.endDate}`)
-            .subscribe(async (result: any) => {
-              if(result.data.length === 0){
-                  this.dataMessage = Constants.NO_RECORDS_FOUND;
-              }
-              if(result.data.length > 0){
-                  result.data.map((v) => {
-                      this.accounts.push(v);
-                  });
-                    this.lastItemSK = result.lastKey;
-                    const newArray: any = _.sortBy(this.accounts, ["accountNo"]);
-                    this.accounts = newArray;
-                    console.log('array1',newArray)
-                    console.log('array2',this.accounts)
-                    console.log('array3',result.data)
-                    
-for (var i=0;i<this.accounts.length;i++){ 
-   if(this.currTab === 'CAD'){
-        this.currency = 'CAD'
-    if(this.accounts[i].debit != null){    
-        this.cadDebitTotal += parseFloat(this.accounts[i].debit);
-    }
-     if(this.accounts[i].credit != null){    
-        this.cadCreditTotal += parseFloat(this.accounts[i].credit);
-    }
-    }
-    if(this.currTab === 'USD'){
-        this.currency = 'USD'
-        if(this.accounts[i].debit != null){    
-        this.usdDebitTotal += parseFloat(this.accounts[i].debit);
-    }
-     if(this.accounts[i].credit != null){    
-        this.usdCreditTotal += parseFloat(this.accounts[i].credit);
-     }
-    }
-}
-                    console.log('Cad Debit Total',this.cadDebitTotal)
-                    console.log('Cad Credit Total',this.cadCreditTotal)
-                     console.log('USD Debit Total',this.usdDebitTotal)
-                    console.log('USD Credit Total',this.usdCreditTotal)
-                    this.accounts = _.filter(this.accounts, function (o) {
-                        return o.actType != 'H';
-                    });
-                    this.accounts = _.filter(this.accounts, function (o) {
-                        return o.actType != 'S';
-                    });
-                    this.accounts = _.filter(this.accounts, function (o) {
-                        return o.actType != 'T';
-                    });
-                    this.loaded = true;
-                }
-            });
+                .subscribe(async (result: any) => {
+                    if (result.data.length === 0) {
+                        this.dataMessage = Constants.NO_RECORDS_FOUND;
+                    }
+                    if (result.data.length > 0) {
+                        result.data.map((v) => {
+                            this.accounts.push(v);
+                        });
+                        this.lastItemSK = result.lastKey;
+                        const newArray: any = _.sortBy(this.accounts, ["accountNo"]);
+                        this.accounts = newArray;
+                        for (var i = 0; i < result.data.length; i++) {
+                            if (this.currTab === 'CAD') {
+                                this.currency = 'CAD'
+                                if (result.data[i].debit != null) {
+                                    this.cadDebitTotal += parseFloat(result.data[i].debit);
+                                }
+                                if (result.data[i].credit != null) {
+                                    this.cadCreditTotal += parseFloat(result.data[i].credit);
+                                }
+                            }
+                            if (this.currTab === 'USD') {
+                                this.currency = 'USD'
+                                if (result.data[i].debit != null) {
+                                    this.usdDebitTotal += parseFloat(result.data[i].debit);
+                                }
+                                if (result.data[i].credit != null) {
+                                    this.usdCreditTotal += parseFloat(result.data[i].credit);
+                                }
+                            }
+                        }
+                        this.accounts = _.filter(this.accounts, function (o) {
+                            return o.actType != 'H';
+                        });
+                        this.accounts = _.filter(this.accounts, function (o) {
+                            return o.actType != 'S';
+                        });
+                        this.accounts = _.filter(this.accounts, function (o) {
+                            return o.actType != 'T';
+                        });
+                        this.loaded = true;
+                    }
+                });
         }
     }
 
-    
+
     //For Scrolling Page
     onScroll() {
         if (this.loaded) {
@@ -170,31 +150,24 @@ for (var i=0;i<this.accounts.length;i++){
             this.isLoadText = "Loading";
             this.fetchAccounts();
             this.lastItemSK = '';
-            
-           
-           // this.fetchCOA();
-            
         }
         this.loaded = false;
     }
-    
 
-searchFilter()
-{
-    if(this.filter.startDate !== null || this.filter.endDate !== null)
-    {
-         this.start = this.filter.startDate;
-         this.end = this.filter.endDate;
-         this.accounts = [];
-         this.lastItemSK = '';
-         this.dataMessage = Constants.FETCHING_DATA;
-         this.fetchAccounts();
-       //  this.fetchCOA();
+
+    searchFilter() {
+        if (this.filter.startDate !== null || this.filter.endDate !== null) {
+            this.start = this.filter.startDate;
+            this.end = this.filter.endDate;
+            this.accounts = [];
+            this.lastItemSK = '';
+            this.dataMessage = Constants.FETCHING_DATA;
+            this.fetchAccounts();
+        }
     }
-}
-  
-  
-  
+
+
+
     resetFilter() {
         this.dataMessage = Constants.FETCHING_DATA;
         this.start = null;
@@ -202,13 +175,11 @@ searchFilter()
         this.lastItemSK = "";
         this.accounts = [];
         this.fetchAccounts();
-     //   this.fetchCOA();
-
     }
-    
-    
-    
-    
+
+
+
+
     fetchAccountClassByIDs() {
         this.accountService
             .getData("chartAc/get/accountClass/list/all")
@@ -227,23 +198,21 @@ searchFilter()
     refreshClass() {
         this.getAcClasses();
     }
-    
-    
+
+
 
     //For Switching Tab
     changeTab(type) {
         this.currTab = type;
-        this.accounts= [];
+        this.accounts = [];
         if (this.currTab === "CAD") {
             this.currency = 'CAD'
             this.lastItemSK = '';
-           this.fetchAccounts();
-           
-           
+            this.fetchAccounts();
         } else if (this.currTab === "USD") {
             this.currency = 'USD'
-            this.lastItemSK= '';
-           this.fetchAccounts();
+            this.lastItemSK = '';
+            this.fetchAccounts();
         }
     }
 
@@ -255,17 +224,17 @@ searchFilter()
         let provArray = [];
         try {
             if (this.accounts.length > 0) {
-                
+
                 for (const element of this.accounts) {
                     let obj = {}
                     obj["Account Number"] = element.accountNo
                     obj["Account Name"] = element.accountName,
-                    obj["Credit"] = element.credit 
+                        obj["Credit"] = element.credit
                     obj["Debit"] = element.debit
                     dataObject.push(obj)
-                }                    
-               
-                console.log('dataObject',dataObject);
+                }
+
+                console.log('dataObject', dataObject);
                 let headers = Object.keys(dataObject[0]).join(',')
                 headers += '\n'
                 csvArray.push(headers)
@@ -286,7 +255,7 @@ searchFilter()
                     link.click();
                     document.body.removeChild(link);
                 }
-                console.log('Export',this.allExportData);
+                console.log('Export', this.allExportData);
                 this.exportLoading = false
             }
             else {
@@ -297,5 +266,5 @@ searchFilter()
             this.exportLoading = false;
         }
     }
-    
+
 }
