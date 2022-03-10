3661463348b5643afb481831366a21b0e9f5050c
Pratik Ganjare <89508246+PratikGanjareFleetHawks@users.noreply.github.com>
2022-03-10 17:29:42 +0530

    Update manage-settings.module.ts

--- src/app/pages/manage/settings/manage-settings.module.ts
+++ src/app/pages/manage/settings/manage-settings.module.ts
@@ -1,46 +1,3 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule, Routes } from '@angular/router';
-import { SettingsOverviewComponent } from './settings-overview/settings-overview.component';
-import { GeneralSettingsComponent } from './general-settings/general-settings.component';
-import { MyAccountComponent } from './my-account/my-account.component';
-import { DriverSettingComponent } from './driver-setting/driver-setting.component';
-import { VehicleSettingComponent } from './vehicle-setting/vehicle-setting.component';
-import { AssetsSettingComponent } from './assets-setting/assets-setting.component';
-import { FuelSettingComponent } from './fuel-setting/fuel-setting.component';
-import { RemindersSettingComponent } from './reminders-setting/reminders-setting.component';
-import { IssueSettingComponent } from './issue-setting/issue-setting.component';
-import { DispatchSettingComponent } from './dispatch-setting/dispatch-setting.component';
-import { ComplianceSettingComponent } from './compliance-setting/compliance-setting.component';
-import { SafetySettingComponent } from './safety-setting/safety-setting.component';
-import { NgSelectModule } from '@ng-select/ng-select';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FormsModule } from '@angular/forms';
-import { InfiniteScrollModule } from 'ngx-infinite-scroll';
-import { CustomerSettingComponent } from './customer-setting/customer-setting.component';
-import { ImportOverviewComponent } from './settings-overview/import-overview/import-overview.component';
-import { VehImportComponent } from './veh-import/veh-import.component';
-import { DoverviewComponent } from './settings-overview/doverview/doverview.component';
-import { ImportDriversComponent } from './import-drivers/import-drivers.component';
-const routes: Routes = [
-  { path: 'overview', component: SettingsOverviewComponent },
-  { path: 'doverview', component: DoverviewComponent},
-  { path: 'general', component: GeneralSettingsComponent },
-  { path: 'myaccount', component: MyAccountComponent },
-  { path: 'driver', component: DriverSettingComponent },
-  { path: 'import', component: ImportDriversComponent},
-  { path: 'vehicle', component: VehicleSettingComponent },
-  { path: 'asset', component: AssetsSettingComponent },
-  { path: 'fuel', component: FuelSettingComponent },
-  { path: 'reminders', component: RemindersSettingComponent },
-  { path: 'issue', component: IssueSettingComponent },
-  { path: 'dispatch', component: DispatchSettingComponent },
-  { path: 'compliance', component: ComplianceSettingComponent },
-  { path: 'safety', component: SafetySettingComponent },
-  { path: 'customer', component: CustomerSettingComponent },
-  { path: 'importoverview', component: ImportOverviewComponent },
-  { path: 'vehimport', component: VehImportComponent },
-]
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule, Routes } from "@angular/router";
@@ -61,6 +18,7 @@ import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { FormsModule } from "@angular/forms";
 import { InfiniteScrollModule } from "ngx-infinite-scroll";
 import { CustomerSettingComponent } from "./customer-setting/customer-setting.component";
+import { VehImportComponent } from './veh-import/veh-import.component';
 import { DoverviewComponent } from "./settings-overview/doverview/doverview.component";
 import { ImportDriversComponent } from "./import-drivers/import-drivers.component";
 
@@ -124,6 +82,10 @@ const routes: Routes = [
     component: CustomerSettingComponent,
     data: { title: "Customer Settings" },
   },
+  { 
+    path: 'vehimport', 
+    component: VehImportComponent
+  },
 ];
 
 @NgModule({
@@ -141,9 +103,8 @@ const routes: Routes = [
     ComplianceSettingComponent,
     SafetySettingComponent,
     CustomerSettingComponent,
-    ImportOverviewComponent,
-    VehImportComponent,
     DoverviewComponent,
+    VehImportComponent,
     ImportDriversComponent,
   ],
   imports: [
