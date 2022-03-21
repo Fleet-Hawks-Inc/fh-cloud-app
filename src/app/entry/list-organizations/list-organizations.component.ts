2b659662a64a4bde9ff670cb530579a1e61d65b7
Pankaj Bassi <pankaj@fleethawks.com>
2022-03-22 03:33:42 +0530

    Organization Page and Dashboard UI Points

---
+++ src/app/entry/list-organizations/list-organizations.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-organizations',
+  templateUrl: './list-organizations.component.html',
+  styleUrls: ['./list-organizations.component.css']
+})
+export class ListOrganizationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
