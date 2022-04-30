b1e8bcbb49d286fc1760cb42f500c1635f9c0d8b
saurkarankita <89508346+saurkarankita@users.noreply.github.com>
2022-04-30 16:26:10 +0530

    build fixed

--- src/app/pages/reports/fleet/drivers/driver-summary/driver-summary.component.ts
+++ src/app/pages/reports/fleet/drivers/driver-summary/driver-summary.component.ts
@@ -41,7 +41,7 @@ export class DriverSummaryComponent implements OnInit {
     loadMsg: string = Constants.NO_LOAD_DATA;
     isSearch = false;
     _selectedColumns: any[];
-    driverOptions: any[];
+    statusOptions: any[];
     listView = true;
     visible = true;
     get = _.get;
@@ -73,7 +73,7 @@ export class DriverSummaryComponent implements OnInit {
         this.fetchDriversCount();
         this.fetchPagination();
         this.setToggleOptions();
-        this.setDriverOptions();
+        this.setAssetOptions();
     }
     
 
@@ -82,8 +82,8 @@ export class DriverSummaryComponent implements OnInit {
         this.selectedColumns = this.dataColumns;
     }
     
-    setDriverOptions() {
-    this.driverOptions = [{ "value": "active", "name": "Active" },
+    setAssetOptions() {
+    this.statusOptions = [{ "value": "active", "name": "Active" },
     { "value": "inActive", "name": "InActive" },
     ];
     }
