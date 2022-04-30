f4092b07c425c350dcd97e644c9a7a97ef2af727
saurkarankita <ankita@fleethawks.com>
2022-04-30 09:49:12 +0000

    feat: prime ng in driver summary report

--- src/app/pages/reports/fleet/drivers/driver-summary/driver-summary.component.ts
+++ src/app/pages/reports/fleet/drivers/driver-summary/driver-summary.component.ts
@@ -1,10 +1,8 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { ApiService } from 'src/app/services';
 import { environment } from 'src/environments/environment';
 import Constants from 'src/app/pages/fleet/constants';
-import { result } from 'lodash';
-import { timeStamp } from 'console';
 import { ToastrService } from 'ngx-toastr';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { HereMapService } from 'src/app/services/here-map.service';
@@ -12,19 +10,22 @@ import { HereMapService } from 'src/app/services/here-map.service';
 import * as moment from 'moment'
 import * as _ from 'lodash';
 import { NgSelectComponent } from '@ng-select/ng-select';
+import { Table } from 'primeng/table';
+import { OnboardDefaultService } from 'src/app/services/onboard-default.service';
 @Component({
     selector: 'app-driver-summary',
     templateUrl: './driver-summary.component.html',
     styleUrls: ['./driver-summary.component.css']
 })
 export class DriverSummaryComponent implements OnInit {
+    @ViewChild('dt') table: Table;
+    
     @ViewChild(NgSelectComponent) ngSelectComponent: NgSelectComponent;
+    environment = environment.isFeatureEnabled;
     dataMessage: string = Constants.FETCHING_DATA;
     drivers: any = [];
+    title = "Driver Summary";
     driverID = '';
-    firstName = '';
-    lastName = '';
-    middleName = '';
     driversCount = {
         total: '',
         active: '',
@@ -39,11 +40,64 @@ export class DriverSummaryComponent implements OnInit {
     loaded = false;
     loadMsg: string = Constants.NO_LOAD_DATA;
     isSearch = false;
-    constructor(private apiService: ApiService, private router: Router, private toastr: ToastrService, private spinner: NgxSpinnerService) { }
-    ngOnInit() {
+    _selectedColumns: any[];
+    statusOptions: any[];
+    listView = true;
+    visible = true;
+    get = _.get;
+    
+     dataColumns = [
+        { field: 'firstName', header: 'First Name', type: "text" },
+        { field: 'lastName', header: 'Last Name', type: "text" },
+        { field: 'email', header: 'Email', type: "text" },
+        { field: 'userName', header: 'Username', type: "text" },
+        { field: 'driverType', header: 'Driver Type', type: "text" },
+        { field: 'startDate', header: 'Start Date', type: "text" },
+        { field: 'DOB', header: 'Date of Birth', type: "text" }, 
+        { field: 'CDL_Number', header: 'CDL#', type: "text" },
+        { field: 'licenceExpiry', header: 'Licence Expiry', type: "text" },     
+        { field: 'licStateName', header: 'Licence Province', type: "text" }, 
+        { field: 'phone', header: 'Phone', type: "text" },                
+        { field: "driverStatus", header: 'Status', type: 'text' },
+
+    ];
+    
+    constructor(private apiService: ApiService, 
+    private router: Router,
+    private toastr: ToastrService,
+    private spinner: NgxSpinnerService,
+    private hereMap: HereMapService,
+    private onboard: OnboardDefaultService) { }
+    
+    async ngOnInit(): Promise<void> {
         this.fetchDriversCount();
         this.fetchPagination();
+        this.setToggleOptions();
+        this.setAssetOptions();
     }
+    
+
+
+     setToggleOptions() {
+        this.selectedColumns = this.dataColumns;
+    }
+    
+    setAssetOptions() {
+    this.statusOptions = [{ "value": "active", "name": "Active" },
+    { "value": "inActive", "name": "InActive" },
+    ];
+    }
+    
+     @Input() get selectedColumns(): any[] {
+    return this._selectedColumns;
+  }
+  
+  set selectedColumns(val: any[]) {
+    //restore original order
+    this._selectedColumns = this.dataColumns.filter(col => val.includes(col));
+
+  }
+    
     async fetchPagination(refresh?: boolean) {
         if (refresh === true) {
             this.lastItemSK = '';
@@ -66,18 +120,21 @@ export class DriverSummaryComponent implements OnInit {
             this.isSearch = false;
         }
     }
-    onScroll() {
+    onScroll = async (event: any) => {
         if (this.loaded) {
             this.fetchPagination();
         }
         this.loaded = false;
     }
+    
     fetchDriversCount() {
         this.apiService.getData(`drivers/fetch/driverCount`).subscribe((result: any) => {
             this.driversCount = result;
         })
     }
+    
     getSuggestions = _.debounce(function (value) {
+
         value = value.toLowerCase();
         if (value != "") {
             this.loadMsg = Constants.LOAD_DATA;
@@ -85,6 +142,7 @@ export class DriverSummaryComponent implements OnInit {
                 .getData(`drivers/get/suggestions/${value}`)
                 .subscribe((result) => {
                     if (result.length === 0) {
+                        this.suggestedDrivers = [];
                         this.loadMsg = Constants.NO_LOAD_FOUND;
                     }
                     if (result.length > 0) {
@@ -97,6 +155,8 @@ export class DriverSummaryComponent implements OnInit {
                             return v;
                         });
                         this.suggestedDrivers = result;
+                    } else {
+
                     }
                 });
         } else {
@@ -115,7 +175,7 @@ export class DriverSummaryComponent implements OnInit {
         if (this.driverID !== '' || this.driverStatus !== null) {
             this.isSearch = true;
             this.drivers = [];
-            this.suggestedDrivers = [];
+           // this.suggestedDrivers = [];
             this.lastItemSK = '';
             this.dataMessage = Constants.FETCHING_DATA;
             this.fetchPagination();
@@ -124,25 +184,46 @@ export class DriverSummaryComponent implements OnInit {
             return false;
         }
     }
-
+    
+    clearInput() {
+    this.suggestedDrivers = null;
+  }
+  
+  clearSuggestions() {
+    this.driverName = '';
+  }
 
     resetDriver() {
         if (this.driverID !== '' || this.driverStatus !== null || this.lastItemSK !== '') {
-            this.ngSelectComponent.handleClearClick();
+            //this.ngSelectComponent.handleClearClick();
             this.driverName = '';
             this.isSearch = true;
             this.driverID = '';
             this.driverStatus = null;
             this.lastItemSK = '';
             this.drivers = [];
+            this.loaded = false;
             this.dataMessage = Constants.FETCHING_DATA;
-            this.suggestedDrivers = [];
+            this.suggestedDrivers = null;
             this.fetchPagination();
         }
         else {
             return false;
         }
     }
+    
+     refreshData() {
+        this.drivers = [];
+        this.driverID = '';
+        this.driverStatus = null;
+        this.driverName = '';
+        this.lastItemSK = '';
+        this.loaded = false;
+        this.fetchPagination();
+        this.dataMessage = Constants.FETCHING_DATA;
+
+    }
+    
     generateDriverCSV() {
         if (this.fullExportDriver.length > 0) {
             let dataObject = []
@@ -201,5 +282,13 @@ export class DriverSummaryComponent implements OnInit {
         } else {
             this.requiredExport();
         }
+    }
+    
+    /**
+     * Clears the table filters
+     * @param table Table 
+     */
+    clear(table: Table) {
+        table.clear();
     }
 }
