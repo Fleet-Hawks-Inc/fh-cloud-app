6299d9dcef451638b8c4dd7a1987c14fcdd38f96
PratikGanjareFleetHawks <pratik@fleethawks.com>
2022-05-12 09:04:28 +0000

    fix: build

--- src/app/pages/manage/users/user-details/user-details.component.ts
+++ src/app/pages/manage/users/user-details/user-details.component.ts
@@ -19,6 +19,7 @@ export class UserDetailsComponent implements OnInit {
   Asseturl = this.apiService.AssetUrl;
   contactID = '';
   contactsObject: any = {};
+  vendor: any;
   profilePath = '';
   userData = {
     companyName: '',
