6ada116b063fd8340100db1b7e6892e663f12f8a
Amitoj Singh <amitojsingh@fleethawks.com>
2022-05-24 12:48:27 -0400

    fix: added get call for scheduler

--- src/app/pages/dispatch/scheduler/scheduler-list/scheduler-list.component.ts
+++ src/app/pages/dispatch/scheduler/scheduler-list/scheduler-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import Constants from 'src/app/pages/fleet/constants';
+import { ApiService } from 'src/app/services';
 
 @Component({
   selector: 'app-scheduler-list',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SchedulerListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private apiService: ApiService) { }
 
+  lastEvaluatedKey='';
+  schedules=[];
+  dataMessage='';
   ngOnInit(): void {
+    this.initData();
   }
 
+  resetData(){
+
+  }
+  async initData(refresh?: boolean){
+    if(refresh===true){
+      this.lastEvaluatedKey='';
+      this.schedules=[];
+    }
+  const result=await this.apiService.getData("orders/schedule?lastKey="+this.lastEvaluatedKey).toPromise();
+  if(result.data.length==0){
+    this.dataMessage=Constants.NO_RECORDS_FOUND
+  }
+  if(result.nextKey!==undefined){
+    this.lastEvaluatedKey=result.nextKey;
+  }
+  else{
+    this.lastEvaluatedKey=undefined
+  }
+this.schedules=this.schedules.concat(result.data)
+console.log(this.schedules)
+  }
 }
