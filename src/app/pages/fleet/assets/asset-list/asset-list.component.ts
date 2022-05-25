c7cfaaf8b90ca6cd114942e1a7395748c2b64f45
saurkarankita <89508346+saurkarankita@users.noreply.github.com>
2022-05-25 17:00:10 +0530

    Update asset-list.component.ts

--- src/app/pages/fleet/assets/asset-list/asset-list.component.ts
+++ src/app/pages/fleet/assets/asset-list/asset-list.component.ts
@@ -1,210 +1,103 @@
-import { Component, OnInit, Input, ViewChild } from '@angular/core';
-import { ApiService } from '../../../../services';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, ViewChild, Input } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { NgxSpinnerService } from 'ngx-spinner';
-import { HereMapService } from '../../../../services';
-import { HttpClient } from '@angular/common/http';
-import Constants from '../../constants';
-import { environment } from '../../../../../environments/environment';
-import { OnboardDefaultService } from '../../../../services/onboard-default.service';
+import { HereMapService } from 'src/app/services/here-map.service';
 import * as _ from 'lodash';
-import { Table } from 'primeng/table';
+import * as moment from 'moment'
+import { environment } from '../../../../../../environments/environment';
+import { ApiService } from '../../../../../services';
 import { NgSelectComponent } from '@ng-select/ng-select';
-import * as moment from 'moment';
+import { Table } from 'primeng/table';
+import { OnboardDefaultService } from '../../../../../services/onboard-default.service';
+import Constants from 'src/app/pages/fleet/constants';
 declare var $: any;
-
 @Component({
-  selector: 'app-asset-list',
-  templateUrl: './asset-list.component.html',
-  styleUrls: ['./asset-list.component.css'],
+  selector: 'app-expense-list',
+  templateUrl: './expense-list.component.html',
+  styleUrls: ['./expense-list.component.css']
 })
-export class AssetListComponent implements OnInit {
+export class ExpenseListComponent implements OnInit {
   @ViewChild('dt') table: Table;
-
   @ViewChild(NgSelectComponent) ngSelectComponent: NgSelectComponent;
+  liveModalTimeout: any;
+  liveStreamVehicle: string;
   environment = environment.isFeatureEnabled;
+  url: any;
   dataMessage: string = Constants.FETCHING_DATA;
-  allAssetTypes: any;
-  assetTypesObjects: any = {};
-  title = 'Assets List';
-  mapView = false;
+  title = 'Vehicle List';
+  vehicles = [];
+  suggestedVehicles = [];
+  vehicleID = '';
+  currentStatus = null;
+  vehicleIdentification = '';
+  allOptions: any = {};
+  groupsList: any = {};
+  vehicleModelList: any = {};
+  vehicleManufacturersList: any = {};
+  serviceProgramsList: any = {};
+  driversList: any = {};
+  vendorsList: any = {};
+  currentView = 'list';
+  totalRecords = 20;
+  lastEvaluatedKey = '';
+  disableSearch = false;
+  vehicleTypeObects: any = {};
+  lastItemSK = ''
+  loaded = false
+  isSearch = false;
+  _selectedColumns: any[];
+  driverOptions: any[];
   listView = true;
   visible = true;
-  allData = [];
-  autoCarrier = [];
-  beverageRack = [];
-  flatbed = [];
-  controlledTemp = [];
-  gondola = [];
-  hopper = [];
-  horseTrailer = [];
-  liveStock = [];
-  lowboy = [];
-  stake = [];
-  stepDeck = [];
-  tanker = [];
-  checked = false;
-  isChecked = false;
-  headCheckbox = false;
-  selectedAssetID: any;
-  assetCheckCount = null;
-
-  allOptions: any = {};
-  reeferOptions: any = {};
-  dryboxOptions: any = {};
-  flatbedOptions: any = {};
-  curtainOptions: any = {};
-  closeResult = '';
-
-  response: any = '';
-  hasError = false;
-  hasSuccess = false;
-  Error = '';
-  Success = '';
-
-  hideShow = {
-    vin: true,
-    assetName: true,
-    type: true,
-    plateNo: true,
-    lastLocation: true,
-    year: true,
-    make: true,
-    model: false,
-    ownership: true,
-    currentStatus: true,
-    group: false,
-    aceID: false,
-    aciID: false,
-    gvwr: false,
-    gawr: false,
-    companyName: true,
-    safetyDate: true
-  }
-
-  message: any;
-  groupsList: any = {};
-
-  suggestedAssets = [];
-  assetID = '';
-  assetType = null;
-  assetIdentification = '';
-  assetTypeList: any = {};
-  totalRecords = 10;
-  pageLength = 10;
-  lastEvaluatedKey = '';
-  manufacturersObjects: any = {};
-  modelsObjects: any = {};
-
-  assetNext = false;
-  assetPrev = true;
-  assetDraw = 0;
-  assetPrevEvauatedKeys = [''];
-  assetStartPoint = 1;
-  assetEndPoint = this.pageLength;
-  contactsObjects = [];
-  loaded = false
-  lastItemSK = ''
-
-  loadMsg: string = Constants.NO_LOAD_DATA;
-  assetOptions: any[];
-  _selectedColumns: any[];
   get = _.get;
-  isSearch = false;
-
-  dataColumns = [
-    { width: '11%', field: 'assetIdentification', header: 'Asset Name/Number', type: "text" },
-    { width: '8%', field: 'VIN', header: 'VIN', type: "text" },
-    { width: '8%', field: 'assetType', header: 'Asset Type', type: "text" },
-    { width: '6%', field: 'manufacturer', header: 'Make', type: "text" },
-    { width: '12%', field: 'licencePlateNumber', header: 'Licence Plate Number', type: "text" },
-    { width: '6%', field: 'year', header: 'Year', type: "text" },
-    { width: '8%', field: 'ownerShip', header: 'Ownership', type: "text" },
-    { width: '10%', field: 'operatorCompany', header: 'Company Name', type: "text" },
-    { width: '10%', field: 'annualSafetyDate', header: 'Annual Safety Date', type: "text" },
-    { width: '6%', field: "currentStatus", header: 'Status', type: 'text' },
-    { width: '8%', field: 'isImport', header: 'Added By', type: "text" },
-
+  
+   dataColumns = [
+    { field: 'vehicleIdentification', header: 'Vehicle Name/Number', type: "text" },
+    { width: '6%', field: 'VIN', header: 'VIN', type: "text" },
+    { width: '5%', field: 'startDate', header: 'Start Date', type: "text" },
+    { width: '5%', field: 'manufacturerID', header: 'Make', type: "text" },
+    { width: '5%', field: 'modelID', header: 'Model', type: "text" },
+    { width: '5%', field: 'year', header: 'Year', type: "text" },
+    { width: '9%', field: 'annualSafetyDate', header: 'Annual Safety Date', type: "text" },
+    { width: '7%', field: 'ownership', header: 'Ownership', type: "text" },
+    { width: '8%', field: 'driverList.driverID', header: 'Driver Assigned', type: 'text' },
+    { width: '10%', field: 'driverList.teamDriverID', header: 'Team Driver Assigned', type: 'text' },
+    { width: '7%', field: 'plateNumber', header: 'Plate Number', type: "text" },
+    { width: '3%', field: 'currentStatus', header: 'Status', type: 'text' },
   ];
+    
 
-  constructor(
-    private apiService: ApiService,
-    private spinner: NgxSpinnerService,
-    private toastr: ToastrService,
-    private httpClient: HttpClient,
-    private hereMap: HereMapService,
-    private onboard: OnboardDefaultService) { }
-
-  async ngOnInit(): Promise<void> {
-    this.setToggleOptions();
-    this.setAssetOptions();
-    this.onboard.checkInspectionForms();
+  constructor(private apiService: ApiService, 
+  private httpClient: HttpClient,
+  private router: Router,
+  protected _sanitizer: DomSanitizer,
+  private toastr: ToastrService,
+  private spinner: NgxSpinnerService,
+  private hereMap: HereMapService,) {
+  }
+  ngOnInit() {
     this.fetchGroups();
-    await this.initDataTable();
-    this.fetchContacts();
-
+    this.fetchDriversList();
+    this.fetchVendorList();
+    this.initDataTable();
+    this.setToggleOptions();
   }
-
-  setToggleOptions() {
-    this.selectedColumns = this.dataColumns;
-  }
-  setAssetOptions() {
-    this.assetOptions = [{ "value": "container", "name": "Container" },
-    { "value": "double_drop", "name": "Double Drop" },
-    { "value": "dump_trailer", "name": "Dump Trailer" },
-    { "value": "flat_bed", "name": "Flat bed" },
-    { "value": "lowboy", "name": "Lowboy" },
-    { "value": "stepDeck", "name": "StepDeck" },
-    { "value": "removable_gooseneck", "name": "Removable Gooseneck" },
-    { "value": "dry_van", "name": "Dry Van" },
-    { "value": "reefer", "name": "Reefer" },
-    { "value": "power_only", "name": "Power Only" },
-    { "value": "conestoga_trailer", "name": "Conestoga Trailer" },
-    { "value": "side_kit_trailer", "name": "Side Kit Trailer" },
-    { "value": "enclosed_trailer", "name": "Enclosed Trailer" },
-    { "value": "scrap_trailer", "name": "Scrap Trailer" },
-    { "value": "semi_trailer", "name": "Semi Trailer" },
-    { "value": "chassis", "name": "Chassis" },
-    { "value": "tank_trailer", "name": "Tank Trailer" },
-    ];
-  }
-  @Input() get selectedColumns(): any[] {
-    return this._selectedColumns;
-  }
-
-  set selectedColumns(val: any[]) {
-    //restore original order
-    this._selectedColumns = this.dataColumns.filter(col => val.includes(col));
-  }
-
   getSuggestions = _.debounce(function (value) {
+
     value = value.toLowerCase();
     if (value != '') {
-      this.loadMsg = Constants.LOAD_DATA;
       this.apiService
-        .getData(`assets/suggestion/${value}`)
+        .getData(`vehicles/suggestion/${value}`)
         .subscribe((result) => {
-          if (result.length === 0) {
-            this.suggestedAssets = [];
-            this.loadMsg = Constants.NO_LOAD_FOUND;
-          }
-
-          if (result.length > 0) {
-            this.suggestedAssets = result;
-          } else {
-            this.suggestedAssets = [];
-          }
+          this.suggestedVehicles = result;
         });
     } else {
-      this.suggestedAssets = [];
+      this.suggestedVehicles = []
     }
   }, 800);
-
-  setAsset(assetID, assetIdentification) {
-    this.assetIdentification = assetIdentification;
-    this.assetID = assetIdentification;
-    this.suggestedAssets = [];
-  }
 
   fetchGroups() {
     this.apiService.getData('groups/get/list').subscribe((result: any) => {
@@ -212,86 +105,75 @@ export class AssetListComponent implements OnInit {
     });
   }
 
-  // fetchManufacturesByIDs() {
-  //   this.apiService.getData('assetManufacturers/get/list').subscribe((result: any) => {
-  //     this.manufacturersObjects = result;
-  //   });
-  // }
+   setToggleOptions() {
+        this.selectedColumns = this.dataColumns;
+    }
+    
+     @Input() get selectedColumns(): any[] {
+    return this._selectedColumns;
+  }
+  
+  set selectedColumns(val: any[]) {
+    //restore original order
+    this._selectedColumns = this.dataColumns.filter(col => val.includes(col));
 
-  // fetchModalsByIDs() {
-  //   this.apiService.getData('assetModels/get/list').subscribe((result: any) => {
-  //     this.modelsObjects = result;
-  //   });
-  // }
+  }
 
-  fetchContacts() {
-    this.apiService.getData('contacts/get/list/ownerOperator').subscribe((result: any) => {
-      this.contactsObjects = result;
+  fetchVehicleModelList() {
+    this.apiService.getData('vehicleModels/get/list').subscribe((result: any) => {
+      this.vehicleModelList = result;
     });
   }
 
-  someClickHandler(info: any): void {
-    this.message = info.id + ' - ' + info.firstName;
+  fetchDriversList() {
+    this.apiService.getData('drivers/get/list').subscribe((result: any) => {
+      this.driversList = result;
+    });
   }
 
 
-  deleteAsset(eventData) {
-    ;
-    // }
-    if (confirm('Are you sure you want to delete?') === true) {
-      // let record = {
-      //   date: eventData.createdDate,
-      //   time: eventData.createdTime,
-      //   eventID: eventData.assetID,
-      //   status: eventData.currentStatus
-      // }
-      this.apiService.deleteData(`assets/delete/${eventData.assetID}/${eventData.assetIdentification}`).subscribe((result: any) => {
-        this.allData = [];
-        this.assetDraw = 0;
-        this.dataMessage = Constants.FETCHING_DATA;
-        this.lastEvaluatedKey = '';
-        this.initDataTable();
-        this.toastr.success('Asset Deleted Successfully!');
-      });
-    }
-
-  }
-  mapShow() {
-    this.mapView = true;
-    this.listView = false;
-    setTimeout(() => {
-      this.hereMap.mapInit();
-    }, 500);
+  fetchVendorList() {
+    this.apiService.getData('contacts/get/list/vendor').subscribe((result: any) => {
+      this.vendorsList = result;
+    });
   }
 
-  valuechange() {
-    this.visible = !this.visible;
+  setVehicle(vehicleID, vehicleIdentification) {
+    this.vehicleIdentification = vehicleIdentification;
+    this.vehicleID = vehicleIdentification;
+    this.suggestedVehicles = [];
   }
-
-  async initDataTable() {
+  
+ 
+  initDataTable() {
     if (this.lastEvaluatedKey !== 'end') {
-      let result = await this.apiService.getData('assets/fetch/records?asset=' + this.assetIdentification + '&assetType=' + this.assetType + '&lastKey=' + this.lastEvaluatedKey).toPromise();
-      if (result.data.length === 0) {
-        this.dataMessage = Constants.NO_RECORDS_FOUND;
-        this.loaded = true;
-      }
-      result.data.map((v) => {
-        v.url = `/fleet/assets/detail/${v.assetID}`;
-      });
-      if (result.nextPage !== undefined) {
-        this.lastEvaluatedKey = encodeURIComponent(result.nextPage);
-      }
-      else {
-        this.lastEvaluatedKey = 'end'
-      }
-      this.allData = this.allData.concat(result.data)
-      this.loaded = true;
-      this.isSearch = false;
+      this.apiService.getData('vehicles/fetch/records?vehicle=' + this.vehicleID + '&status=' + this.currentStatus + '&lastKey=' + this.lastEvaluatedKey)
+        .subscribe(async (result: any) => {
+          this.dataMessage = Constants.FETCHING_DATA
+          if (result.data.length === 0) {
+            this.disableSearch = false;
+            this.dataMessage = Constants.NO_RECORDS_FOUND
+          }
+          result[`data`].map((v: any) => {
+            v.url = `/reports/fleet/vehicles/expense/${v.vehicleID}`;
+          })
+          if (result.data.length > 0) {
+            this.disableSearch = false;
+            if (result.nextPage !== undefined) {
+              this.lastEvaluatedKey = encodeURIComponent(result.nextPage);
+            }
+            else {
+              this.lastEvaluatedKey = 'end'
+            }
+            this.vehicles = this.vehicles.concat(result.data)
+
+            this.loaded = true;
+          }
+        });
     }
   }
-
-
-  onScroll = async (event: any) => {
+  
+  onScroll= async (event: any) =>{
     if (this.loaded) {
       this.initDataTable();
     }
@@ -299,110 +181,53 @@ export class AssetListComponent implements OnInit {
   }
 
   searchFilter() {
-    if (this.assetIdentification !== '' || this.assetType !== null) {
-      this.assetIdentification = this.assetIdentification.toLowerCase();
-      if (this.assetID == '') {
-        this.assetID = this.assetIdentification;
+    if (this.vehicleIdentification !== '' || this.currentStatus !== null) {
+      this.vehicleIdentification = this.vehicleIdentification.toLowerCase();
+      if (this.vehicleID == '') {
+        this.vehicleID = this.vehicleIdentification;
       }
-      this.isSearch = true;
-      this.allData = [];
+      this.disableSearch = true;
       this.dataMessage = Constants.FETCHING_DATA;
+      this.vehicles = [];
       this.lastEvaluatedKey = ''
+      this.suggestedVehicles = [];
       this.initDataTable();
-
     } else {
       return false;
     }
   }
-
-  clearInput() {
-    this.suggestedAssets = null;
-  }
-
-  clearSuggestions() {
-    this.assetIdentification = '';
-  }
-
-  resetFilter() {
-    if (this.assetIdentification !== '' || this.assetType !== null) {
-      this.assetID = '';
-      this.assetIdentification = '';
-      this.assetType = null;
-      this.suggestedAssets = [];
-      this.allData = [];
-      this.isSearch = true;
-      this.dataMessage = Constants.FETCHING_DATA;
-      this.lastEvaluatedKey = ''
-      this.initDataTable();
-
-    } else {
-      return false;
-    }
-  }
-
+  
   refreshData() {
-    this.allData = [];
-    this.assetID = '';
-    this.assetIdentification = '';
-    this.assetType = [];
-    this.suggestedAssets = [];
+    this.vehicleID = '';
+    this.suggestedVehicles = [];
+    this.vehicleIdentification = '';
+    this.currentStatus = null;
+    this.vehicles = [];
     this.lastEvaluatedKey = '';
     this.loaded = false;
     this.initDataTable();
     this.dataMessage = Constants.FETCHING_DATA;
-
   }
-  
-   generateVehicleCSV() {
-        if (this.allData.length > 0) {
-            let dataObject = []
-            let csvArray = []
-            this.allData.forEach(element => {
-                let obj = {}
-                obj["Asset Name/Number"] = element.assetIdentification
-                obj["VIN"] = element.VIN
-                obj["Asset Type"] = element.assetType
-                obj["Make"] = element.manufacturer.toLowerCase() ? element.manufacturer.toLowerCase(): '-'
-                obj["year"] = element.year
-                obj["Ownership"] = element.ownerShip
-                 if(element.ownerShip === 'rented') {
-                   obj["Company Name"] = element.ownCname
-                 }
-                 else if(element.ownerShip === 'leased') {
-                   obj["Company Name"] = element.ownCname 
-                 }
-                 else if(element.ownerShip === 'ownerOperator') {
-                   obj["Company Name"] = this.contactsObjects[element.ownerOperator]
-                 }
-                obj["Annual Safety Date"] = element.annualSafetyDate
-                obj["Status"] = element.currentStatus
-                dataObject.push(obj)
-            });
-            let headers = Object.keys(dataObject[0]).join(',')
-            headers += '\n'
-            csvArray.push(headers)
-            dataObject.forEach(element => {
-                let obj = Object.values(element).join(',')
-                obj += '\n'
-                csvArray.push(obj)
-            });
-            const blob = new Blob(csvArray, { type: 'text/csv;charset=utf-8;' });
-            const link = document.createElement('a');
-            if (link.download !== undefined) {
-                const url = URL.createObjectURL(blob);
-                link.setAttribute('href', url);
-                link.setAttribute('download', `${moment().format("YYYY-MM-DD:HH:m")}Vehicle-Report.csv`);
-                document.body.appendChild(link);
-                link.click();
-                document.body.removeChild(link);
-            }
-        }
-        else {
-            this.toastr.error("No Records found")
-        }
+
+  resetFilter() {
+    if (this.vehicleIdentification !== '' || this.currentStatus !== null) {
+      this.disableSearch = true;
+      this.vehicleID = '';
+      this.suggestedVehicles = [];
+      this.vehicleIdentification = '';
+      this.currentStatus = null;
+      this.lastEvaluatedKey = ''
+      this.vehicles = [];
+      this.dataMessage = Constants.FETCHING_DATA;
+      this.initDataTable();
+    } else {
+      return false;
+    }
+  }
+   clear(table: Table) {
+        table.clear();
     }
 
-  clear(table: Table) {
-    table.clear();
-  }
 }
+
+
