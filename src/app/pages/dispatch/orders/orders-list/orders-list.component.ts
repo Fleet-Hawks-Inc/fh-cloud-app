a9eed0dfdcae8cbf0656dcf0aaf8fdfc613c21e4
Amitoj Singh <amitojsingh@fleethawks.com>
2022-06-17 15:36:17 -0400

    fix: order list scheduler

--- src/app/pages/dispatch/orders/orders-list/orders-list.component.ts
+++ src/app/pages/dispatch/orders/orders-list/orders-list.component.ts
@@ -238,19 +238,18 @@ export class OrdersListComponent implements OnInit {
     orderNumber:null,
     name:null,
     time:null,
+    repeatType:null,
     type:{
-      daysNo:0,
+      daysNo:'',
       days:[]
     },
-    range:{
-      dateRange:{
-        to:null,
-        from:null,
-      },
-      months:[]
-    }
+    rangeType:null,
+    dateRange:{
+      to:null,
+      from:null,
+    },
+    selectedMonths:[]
   }
-
   saveDisabled=false;
   repeatType=null;
   range=null;
@@ -859,17 +858,17 @@ export class OrdersListComponent implements OnInit {
       orderNumber:null,
       name:null,
       time:null,
+      repeatType:null,
       type:{
-        daysNo:0,
+        daysNo:"",
         days:[]
       },
-      range:{
-        dateRange:{
-          to:null,
-          from:null,
-        },
-        months:[]
-      }
+      rangeType:null,
+     dateRange:{
+      to:null,
+      from:null,
+    },
+    selectedMonths:[]
     }
 
   }
@@ -903,16 +902,23 @@ export class OrdersListComponent implements OnInit {
 
   onRangeCheckboxChange(value,isChecked){
     if(isChecked){
-      this.scheduler.range.months.push(value)
+      this.scheduler.selectedMonths.push(value)
     }
     else{
-      const index=this.scheduler.range.months.findIndex(x=>x==value);
+      const index=this.scheduler.selectedMonths.findIndex(x=>x==value);
       this.scheduler.type.days.splice(index,1)
     }
   }
 
   async saveScheduler(){
     this.saveDisabled=true;
+    if(this.scheduler.orderID) this.scheduler.orderNumber=this.orders[this.scheduler.orderID]
+    if(this.scheduler.orderID==null || this.scheduler.orderNumber==null)
+    {
+      this.toastr.error("Reference Order is required");
+      this.saveDisabled=false;
+      return 
+    }
     if(this.scheduler.name==null){
       this.toastr.error("Scheduler Name is required");
       this.saveDisabled=false;
@@ -933,30 +939,57 @@ export class OrdersListComponent implements OnInit {
       this.saveDisabled=false;
       return;
     }
+
+    if(!this.scheduler.dateRange.from || !this.scheduler.dateRange.to){
+      this.toastr.error("Date Range is required");
+      this.saveDisabled=false;
+      return
+    }
+    else if(this.scheduler.dateRange.to<this.scheduler.dateRange.from){
+      this.toastr.error("Date range from must be greater")
+      this.saveDisabled=false;
+      return;
+
+    }
+    if(this.range=="month"){
+      if(this.scheduler.selectedMonths.length===0){
+        this.toastr.error("Please Select at least 1 month")
+        this.saveDisabled=false;
+        return
+      }
+    }
+
     if(this.repeatType=="selectDaysNo"){
       delete this.scheduler.type.days
     }
-    else{
+    else if (this.repeatType=="days"){
       delete this.scheduler.type.daysNo
     }
-
-    if(this.range=="date"){
-      delete this.scheduler.range.months;
-    }
     else{
-      delete this.scheduler.range.dateRange
+      delete this.scheduler.type
+    }
+
+    if(this.range=="everyMonth"){
+      delete this.scheduler.selectedMonths;
     }
     const scheduleData={
       orderID:this.scheduler.orderID,
       orderNumber:this.scheduler.orderNumber,
+      repeatType:this.repeatType,
       sName:this.scheduler.name,
-      sType:this.scheduler.type,
+      dateRange:this.scheduler.dateRange,
+      sType:(this.scheduler.type)?this.scheduler.type:undefined,
+      selectedMonths:this.scheduler.selectedMonths?this.scheduler.selectedMonths:undefined,
+      sRange:this.range,
       sTime:this.scheduler.time,
-      sRange:this.scheduler.range
+      timezone: moment.tz.guess()
   }
+
     this.apiService.postData('orders/schedule',scheduleData).subscribe({
       complete:()=>{},
-      error:(err)=>{},
+      error:(err)=>{
+        this.saveDisabled=false
+      },
       next:(res)=>{
         this.toastr.success("Schedule added successfully");
         this.modalService.dismissAll();
