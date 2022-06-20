18522fa1974da6b386aefc194c8ad9cecb7ad52b
PratikGanjareFleetHawks <pratik@fleethawks.com>
2022-06-20 06:15:03 +0000

    feat: brokerage report update

--- src/app/pages/reports/dispatch/brokerage-report/manage-brokerage.module.ts
+++ src/app/pages/reports/dispatch/brokerage-report/manage-brokerage.module.ts
@@ -14,12 +14,11 @@ import {
   NgbModule,
 } from "@ng-bootstrap/ng-bootstrap";
 import { NgxDatatableModule } from "@swimlane/ngx-datatable";
-import { SharedModule, CustomAdapter } from "../../../../shared/shared.module";
+import { SharedModule} from "../../../../shared/shared.module";
 import { NgSelectModule } from "@ng-select/ng-select";
 import { NgxSpinnerModule } from "ngx-spinner";
 import { ChartsModule } from "ng2-charts";
 //import { unsavedChangesGuard } from "src/app/guards/unsaved-changes.guard";
-///import { Injectable } from "@angular/core";
 import { GoogleMapsModule } from "@angular/google-maps";
 import { InfiniteScrollModule } from "ngx-infinite-scroll";
 import { ButtonModule } from 'primeng/button';
@@ -37,6 +36,63 @@ const routes: Routes = [
   { path: "boverview", component: BoverviewComponent },
   { path: "brokerage", component: BrokerageComponent },
 ];
+
+
+
+
+@Injectable()
+export class CustomAdapter extends NgbDateAdapter<string> {
+  readonly DELIMITER = "-";
+
+  fromModel(value: string): NgbDateStruct {
+    if (!value) return null;
+    let parts = value.split(this.DELIMITER);
+    return {
+      year: +parseInt(parts[0]),
+      month: +parseInt(parts[1]),
+      day: +parseInt(parts[2]),
+    };
+  }
+
+  toModel(date: NgbDateStruct): string { // from internal model -> your mode
+    return date
+      ? date.year +
+          this.DELIMITER +
+          ("0" + date.month).slice(-2) +
+          this.DELIMITER +
+          ("0" + date.day).slice(-2)
+      : null;
+  }
+}
+
+@Injectable()
+export class CustomDateParserFormatter extends NgbDateParserFormatter {
+  readonly DELIMITER = "/";
+
+  parse(value: string): NgbDateStruct | null {
+    if (value) {
+      const date = value.split(this.DELIMITER);
+      return {
+        year: parseInt(date[2], 10),
+        month: parseInt(date[1], 10),
+        day: parseInt(date[0], 10),
+      };
+    }
+    return null;
+  }
+
+  format(date: NgbDateStruct | null): string {
+    return date
+      ? date.year + this.DELIMITER + date.month + this.DELIMITER + date.day
+      : "";
+  }
+}
+
+
+
+
+
+
 
 @NgModule({
   declarations: [BoverviewComponent, BrokerageComponent],
