b2a83d12db019ec532ea6ed934f2f73d166c28ee
PratikGanjareFleetHawks <pratik@fleethawks.com>
2022-07-20 10:43:13 +0000

    enhancement: balance sheet

--- src/app/pages/accounts/reports/balance-sheet/balance-sheet.component.ts
+++ src/app/pages/accounts/reports/balance-sheet/balance-sheet.component.ts
@@ -14,134 +14,17 @@ import * as moment from 'moment'
 export class BalanceSheetComponent implements OnInit {
     @ViewChild('dt') table: Table;
     get = _.get;
-
-    dataMessage = Constants.FETCHING_DATA;
-    mainArray = [];
-    allTotal = [];
-
-    assetArr = [];
-    liabilityArr = [];
-    equityArr = [];
-    revenueArr = [];
-    accArray = []
-    expenseArr = [];
-    lastItemSK = "";
-    actName = null;
-    actType = null;
-    accounts: any = [];
-    exportData = []
-    filter = {
+    accounts: any = []
+      filter = {
         actType: null,
         actName: null,
         startDate: null,
         endDate: null,
     };
     datee = '';
-    disableSearch = false;
-    exportLoading = false;
-    loaded = false;
-    start = null;
-    end = null;
-    currTab = "CAD";
-    isLoadText = "Load More...";
-    isLoad = false;
-    dateMinLimit = { year: 1950, month: 1, day: 1 };
-    date = new Date();
-    futureDatesLimit = { year: this.date.getFullYear() + 30, month: 12, day: 31 };
-    //deactivatePredefined = true;
-    CAD = [];
-    USD = [];
-    actID = "";
-    acClasses = [];
-    totalUSD = 0;
-    totalCUSD = 0;
-    totalDCAD = 0;
-    totalCCAD = 0
-    allExportData = [];
-    lastExportSk = "";
-    data1 = [];
-    cadDebitTotal = 0;
-    cadCreditTotal = 0;
-    usdDebitTotal = 0;
-    usdCreditTotal = 0;
-
-    accountsClassObjects = {};
-    coaData = {}
-    mainactType: any;
-    logDate: any;
-    opnBalTypeCAD: string;
-    tempResults = [];
-    account: any;
-    lastTimestamp: string;
-    lastKey: "";
-    dataMsgCad: string;
     currency = 'CAD';
-
-
-    transactionLogCAD = [];
-    creditTotal = 0;
-
-
-    sortInventoryAssets: any = [];
-    sortOtherCurrentAssets: any = [];
-    sortCapitalAsset: any = [];
-    sortONCAsset: any = [];
-    currLiability: any = [];
-    scEquity: any = [];
-    retainedEarnings: any = [];
-    ltLiability: any = [];
-
-    //Current Assets Total CAD USD
-    currentAssetTotalCAD = 0;
-    currentAssetCreditTotalCAD = 0;
-    currentAssetTotalUSD = 0;
-    currentAssetCreditTotalUSD = 0;
-
-    //Inventory Assets Total CAD USD
-    inventoryAssetTotalCAD = 0;
-    inventoryAssetTotalUSD = 0;
-
-    //Capital Assets Total CAD USD
-    capitalAssetTotalCAD = 0;
-    capitalAssetTotalUSD = 0;
-
-    //Other Non Current Assets Total CAD USD
-    ONCAssetTotalCAD = 0;
-    ONCAssetTotalUSD = 0;
-
-    //Total of All Assets CAD USD
-    totalAssetsCAD = 0;
-    totalAssetsUSD = 0;
-
-    //Current Liability Total CAD USD
-    totalCurrentLiabilityCAD = 0;
-    totalCurrentLiabilityUSD = 0;
-
-    //Long Term Liabilities Total CAD USD
-    totalLongTermLiabilityCAD = 0;
-    totalLongTermLiabilityUSD = 0;
-
-    //Total of all liability
-    totalLiabilityCAD = 0;
-    totalLiabilityUSD = 0;
-
-    //Share Capital Total CAD USD
-    totalShareCapitalCAD = 0;
-    totalShareCapitalUSD = 0;
-
-    //Retained Earnings Total CAD USD
-    totalRetainedEarningsCAD = 0;
-    totalRetainedEarningsUSD = 0;
-
-    //Total of All Equities
-    totalEquityCAD = 0
-    totalEquityUSD = 0;
-
-    //Total of All Equities and Liabilities
-    totalLiabilityEquityCAD = 0;
-    totalLiabilityEquityUSD = 0;
-      _selectedColumns: any[];
-
+    dataMessage = Constants.FETCHING_DATA;
+    _selectedColumns: any[];
     dataColumns = [
     { width: '14%', field: 'accountNo', header: 'Account Number', type: "text" },
     { width: '14%', field: 'accountName', header: 'Account Name', type: "text" },
@@ -156,7 +39,6 @@ export class BalanceSheetComponent implements OnInit {
         this.filter.startDate = moment().subtract(15, 'day').format('YYYY-MM-DD');
         this.fetchBalance();
         this.setToggleOptions();
-
     }
 
 
@@ -176,6 +58,9 @@ export class BalanceSheetComponent implements OnInit {
     async fetchBalance(refresh?: boolean) {
         this.accountService.getData(`chartAc/get/balance/report/${this.currency}/?start=${this.filter.startDate}&end=${this.filter.endDate}&date=${this.datee}`)
             .subscribe(async (result: any) => {
+            if(result.data.length === 0){
+            this.dataMessage = Constants.NO_RECORDS_FOUND;
+            }
                 if (result.data.length > 0) {
                     result.data.map((v) => {
                         this.accounts.push(v);
@@ -184,36 +69,5 @@ export class BalanceSheetComponent implements OnInit {
                     })
                 }
             });
-    }
-
-    changeTab(type) {
-        this.currTab = type;
-        this.accounts = [];
-        if (this.currTab === 'CAD') {
-            this.currency = 'CAD'
-            this.currentAssetTotalCAD = 0;
-            this.currentAssetCreditTotalCAD = 0;
-            this.totalCurrentLiabilityCAD = 0;
-            this.inventoryAssetTotalCAD = 0;
-            this.capitalAssetTotalCAD = 0;
-            this.ONCAssetTotalCAD = 0;
-            this.totalLongTermLiabilityCAD = 0;
-            this.totalShareCapitalCAD = 0;
-            this.totalRetainedEarningsCAD = 0;
-            // this.lastItemSK = '';
-            this.fetchBalance();
-        } else if (this.currTab === 'USD') {
-            this.currency = 'USD'
-            this.currentAssetTotalUSD = 0;
-            this.inventoryAssetTotalUSD = 0;
-            this.totalCurrentLiabilityUSD = 0;
-            this.capitalAssetTotalUSD = 0;
-            this.ONCAssetTotalUSD = 0;
-            this.totalLongTermLiabilityUSD = 0;
-            this.totalShareCapitalUSD = 0;
-            this.totalRetainedEarningsUSD = 0;
-            // this.lastItemSK = '';
-            this.fetchBalance();
-        }
     }
 }
