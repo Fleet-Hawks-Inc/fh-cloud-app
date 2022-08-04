6d92d2b10d0addc0aad9a8763499c9435d2caa40
Kunal Deshmukh <kunal@fleethawks.com>
2022-08-04 21:55:42 +0530

    feat: driver download eld file

--- src/app/pages/compliance/hos/detailed/detailed.component.ts
+++ src/app/pages/compliance/hos/detailed/detailed.component.ts
@@ -5,6 +5,7 @@ import { ActivatedRoute } from "@angular/router";
 import * as moment from "moment";
 import * as _ from "lodash";
 import { ELDService } from "src/app/services/eld.service";
+import { Subject } from "rxjs";
 
 declare var $: any;
 
@@ -150,5 +151,72 @@ export class DetailedComponent implements OnInit {
     let isChecked = e.checked;
     this.complianceText = this.isCanada ? "Canada" : "USA";
     await this.fetchEvents();
+  }
+
+  base64: any;
+  showUsaLogs = false;
+  downloadFileName = "download_file";
+  async downloadUSAFile() {
+    const response: any = await this.eldService
+      .postData("reports/usa/printLogs", {
+        HOSDriverId: 77400,
+        FromDate: 1634156520,
+        EndDate: 1634156520,
+      })
+      .toPromise();
+    if (response && response.data) {
+      console.log(response.data.File);
+      this.base64 = this._base64ToArrayBuffer(response.data.File);
+      this.showUsaLogs = true;
+
+      this.downloadFileName = response.data.FileName;
+    }
+  }
+
+  async downloadCANFile() {
+    const response: any = await this.eldService
+      .postData("reports/can/printLogs", {
+        HOSDriverId: 77400,
+        FromDate: 1634156520,
+        EndDate: 1634156520,
+      })
+      .toPromise();
+    if (response && response.data) {
+      const data = response.data[0];
+      console.log(data);
+      this.base64 = this._base64ToArrayBuffer(data.File);
+      this.showUsaLogs = true;
+
+      this.downloadFileName = data.FileName;
+    }
+  }
+
+  downLoadFile() {
+    const file = new Blob([this.base64], { type: "application/pdf" });
+    const navigator = window.navigator as any;
+    if (navigator && navigator.msSaveOrOpenBlob) {
+      // for IE
+      navigator.msSaveOrOpenBlob(file, this.downloadFileName);
+    } else {
+      var a = document.createElement("a");
+      document.body.appendChild(a);
+      a.style.display = "none";
+      var csvUrl = URL.createObjectURL(file);
+      a.href = csvUrl;
+      a.download = this.downloadFileName;
+      a.click();
+      URL.revokeObjectURL(a.href);
+      a.remove();
+    }
+  }
+
+  _base64ToArrayBuffer(base64) {
+    var binary_string = window.atob(base64);
+    var len = binary_string.length;
+    var bytes = new Uint8Array(len);
+    for (var i = 0; i < len; i++) {
+      bytes[i] = binary_string.charCodeAt(i);
+    }
+    return bytes.buffer;
   }
 }
